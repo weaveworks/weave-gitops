7d8e253cc1bb1147ab381d37e6a198efddabce92
Jerry Jackson <jerry@weave.works>
2021-03-22 15:28:01 -0600

    more lines for coverage

--- main.go
+++ main.go
@@ -8,9 +8,11 @@ func main() {
 }
 
 func test() string {
+	a := 1
+	w := 1
 	x := 6
 	y := 28
 	z := 496
 	q := 8128
-	return fmt.Sprintf("%s:%d", "test", x*y*z*q)
+	return fmt.Sprintf("%s:%d", "test", a*w*x*y*z*q)
 }
