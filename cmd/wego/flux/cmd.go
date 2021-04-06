cf59b15c8db485189312c43d0d7267f525b8501b
Justin thompson <justinthompson@Justins-MacBook-Pro.local>
2021-04-06 09:21:37 -0600

    use dependencies file

--- cmd/wego/flux/cmd.go
+++ cmd/wego/flux/cmd.go
@@ -6,28 +6,43 @@ import (
 	"os"
 	"os/exec"
 
+	"github.com/BurntSushi/toml"
 	"github.com/spf13/cobra"
 )
 
 //go:embed flux
-var flux []byte
+var fluxExe []byte
+
+type fluxDep struct {
+	Flux flux
+}
+type flux struct {
+	Version string
+}
 
 var Cmd = &cobra.Command{
 	Use:   "flux",
 	Short: "Use flux commands",
 	Run:   runCmd,
 }
+var exe = "temp/bin/flux-"
 
 func init() {
-	_ = os.WriteFile("bin/flux", flux, 0755)
+	var f fluxDep
+	if _, err := toml.DecodeFile("tools/dependencies.toml", &f); err != nil {
+		fmt.Println(err)
+	}
+	exe = exe + f.Flux.Version
+	_ = os.WriteFile(exe, fluxExe, 0755)
 }
 
 func runCmd(cmd *cobra.Command, args []string) {
-	c := exec.Command("./bin/flux", args...)
+	c := exec.Command("./"+exe, args...)
 
 	// run command
 	if output, err := c.CombinedOutput(); err != nil {
 		fmt.Println("Error:", err)
+		os.Exit(1)
 	} else {
 		fmt.Printf("Output: %s\n", output)
 	}
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/weaveworks/weave-gitops
 go 1.16
 
 require (
+	github.com/BurntSushi/toml v0.3.1
 	github.com/kr/pretty v0.2.0 // indirect
 	github.com/sirupsen/logrus v1.2.0
 	github.com/spf13/cobra v1.1.3
