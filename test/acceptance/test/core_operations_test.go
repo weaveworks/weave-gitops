c4446c2ed1249c6ee401f4b95d7bd6a18a922080
Jerry Jackson <jerry@weave.works>
2021-04-12 10:09:25 -0600

    cleanup

--- test/acceptance/test/core_operations_test.go
+++ test/acceptance/test/core_operations_test.go
@@ -3,24 +3,12 @@ package test
 // Runs basic WeGO operations against a kind cluster.
 
 import (
-	"bytes"
-	"encoding/base64"
-	"encoding/json"
-	"fmt"
-	"io/ioutil"
 	"os"
-	"path/filepath"
-	"strings"
 	"testing"
-	"text/template"
-	"time"
 
-	log "github.com/sirupsen/logrus"
 	"github.com/stretchr/testify/require"
-	"github.com/weaveworks/cluster-api-provider-existinginfra/apis/cluster.weave.works/v1alpha3"
-	capeios "github.com/weaveworks/cluster-api-provider-existinginfra/pkg/apis/wksprovider/machine/os"
-	clientcmdv1 "k8s.io/client-go/tools/clientcmd/api/v1"
-	"sigs.k8s.io/yaml"
+	"github.com/weaveworks/weave-gitops/pkg/fluxops"
+	"github.com/weaveworks/weave-gitops/pkg/status"
 )
 
 // Run core operations and check status
@@ -29,8 +17,8 @@ func TestCoreOperations(t *testing.T) {
 	err := os.Setenv("HOME", "/iewojfoiwejfoiwjfwoijfewj")
 	require.NoError(t, err)
 	require.Equal(t, status.GetClusterStatus(), status.Unknown)
-	err := os.Setenv("HOME", savedHome)
+	err = os.Setenv("HOME", savedHome)
 	require.NoError(t, err)
 	require.Equal(t, status.GetClusterStatus(), status.Unmodified)
-	callFlux("bootstrap github --owner=$GITHUB_USER --repository=fleet-infra --branch=main --path=./clusters/my-cluster --personal")
+	fluxops.CallFlux("bootstrap github --owner=$GITHUB_USER --repository=fleet-infra --branch=main --path=./clusters/my-cluster --personal")
 }
