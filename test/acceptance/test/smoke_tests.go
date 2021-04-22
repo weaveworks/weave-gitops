1a59523f4756f502291b1ca199bc1d5746e5ccea
rokshana-b <rokshana@weave.works>
2021-04-22 12:26:11 -0400

    add 'wego --help' test case

--- test/acceptance/test/smoke_tests.go
+++ test/acceptance/test/smoke_tests.go
@@ -67,4 +67,42 @@ var _ = Describe("WEGO Acceptance Tests ", func() {
 			Eventually(session).Should(gbytes.Say("Branch: main|HEAD\n"))
 		})
 	})
+
+	It("Verify that wego help flag prints the help text", func() {
+
+		var session *gexec.Session
+		var err error
+
+		By("Given I have a wego binary installed on my local machine", func() {
+			Expect(FileExists(WEGO_BIN_PATH)).To(BeTrue())
+		})
+
+		By("When I run the command 'wego --help' ", func() {
+			command := exec.Command(WEGO_BIN_PATH, "--help")
+			session, err = gexec.Start(command, GinkgoWriter, GinkgoWriter)
+			Expect(err).ShouldNot(HaveOccurred())
+		})
+
+		By("Then I should see help message printed with the product name", func() {
+			Eventually(session).Should(gbytes.Say("Weave GitOps"))
+		})
+
+		By("And Usage category", func() {
+			Eventually(session).Should(gbytes.Say("Usage:"))
+			Eventually(session.Wait().Out.Contents()).Should(ContainSubstring("wego [command]"))
+		})
+
+		By("And Avalaible-Commands category", func() {
+			Eventually(session).Should(gbytes.Say("Available Commands:"))
+			Eventually(string(session.Wait().Out.Contents())).Should(MatchRegexp(`flux[\s]+Use flux commands`))
+			Eventually(string(session.Wait().Out.Contents())).Should(MatchRegexp(`help[\s]+Help about any command`))
+			Eventually(string(session.Wait().Out.Contents())).Should(MatchRegexp(`version[\s]+Display wego version`))
+		})
+
+		By("And Flags category", func() {
+			Eventually(session).Should(gbytes.Say("Flags:"))
+			Eventually(string(session.Wait().Out.Contents())).Should(MatchRegexp(`-h, --help[\s]+help for wego`))
+			Eventually(string(session.Wait().Out.Contents())).Should(MatchRegexp(`-v, --verbose[\s]+Enable verbose output`))
+		})
+	})
 })
