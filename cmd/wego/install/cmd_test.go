ed3d275b3fc419fb365d988a2fb9edcc6399e195
Luiz Filho <luizbafilho@gmail.com>
2021-05-03 14:38:36 -0300

    Adding wego install cmd command tests

--- cmd/wego/install/cmd_test.go
+++ cmd/wego/install/cmd_test.go
@@ -1,24 +1,29 @@
 package install
 
 import (
-	"fmt"
 	"testing"
 
+	"github.com/spf13/cobra"
 	"github.com/stretchr/testify/assert"
 	"github.com/weaveworks/weave-gitops/pkg/fluxops"
 	"github.com/weaveworks/weave-gitops/pkg/fluxops/fluxopsfakes"
 )
 
-func TestRunCmdError(t *testing.T) {
+func TestRunCmd(t *testing.T) {
 	assert := assert.New(t)
 
 	fakeHandler := &fluxopsfakes.FakeFluxHandler{
 		HandleStub: func(args string) ([]byte, error) {
-			return []byte(""), fmt.Errorf("foo")
+			return []byte("manifests"), nil
 		},
 	}
 	fluxops.SetFluxHandler(fakeHandler)
 
-	_, err := fluxops.Install("my-namespace")
-	assert.EqualError(err, "foo")
+	params = paramSet{
+		namespace: "my-namespace",
+	}
+	runCmd(&cobra.Command{}, []string{})
+
+	args := fakeHandler.HandleArgsForCall(0)
+	assert.Equal(args, "install --namespace=my-namespace --export")
 }
