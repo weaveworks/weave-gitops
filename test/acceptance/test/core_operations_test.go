1e93936b7740115346ec3ebf3e4f7aaafe50d9fa
Justin Thompson <37157877+J-Thompson12@users.noreply.github.com>
2021-05-07 11:08:27 -0600

    increase coverage on integration tests (#120)
    * increase coverage on integration tests
    
    * add go acc
    
    * run with acceptance tests
    
    * ignore fluxopsfakes
    
    * include file exists test
    
    * change file
    
    Co-authored-by: Justin thompson <justinthompson@Justins-MacBook-Pro.local>

--- test/acceptance/test/core_operations_test.go
+++ test/acceptance/test/core_operations_test.go
@@ -229,5 +229,5 @@ func deleteRepos(t *testing.T) {
 }
 
 func checkInitialStatus(t *testing.T) {
-	require.Equal(t, status.GetClusterStatus(), status.Unmodified)
+	require.Equal(t, status.Unmodified, status.GetClusterStatus())
 }
