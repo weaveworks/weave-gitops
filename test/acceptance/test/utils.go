e2626ab3b442edf5fd499a3d6241c9e8806c8840
Iftikhar Ahmad <iftikhar@weave.works>
2021-05-18 21:54:47 +0100

    show cluster pods on test failure

--- test/acceptance/test/utils.go
+++ test/acceptance/test/utils.go
@@ -5,6 +5,7 @@ package acceptance
 
 import (
 	"os"
+	"os/exec"
 	"time"
 )
 
@@ -19,4 +20,20 @@ func FileExists(name string) bool {
 		}
 	}
 	return true
+}
+
+// showItems displays the current set of a specified object type in tabular format
+func showItems(itemType string) error {
+	return runCommandPassThrough([]string{}, "kubectl", "get", itemType, "--all-namespaces", "-o", "wide")
+}
+
+// Run a command, passing through stdout/stderr to the OS standard streams
+func runCommandPassThrough(env []string, name string, arg ...string) error {
+	cmd := exec.Command(name, arg...)
+	if len(env) > 0 {
+		cmd.Env = env
+	}
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	return cmd.Run()
 }
