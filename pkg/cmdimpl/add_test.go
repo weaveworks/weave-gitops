9ef33bc7fb93159746e8fac044093c984ae8d604
Jose C Ordaz <josecordaz@gmail.com>
2021-05-24 12:34:10 -0500

    Fixed typo

--- pkg/cmdimpl/add_test.go
+++ pkg/cmdimpl/add_test.go
@@ -182,7 +182,7 @@ var _ = Describe("Test helm manifest", func() {
 		params.Name = "simple-name"
 		params.Name = "simple-name"
 		params.Path = "./my-chart"
-		params.Namespace = "wego-sytem"
+		params.Namespace = "wego-system"
 
 		Expect(generateHelmManifest()).Should(Succeed())
 	})
