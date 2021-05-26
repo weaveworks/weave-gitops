b019028d19ca8758ef21f9519329b26d8339377d
Iftikhar Ahmad <iftikhar@weave.works>
2021-05-26 13:05:02 +0100

    wait for the cluster to be ready

--- test/acceptance/test/scripts/kind-cluster.sh
+++ test/acceptance/test/scripts/kind-cluster.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
+echo "Delete existing kind clusters"
 kind delete clusters --all
+echo "Create a new kind cluster with name "$1
 kind create cluster --name=$1 --image=$2 --config=./configs/kind-config.yaml
+echo "Wait for pods to be ready "$1
+kubectl wait --for=condition=Ready --timeout=300s -n kube-system --all pod
