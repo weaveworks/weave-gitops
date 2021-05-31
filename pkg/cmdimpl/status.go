cc672fafd17ab133276282f236a90693d0a5adf6
Jose C Ordaz <josecordaz@gmail.com>
2021-05-31 15:45:11 -0500

    Added status application logic and unit tests

---
+++ pkg/cmdimpl/status.go
@@ -0,0 +1,110 @@
+package cmdimpl
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"regexp"
+	"strings"
+
+	"github.com/weaveworks/weave-gitops/pkg/fluxops"
+	"gopkg.in/yaml.v2"
+
+	"github.com/weaveworks/weave-gitops/pkg/utils"
+)
+
+// Status provides the implementation for the wego status application command
+func Status(allParams AddParamSet) error {
+	// verify the app is in the wego apps folder
+	fluxRepoName, err := fluxops.GetRepoName()
+	if err != nil {
+		return wrapError(err, "could not get repo name")
+	}
+	reposDir := filepath.Join(os.Getenv("HOME"), ".wego", "repositories")
+	fluxRepo := filepath.Join(reposDir, fluxRepoName)
+	appPath := filepath.Join(fluxRepo, "apps", allParams.Name)
+	if err != nil {
+		return fmt.Errorf("error getting path for app [%s], err: %s \n", allParams.Name, err)
+	}
+
+	if !utils.Exists(appPath) && allParams.Name != "wego" {
+		return fmt.Errorf("app provided does not exist in apps folder [%s]\n", appPath)
+	}
+
+	deploymentType, err := getDeploymentType(allParams.Namespace, allParams.Name)
+	if err != nil {
+		return fmt.Errorf("error getting deployment type [%s]", err)
+	}
+
+	err = printOutLatestSuccessfulDeploymentType(allParams.Namespace, allParams.Name, deploymentType)
+	if err != nil {
+		return fmt.Errorf("error on latest deployment time [%s]", err)
+	}
+
+	output, err := fluxops.GetAllResourcesStatus(allParams.Name)
+	if err != nil {
+		return fmt.Errorf("error getting flux app resources status [%s", err)
+	}
+
+	fmt.Println(string(output))
+
+	return err
+}
+
+type Yaml struct {
+	Status struct {
+		Conditions []struct {
+			LastTransitionTime string `yaml:"lastTransitionTime"`
+		} `yaml:"conditions"`
+	} `yaml:"status"`
+}
+
+func printOutLatestSuccessfulDeploymentType(namespace, appName string, deploymentType DeploymentType) error {
+
+	c := fmt.Sprintf(`kubectl \
+			-n %s \
+			get %s/%s -oyaml`,
+		namespace,
+		deploymentType,
+		appName,
+	)
+
+	stdout, stderr, err := utils.CallCommandSeparatingOutputStreams(c)
+	if err != nil {
+		return fmt.Errorf("error getting resource info [%s %s]\n", err.Error(), string(stderr))
+	}
+	var yamlOutput Yaml
+	err = yaml.Unmarshal(stdout, &yamlOutput)
+	if err != nil {
+		return fmt.Errorf("error unmarshalling yaml output [%s] \n", err.Error())
+	}
+	if len(yamlOutput.Status.Conditions) == 0 {
+		return fmt.Errorf("error getting latest deployment time [%s] \n", stdout)
+	}
+	fmt.Fprintf(os.Stdout, "Latest successful deployment time: %s\n", yamlOutput.Status.Conditions[0].LastTransitionTime)
+
+	return nil
+}
+
+func getDeploymentType(namespace, appName string) (DeploymentType, error) {
+
+	stdout, err := fluxops.GetAllResources(namespace)
+	if err != nil && !strings.Contains(err.Error(), "exit status 1") {
+		return "", err
+	}
+
+	var re = regexp.MustCompile(fmt.Sprintf(`(?m)(kustomization|helmrelease)\/%s`, appName))
+
+	matches := re.FindAllStringSubmatch(string(stdout), -1)
+
+	if len(matches) != 1 {
+		return "", fmt.Errorf("error trying to get the deployment type of the app. raw output => %s", stdout)
+	}
+
+	if len(matches[0]) != 2 {
+		return "", fmt.Errorf("error trying to get the deployment type of the app. raw matches => %s", matches)
+	}
+
+	return DeploymentType(matches[0][1]), nil
+
+}
---
+++ pkg/cmdimpl/status_test.go
@@ -0,0 +1,134 @@
+package cmdimpl
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+
+	"github.com/weaveworks/weave-gitops/pkg/override"
+	"github.com/weaveworks/weave-gitops/pkg/utils"
+
+	"github.com/weaveworks/weave-gitops/pkg/fluxops"
+	"github.com/weaveworks/weave-gitops/pkg/fluxops/fluxopsfakes"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("Run Command Status Test", func() {
+	It("Verify status info for kustomize app", func() {
+
+		fmt.Println("Testing STATUS CMD")
+
+		// Setup
+		appName := "kustomize-app"
+		clusterName := "mycluster"
+
+		// create temporary directory to use as HOME
+		homePath, err := ioutil.TempDir(os.TempDir(), "status-dir")
+		Expect(err).Should(Succeed())
+		defer Expect(os.RemoveAll(homePath)).Should(Succeed())
+		Expect(os.Setenv("HOME", homePath)).Should(Succeed())
+
+		Expect(
+			os.MkdirAll(
+				filepath.Join(homePath, ".wego", "repositories", clusterName+"-wego", "apps", appName),
+				0755,
+			),
+		).Should(Succeed())
+
+		// flux mocks
+		case0 := "get all -n wego-system"
+		output0 := `NAME                     	READY	MESSAGE                                                        	REVISION                                     SUSPENDED
+gitrepository/wego       	True 	Fetched revision: main/00b92bf6606e040c59404a7257508d65d300bc91	main/00b92bf6606e040c59404a7257508d65d300bc91False
+gitrepository/kustomize-app	True 	Fetched revision: main/a2b5b8c0919f405e52619bfc52b5304240d9ef76	main/a2b5b8c0919f405e52619bfc52b5304240d9ef76False
+
+NAME                     	READY	MESSAGE                                                        	REVISION                                     SUSPENDED
+kustomization/wego       	True 	Applied revision: main/00b92bf6606e040c59404a7257508d65d300bc91	main/00b92bf6606e040c59404a7257508d65d300bc91False
+kustomization/kustomize-app	True 	Applied revision: main/a2b5b8c0919f405e52619bfc52b5304240d9ef76	main/a2b5b8c0919f405e52619bfc52b5304240d9ef76False`
+
+		case1 := "get all -A kustomize-app"
+		output1 := `NAMESPACE   	NAME                     	READY	MESSAGE                                                        	REVISION                                     	SUSPENDED
+wego-system	gitrepository/kustomize-app	True 	Fetched revision: main/a2b5b8c0919f405e52619bfc52b5304240d9ef76	main/a2b5b8c0919f405e52619bfc52b5304240d9ef76	False
+
+NAMESPACE   	NAME                     	READY	MESSAGE                                                        	REVISION                                     	SUSPENDED
+wego-system	kustomization/kustomize-app	True 	Applied revision: main/a2b5b8c0919f405e52619bfc52b5304240d9ef76	main/a2b5b8c0919f405e52619bfc52b5304240d9ef76	False
+`
+
+		fakeHandler := &fluxopsfakes.FakeFluxHandler{
+			HandleStub: func(args string) ([]byte, error) {
+				Expect(args).Should(BeElementOf(case0, case1))
+				switch args {
+				case case0:
+					return []byte(output0), nil
+				case case1:
+					return []byte(output1), nil
+				default:
+					return nil, fmt.Errorf("arguments not expected %s", args)
+				}
+			},
+		}
+
+		fluxops.SetFluxHandler(fakeHandler)
+
+		// kubectl mocks
+		case0Kubectl := `kubectl config current-context`
+		case0KubectlOutput := clusterName
+
+		case1KubectlOutput := `status:
+  conditions:
+  - lastTransitionTime: "2021-05-24T22:48:28Z"`
+		case1Kubectl := `kubectl \
+			-n wego-system \
+			get kustomization/kustomize-app -oyaml`
+
+		_ = override.WithOverrides(func() override.Result {
+
+			expectedOutput := `Latest successful deployment time: 2021-05-24T22:48:28Z
+			NAMESPACE   	NAME                     	READY	MESSAGE                                                        	REVISION                                     	SUSPENDED
+			wego-system	gitrepository/kustomize-app	True 	Fetched revision: main/a2b5b8c0919f405e52619bfc52b5304240d9ef76	main/a2b5b8c0919f405e52619bfc52b5304240d9ef76	False
+			
+			NAMESPACE   	NAME                     	READY	MESSAGE                                                        	REVISION                                     	SUSPENDED
+			wego-system	kustomization/kustomize-app	True 	Applied revision: main/a2b5b8c0919f405e52619bfc52b5304240d9ef76	main/a2b5b8c0919f405e52619bfc52b5304240d9ef76	False
+			
+			`
+
+			reader, writer, _ := os.Pipe()
+			backup := os.Stdout
+			os.Stdout = writer
+
+			// Command parameters
+			params := AddParamSet{}
+			params.DeploymentType = "kustomize"
+			params.Namespace = "wego-system"
+			params.Name = appName
+			Expect(Status(params)).Should(Succeed())
+
+			os.Stdout = backup
+
+			Expect(writer.Close()).Should(Succeed())
+			bts, err := ioutil.ReadAll(reader)
+			Expect(err).Should(Succeed())
+			Expect(reader.Close()).Should(Succeed())
+
+			Expect(string(bts)).To(Equal(expectedOutput))
+
+			return override.Result{} //CallCommandSeparatingOutputStreams
+		}, utils.OverrideBehavior(utils.CallCommandSeparatingOutputStreamsOp,
+			func(args ...interface{}) ([]byte, []byte, error) {
+				Expect(args[0].(string)).Should(BeElementOf(case0Kubectl, case1Kubectl))
+				switch (args[0]).(string) {
+				case case0Kubectl:
+					return []byte(case0KubectlOutput), []byte(""), nil
+				case case1Kubectl:
+					return []byte(case1KubectlOutput), []byte(""), nil
+				default:
+					return nil, nil, fmt.Errorf("arguments not expected %s", args)
+				}
+
+			}),
+		)
+
+	})
+})
