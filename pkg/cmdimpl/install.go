16f1090de78dffaaf4738d62e3232638ed5f05a4
Luiz Filho <luizbafilho@gmail.com>
2021-06-08 16:07:53 -0300

    Adding dry-run mode to install (#264)

--- pkg/cmdimpl/install.go
+++ pkg/cmdimpl/install.go
@@ -13,19 +13,24 @@ var appCRD []byte
 
 type InstallParamSet struct {
 	Namespace string
+	DryRun    bool
 }
 
-func Install(params InstallParamSet) ([]byte, error) {
-	kubectlApply := fmt.Sprintf("kubectl apply --namespace=%s -f -", params.Namespace)
-
-	if err := utils.CallCommandForEffectWithInputPipe(kubectlApply, string(appCRD)); err != nil {
-		return []byte(""), wrapError(err, "could not apply wego manifests")
-	}
-
-	manifests, err := fluxops.Install(params.Namespace)
+func Install(params InstallParamSet) error {
+	manifests, err := fluxops.Install(params.Namespace, params.DryRun)
 	if err != nil {
-		return nil, err
+		return fmt.Errorf("error on install %s", err)
 	}
 
-	return append(manifests, appCRD...), nil
+	if params.DryRun {
+		fmt.Print(string(manifests))
+		fmt.Println(string(appCRD))
+	} else {
+		kubectlApply := fmt.Sprintf("kubectl apply --namespace=%s -f -", params.Namespace)
+		if err := utils.CallCommandForEffectWithInputPipe(kubectlApply, string(appCRD)); err != nil {
+			return wrapError(err, "could not apply wego manifests")
+		}
+	}
+
+	return nil
 }
