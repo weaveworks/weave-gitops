3bfa9e964e2a0d57a353fd8174e9ad0577ee4528
Luiz Filho <luizbafilho@gmail.com>
2021-06-15 12:04:59 -0300

    Improving add tests

--- pkg/services/app/add_test.go
+++ pkg/services/app/add_test.go
@@ -45,6 +45,7 @@ var _ = BeforeEach(func() {
 
 	defaultParams = app.AddParams{
 		Url:            "https://github.com/foo/bar",
+		Path:           "./kustomize",
 		Branch:         "main",
 		Dir:            ".",
 		DeploymentType: "kustomize",
@@ -92,35 +93,120 @@ var _ = Describe("Add", func() {
 		Expect(deployKey).To(Equal([]byte("deploy key")))
 	})
 
-	Describe("generates source manifest", func() {
-		It("creates GitRepository when source type is git", func() {
-			defaultParams.SourceType = string(app.SourceTypeGit)
-
-			err := appSrv.Add(defaultParams)
-			Expect(err).ShouldNot(HaveOccurred())
-
-			Expect(fluxClient.CreateSourceGitCallCount()).To(Equal(1))
-
-			name, url, branch, secretRef, namespace := fluxClient.CreateSourceGitArgsForCall(0)
-			Expect(name).To(Equal("bar"))
-			Expect(url).To(Equal("ssh://git@github.com/foo/bar.git"))
-			Expect(branch).To(Equal("main"))
-			Expect(secretRef).To(Equal("weave-gitops-test-cluster"))
-			Expect(namespace).To(Equal("wego-system"))
+	Context("add app with no config repo", func() {
+		BeforeEach(func() {
+			defaultParams.AutomationRepo = ""
+			defaultParams.CommitManifests = false
 		})
 
-		It("creates HelmResitory when source type is helm", func() {
-			defaultParams.Url = "https://charts.kube-ops.io"
-			defaultParams.Chart = "loki"
+		Describe("generates source manifest", func() {
+			It("creates GitRepository when source type is git", func() {
+				defaultParams.SourceType = string(app.SourceTypeGit)
+
+				err := appSrv.Add(defaultParams)
+				Expect(err).ShouldNot(HaveOccurred())
+
+				Expect(fluxClient.CreateSourceGitCallCount()).To(Equal(1))
+
+				name, url, branch, secretRef, namespace := fluxClient.CreateSourceGitArgsForCall(0)
+				Expect(name).To(Equal("bar"))
+				Expect(url).To(Equal("ssh://git@github.com/foo/bar.git"))
+				Expect(branch).To(Equal("main"))
+				Expect(secretRef).To(Equal("weave-gitops-test-cluster"))
+				Expect(namespace).To(Equal("wego-system"))
+			})
+
+			It("creates HelmResitory when source type is helm", func() {
+				defaultParams.Url = "https://charts.kube-ops.io"
+				defaultParams.Chart = "loki"
+
+				err := appSrv.Add(defaultParams)
+				Expect(err).ShouldNot(HaveOccurred())
+
+				Expect(fluxClient.CreateSourceHelmCallCount()).To(Equal(1))
+
+				name, url, namespace := fluxClient.CreateSourceHelmArgsForCall(0)
+				Expect(name).To(Equal("loki"))
+				Expect(url).To(Equal("https://charts.kube-ops.io"))
+				Expect(namespace).To(Equal("wego-system"))
+			})
+		})
+
+		Describe("generates application goat", func() {
+			It("creates a kustomization if deployment type kustomize", func() {
+				err := appSrv.Add(defaultParams)
+				Expect(err).ShouldNot(HaveOccurred())
+
+				Expect(fluxClient.CreateKustomizationCallCount()).To(Equal(1))
+
+				name, source, path, namespace := fluxClient.CreateKustomizationArgsForCall(0)
+				Expect(name).To(Equal("bar"))
+				Expect(source).To(Equal("bar"))
+				Expect(path).To(Equal("./kustomize"))
+				Expect(namespace).To(Equal("wego-system"))
+			})
+
+			It("creates helm release using a helm repository if source type is helm", func() {
+				defaultParams.Chart = "loki"
+
+				err := appSrv.Add(defaultParams)
+				Expect(err).ShouldNot(HaveOccurred())
+
+				Expect(fluxClient.CreateHelmReleaseHelmRepositoryCallCount()).To(Equal(1))
+
+				name, chart, namespace := fluxClient.CreateHelmReleaseHelmRepositoryArgsForCall(0)
+				Expect(name).To(Equal("loki"))
+				Expect(chart).To(Equal("loki"))
+				Expect(namespace).To(Equal("wego-system"))
+			})
+
+			It("creates a helm release using a git source if source type is git", func() {
+				defaultParams.Path = "./charts/my-chart"
+				defaultParams.DeploymentType = string(app.DeployTypeHelm)
+
+				err := appSrv.Add(defaultParams)
+				Expect(err).ShouldNot(HaveOccurred())
+
+				Expect(fluxClient.CreateHelmReleaseGitRepositoryCallCount()).To(Equal(1))
+
+				name, source, path, namespace := fluxClient.CreateHelmReleaseGitRepositoryArgsForCall(0)
+				Expect(name).To(Equal("bar"))
+				Expect(source).To(Equal("bar"))
+				Expect(path).To(Equal("./charts/my-chart"))
+				Expect(namespace).To(Equal("wego-system"))
+			})
+
+			It("fails if deployment type is invalid", func() {
+				defaultParams.DeploymentType = "foo"
+
+				err := appSrv.Add(defaultParams)
+				Expect(err).Should(HaveOccurred())
+			})
+		})
+
+		It("applies the manifests to the cluster", func() {
+			fluxClient.CreateSourceGitStub = func(s1, s2, s3, s4, s5 string) ([]byte, error) {
+				return []byte("git source"), nil
+			}
+			fluxClient.CreateKustomizationStub = func(s1, s2, s3, s4 string) ([]byte, error) {
+				return []byte("kustomization"), nil
+			}
 
 			err := appSrv.Add(defaultParams)
 			Expect(err).ShouldNot(HaveOccurred())
 
-			Expect(fluxClient.CreateSourceHelmCallCount()).To(Equal(1))
+			Expect(kubeClient.ApplyCallCount()).To(Equal(3))
 
-			name, url, namespace := fluxClient.CreateSourceHelmArgsForCall(0)
-			Expect(name).To(Equal("loki"))
-			Expect(url).To(Equal("https://charts.kube-ops.io"))
+			sourceManifest, namespace := kubeClient.ApplyArgsForCall(0)
+			Expect(sourceManifest).To(Equal([]byte("git source")))
+			Expect(namespace).To(Equal("wego-system"))
+
+			kustomizationManifest, namespace := kubeClient.ApplyArgsForCall(1)
+			Expect(kustomizationManifest).To(Equal([]byte("kustomization")))
+			Expect(namespace).To(Equal("wego-system"))
+
+			appSpecManifest, namespace := kubeClient.ApplyArgsForCall(2)
+			Expect(string(appSpecManifest)).To(ContainSubstring("kind: Application"))
 			Expect(namespace).To(Equal("wego-system"))
 		})
 	})
