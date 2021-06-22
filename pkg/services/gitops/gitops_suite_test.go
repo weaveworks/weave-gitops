6f886de3a71f8e68dfd11abffed306ed947ec17f
Luiz Filho <luizbafilho@gmail.com>
2021-06-22 11:15:44 -0300

    adding unit tests

---
+++ pkg/services/gitops/gitops_suite_test.go
@@ -0,0 +1,13 @@
+package gitops_test
+
+import (
+	"testing"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+func TestGitops(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "Gitops Suite")
+}
