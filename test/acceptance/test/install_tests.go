be02bd5e40146ce5a1caca27d1fdb4ca69b419b8
rokshana-b <rokshana@weave.works>
2021-06-29 23:28:45 -0400

    remove smoke tags

--- test/acceptance/test/install_tests.go
+++ test/acceptance/test/install_tests.go
@@ -21,7 +21,7 @@ var _ = Describe("Weave GitOps Install Tests", func() {
 		})
 	})
 
-	It("SmokeTest - Validate that wego displays help text for 'install' command", func() {
+	It("Validate that wego displays help text for 'install' command", func() {
 
 		var session *gexec.Session
 		var err error
@@ -39,7 +39,7 @@ var _ = Describe("Weave GitOps Install Tests", func() {
 		})
 	})
 
-	It("SmokeTest - Verify that wego can install & uninstall wego components under default namespace `wego-system`", func() {
+	It("Verify that wego can install & uninstall wego components under default namespace `wego-system`", func() {
 
 		By("And I have a brand new cluster", func() {
 			_, err := ResetOrCreateCluster(WEGO_DEFAULT_NAMESPACE, true)
@@ -63,7 +63,7 @@ var _ = Describe("Weave GitOps Install Tests", func() {
 		})
 	})
 
-	It("SmokeTest - Verify that wego can install & uninstall wego components under a user-specified namespace", func() {
+	It("Verify that wego can install & uninstall wego components under a user-specified namespace", func() {
 
 		namespace := "test-namespace"
 
