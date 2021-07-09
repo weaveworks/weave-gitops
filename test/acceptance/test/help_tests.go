c9a35a28a0efdea6b8fbbd57d22e50c0a53917f5
rokshana-b <rokshana@weave.works>
2021-07-09 13:58:11 -0400

    refactor tests to be more flexible

--- test/acceptance/test/help_tests.go
+++ test/acceptance/test/help_tests.go
@@ -12,6 +12,7 @@ import (
 var _ = Describe("WEGO Help Tests", func() {
 
 	var sessionOutput *gexec.Session
+	var stringOutput string
 	var err error
 
 	BeforeEach(func() {
@@ -38,36 +39,57 @@ var _ = Describe("WEGO Help Tests", func() {
 	It("Verify that wego help flag prints the help text", func() {
 
 		By("When I run the command 'wego --help' ", func() {
-			sessionOutput = runCommandAndReturnSessionOutput(WEGO_BIN_PATH + " --help")
+			stringOutput, _ = runCommandAndReturnStringOutput(WEGO_BIN_PATH + " --help")
 		})
 
 		By("Then I should see help message printed for wego", func() {
-			Eventually(string(sessionOutput.Wait().Out.Contents())).Should(MatchRegexp(
-				`Weave GitOps\n*Usage:\n\s*wego \[command]\n*Available Commands:\n\s*app\s*\n\s*flux\s*Use flux commands\n\s*gitops\s*Manages your wego installation\n\s*help\s*Help about any command\n\s*version\s*Display wego version\n*Flags:\n\s*-h, --help\s*help for wego\n\s*--namespace string\s*gitops runtime namespace \(default "wego-system"\)\n\s*-v, --verbose\s*Enable verbose output\n*Use "wego \[command] --help" for more information about a command.`))
+			Eventually(stringOutput).Should(MatchRegexp(`Weave GitOps\n*Usage:\n\s*wego \[command]\n*Available Commands:`))
+			Eventually(stringOutput).Should(MatchRegexp(`app`))
+			Eventually(stringOutput).Should(MatchRegexp(`gitops\s*Manages your wego installation`))
+			Eventually(stringOutput).Should(MatchRegexp(`flux\s*Use flux commands`))
+			Eventually(stringOutput).Should(MatchRegexp(`version\s*Display wego version`))
+			Eventually(stringOutput).Should(MatchRegexp(`help\s*Help about any command`))
+			Eventually(stringOutput).Should(MatchRegexp(`Flags:\n\s*-h, --help\s*help for wego\n\s*--namespace string\s*gitops runtime namespace \(default "wego-system"\)\n\s*-v, --verbose\s*Enable verbose output\n*Use "wego \[command] --help" for more information about a command.`))
 		})
 	})
 
 	It("Verify that wego app help flag prints the help text", func() {
 
 		By("When I run the command 'wego app -h' ", func() {
-			sessionOutput = runCommandAndReturnSessionOutput(WEGO_BIN_PATH + " app -h")
+			stringOutput, _ = runCommandAndReturnStringOutput(WEGO_BIN_PATH + " app -h")
 		})
 
 		By("Then I should see help message printed for wego app", func() {
-			Eventually(string(sessionOutput.Wait().Out.Contents())).Should(MatchRegexp(
-				`Usage:\n\s*wego app \[command]\n*Available Commands:\n\s*add\s*Add a workload repository to a wego cluster\n\s*list\s*List applications\n\s*status\s*Get status of an app\n*Flags:\n\s*-h, --help\s*help for app\n*Global Flags:\n\s*--namespace string\s*gitops runtime namespace \(default "wego-system"\)\n\s*-v, --verbose\s*Enable verbose output\n*Use "wego app \[command] --help" for more information about a command.`))
+			Eventually(stringOutput).Should(MatchRegexp(`Usage:\n\s*wego app \[command]\n*Available Commands:`))
+			Eventually(stringOutput).Should(MatchRegexp(`add\s*Add a workload repository to a wego cluster`))
+			Eventually(stringOutput).Should(MatchRegexp(`list\s*List applications`))
+			Eventually(stringOutput).Should(MatchRegexp(`status\s*Get status of an app`))
+			Eventually(stringOutput).Should(MatchRegexp(`Flags:\n\s*-h, --help\s*help for app\n*Global Flags:\n\s*--namespace string\s*gitops runtime namespace \(default "wego-system"\)\n\s*-v, --verbose\s*Enable verbose output\n*Use "wego app \[command] --help" for more information about a command.`))
 		})
 	})
 
 	It("Verify that wego app add help flag prints the help text", func() {
 
 		By("When I run the command 'wego app add -h' ", func() {
-			sessionOutput = runCommandAndReturnSessionOutput(WEGO_BIN_PATH + " app add -h")
+			stringOutput, _ = runCommandAndReturnStringOutput(WEGO_BIN_PATH + " app add -h")
 		})
 
 		By("Then I should see help message printed for wego app add", func() {
-			Eventually(string(sessionOutput.Wait().Out.Contents())).Should(MatchRegexp(
-				`Associates an additional application in a git repository with a wego cluster so that its contents may be managed via GitOps\n*Usage:\n\s*wego app add \[--name <name>] \[--url <url>] \[--branch <branch>] \[--path <path within repository>] \[--private-key <keyfile>] <repository directory> \[flags]\n*Examples:\nwego app add .\n*Flags:\n\s*--app-config-url string\s*URL of external repository \(if any\) which will hold automation manifests; NONE to store only in the cluster\n\s*--auto-merge\s*If set, 'wego add' will merge automatically into the set --branch\n\s*--branch string\s*Branch to watch within git repository \(default "main"\)\n\s*--chart string\s*Specify chart for helm source\n\s*--deployment-type string\s*deployment type \[kustomize, helm] \(default "kustomize"\)\n\s*--dry-run\s*If set, 'wego add' will not make any changes to the system; it will just display the actions that would have been taken\n\s*-h, --help\s*help for add\n\s*--name string\s*Name of remote git repository\n\s*--path string\s*Path of files within git repository \(default "\.\/"\)\n\s*--private-key string\s*Private key to access git repository over ssh\n\s*--url string\s*URL of remote repository\n*Global Flags:\n\s*--namespace string\s*gitops runtime namespace \(default "wego-system"\)\n\s*-v, --verbose\s*Enable verbose output`))
+			Eventually(stringOutput).Should(MatchRegexp(`Associates an additional application in a git repository with a wego cluster so that its contents may be managed via GitOps\n*Usage:`))
+			Eventually(stringOutput).Should(MatchRegexp(`wego app add \[--name <name>] \[--url <url>] \[--branch <branch>] \[--path <path within repository>] \[--private-key <keyfile>] <repository directory> \[flags]`))
+			Eventually(stringOutput).Should(MatchRegexp(`Examples:\nwego app add .\n*Flags:`))
+			Eventually(stringOutput).Should(MatchRegexp(`--app-config-url string\s*URL of external repository \(if any\) which will hold automation manifests; NONE to store only in the cluster`))
+			Eventually(stringOutput).Should(MatchRegexp(`--auto-merge\s*If set, 'wego add' will merge automatically into the set`))
+			Eventually(stringOutput).Should(MatchRegexp(`--branch\n\s*--branch string\s*Branch to watch within git repository \(default "main"\)`))
+			Eventually(stringOutput).Should(MatchRegexp(`--chart string\s*Specify chart for helm source`))
+			Eventually(stringOutput).Should(MatchRegexp(`--deployment-type string\s*deployment type \[kustomize, helm] \(default "kustomize"\)`))
+			Eventually(stringOutput).Should(MatchRegexp(`--dry-run\s*If set, 'wego add' will not make any changes to the system; it will just display the actions that would have been taken`))
+			Eventually(stringOutput).Should(MatchRegexp(`-h, --help\s*help for add`))
+			Eventually(stringOutput).Should(MatchRegexp(`--name string\s*Name of remote git repository`))
+			Eventually(stringOutput).Should(MatchRegexp(`--path string\s*Path of files within git repository \(default "\.\/"\)`))
+			Eventually(stringOutput).Should(MatchRegexp(`--private-key string\s*Private key to access git repository over ssh`))
+			Eventually(stringOutput).Should(MatchRegexp(`--url string\s*URL of remote repository`))
+			Eventually(stringOutput).Should(MatchRegexp(`Global Flags:\n\s*--namespace string\s*gitops runtime namespace \(default "wego-system"\)\n\s*-v, --verbose\s*Enable verbose output`))
 		})
 	})
 
@@ -86,12 +108,13 @@ var _ = Describe("WEGO Help Tests", func() {
 	It("Verify that wego app list help flag prints the help text", func() {
 
 		By("When I run the command 'wego app list -h' ", func() {
-			sessionOutput = runCommandAndReturnSessionOutput(WEGO_BIN_PATH + " app list -h")
+			stringOutput, _ = runCommandAndReturnStringOutput(WEGO_BIN_PATH + " app list -h")
 		})
 
 		By("Then I should see help message printed for wego app list", func() {
-			Eventually(string(sessionOutput.Wait().Out.Contents())).Should(MatchRegexp(
-				`List applications\n*Usage:\n\s*wego app list \[flags]\n*Examples:\nwego app list\n*Flags:\n\s*-h, --help\s*help for list\n*\s*Global Flags:\n\s*--namespace string\s*gitops runtime namespace \(default "wego-system"\)\n\s*-v, --verbose\s*Enable verbose output`))
+			Eventually(stringOutput).Should(MatchRegexp(`List applications\n*Usage:\n\s*wego app list \[flags]`))
+			Eventually(stringOutput).Should(MatchRegexp(`Examples:\nwego app list`))
+			Eventually(stringOutput).Should(MatchRegexp(`Flags:\n\s*-h, --help\s*help for list\n*\s*Global Flags:\n\s*--namespace string\s*gitops runtime namespace \(default "wego-system"\)\n\s*-v, --verbose\s*Enable verbose output`))
 		})
 	})
 })
