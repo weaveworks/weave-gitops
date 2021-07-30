46e0be51dd99bfd5dd62bf7b0cf30ff19c0e49bf
Jordan Pellizzari <jordan@weave.works>
2021-07-30 11:46:45 -0700

    f

---
+++ ui/index.js
@@ -0,0 +1,13 @@
+import _Timestamp from "./components/Timestamp.tsx";
+import _AppContextProvider from "./contexts/AppContext";
+import { Applications as appsClient } from "./lib/api/applications/applications.pb";
+import _Theme from "./lib/theme";
+import _ApplicationDetail from "./pages/ApplicationDetail";
+import _Applications from "./pages/Applications";
+
+export const Timestamp = _Timestamp;
+export const theme = _Theme;
+export const AppContextProvider = _AppContextProvider;
+export const Applications = _Applications;
+export const ApplicationDetail = _ApplicationDetail;
+export const applicationsClient = appsClient;
