040716af28d2371957905e104dfd5be3343b55dd
Justin Thompson <justin@weave.works>
2021-08-12 10:40:00 -0600

    calculate appHash instead of storing as label

--- pkg/utils/common.go
+++ pkg/utils/common.go
@@ -1,6 +1,8 @@
 package utils
 
 import (
+	"crypto/md5"
+	"encoding/hex"
 	"fmt"
 	"io"
 	"io/ioutil"
@@ -9,6 +11,7 @@ import (
 	"strings"
 	"time"
 
+	wego "github.com/weaveworks/weave-gitops/api/v1alpha1"
 	validation "k8s.io/apimachinery/pkg/api/validation"
 )
 
@@ -108,4 +111,35 @@ func SanitizeRepoUrl(url string) string {
 	}
 
 	return url
+}
+
+func GetAppHash(app wego.Application) (string, error) {
+	var appHash string
+	var err error
+
+	var getHash = func(inputs ...string) (string, error) {
+		h := md5.New()
+		final := ""
+		for _, input := range inputs {
+			final += input
+		}
+		_, err := h.Write([]byte(final))
+		if err != nil {
+			return "", fmt.Errorf("error generating app hash %s", err)
+		}
+		return hex.EncodeToString(h.Sum(nil)), nil
+	}
+
+	if app.Spec.DeploymentType == wego.DeploymentTypeHelm {
+		appHash, err = getHash(app.Spec.URL, app.Name, app.Spec.Branch)
+		if err != nil {
+			return "", err
+		}
+	} else {
+		appHash, err = getHash(app.Spec.URL, app.Spec.Path, app.Spec.Branch)
+		if err != nil {
+			return "", err
+		}
+	}
+	return "wego-" + appHash, nil
 }
