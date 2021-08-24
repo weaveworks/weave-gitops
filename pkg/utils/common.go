6b91a81d5442c307b697100559a274acfcebdde0
Jerry R. Jackson <jrryjcksn@gmail.com>
2021-08-24 16:35:46 -0600

    make wait tests reliable by abstracting out the time update (#659)
    Co-authored-by: Jerry Jackson <jerry@weave.works>

--- pkg/utils/common.go
+++ pkg/utils/common.go
@@ -35,14 +35,32 @@ func Exists(filePath string) bool {
 
 // WaitUntil runs checkDone until a timeout is reached
 func WaitUntil(out io.Writer, poll, timeout time.Duration, checkDone func() error) error {
-	for start := time.Now(); time.Since(start) < timeout; time.Sleep(poll) {
+	_, err := timedRepeat(
+		out,
+		time.Now(),
+		poll,
+		timeout,
+		func(currentTime time.Time) time.Time {
+			time.Sleep(poll)
+			return time.Now()
+		},
+		checkDone)
+	return err
+}
+
+// timedRepeat runs checkDone until a timeout is reached by updating the current time via a specified operation
+func timedRepeat(out io.Writer, start time.Time, poll, timeout time.Duration, updater func(currentTime time.Time) time.Time, checkDone func() error) (time.Time, error) {
+	currentTime := start
+	endTime := currentTime.Add(timeout)
+
+	for ; currentTime.Before(endTime); currentTime = updater(currentTime) {
 		err := checkDone()
 		if err == nil {
-			return nil
+			return currentTime, nil
 		}
 		fmt.Fprintf(out, "error occurred %s, retrying in %s\n", err, poll.String())
 	}
-	return fmt.Errorf("timeout reached %s", timeout.String())
+	return currentTime, fmt.Errorf("timeout reached %s", timeout.String())
 }
 
 type callback func()
