f8ec768ab70402bb4b27b57175d39b08917c5b44
Justin Thompson <justin@weave.works>
2021-08-25 11:56:09 -0600

    combine app and commit proto

--- ui/lib/api/applications/applications.pb.ts
+++ ui/lib/api/applications/applications.pb.ts
@@ -4,8 +4,16 @@
 * This file is a generated Typescript file for GRPC Gateway, DO NOT MODIFY
 */
 
-import * as Wego_serverV1Commits from "./commits.pb"
 import * as fm from "./fetch.pb"
+
+type Absent<T, K extends keyof T> = { [k in Exclude<keyof T, K>]?: undefined };
+type OneOf<T> =
+  | { [k in keyof T]?: undefined }
+  | (
+    keyof T extends infer K ?
+      (K extends string & keyof T ? { [k in K]: T[K] } & Absent<T, K>
+        : never)
+    : never);
 export type Condition = {
   type?: string
   status?: string
@@ -39,6 +47,28 @@ export type GetApplicationResponse = {
   application?: Application
 }
 
+export type Commit = {
+  commitHash?: string
+  date?: string
+  author?: string
+  message?: string
+}
+
+
+type BaseListCommitsRequest = {
+  name?: string
+  namespace?: string
+  pageSize?: number
+}
+
+export type ListCommitsRequest = BaseListCommitsRequest
+  & OneOf<{ pageToken: number }>
+
+export type ListCommitsResponse = {
+  commits?: Commit[]
+  nextPageToken?: number
+}
+
 export class Applications {
   static ListApplications(req: ListApplicationsRequest, initReq?: fm.InitReq): Promise<ListApplicationsResponse> {
     return fm.fetchReq<ListApplicationsRequest, ListApplicationsResponse>(`/v1/applications?${fm.renderURLSearchParams(req, [])}`, {...initReq, method: "GET"})
@@ -46,7 +76,7 @@ export class Applications {
   static GetApplication(req: GetApplicationRequest, initReq?: fm.InitReq): Promise<GetApplicationResponse> {
     return fm.fetchReq<GetApplicationRequest, GetApplicationResponse>(`/v1/applications/${req["name"]}?${fm.renderURLSearchParams(req, ["name"])}`, {...initReq, method: "GET"})
   }
-  static ListCommits(req: Wego_serverV1Commits.ListCommitsRequest, initReq?: fm.InitReq): Promise<Wego_serverV1Commits.ListCommitsResponse> {
-    return fm.fetchReq<Wego_serverV1Commits.ListCommitsRequest, Wego_serverV1Commits.ListCommitsResponse>(`/v1/applications/${req["name"]}/commits?${fm.renderURLSearchParams(req, ["name"])}`, {...initReq, method: "GET"})
+  static ListCommits(req: ListCommitsRequest, initReq?: fm.InitReq): Promise<ListCommitsResponse> {
+    return fm.fetchReq<ListCommitsRequest, ListCommitsResponse>(`/v1/applications/${req["name"]}/commits?${fm.renderURLSearchParams(req, ["name"])}`, {...initReq, method: "GET"})
   }
 }
