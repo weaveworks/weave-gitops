0b43e76d1b6ca5f3681cdb06c3b5f7e10cc01960
AlinaGoaga <goaga.alina@gmail.com>
2021-08-31 16:53:55 +0100

    add useApplications to the exports

--- ui/index.ts
+++ ui/index.ts
@@ -3,9 +3,11 @@ import { Applications as appsClient } from "./lib/api/applications/applications.
 import _Theme from "./lib/theme";
 import _ApplicationDetail from "./pages/ApplicationDetail";
 import _Applications from "./pages/Applications";
+import _useApplications from "./hooks/applications";
 
 export const theme = _Theme;
 export const AppContextProvider = _AppContextProvider;
 export const Applications = _Applications;
 export const ApplicationDetail = _ApplicationDetail;
 export const applicationsClient = appsClient;
+export const useApplications = _useApplications;
