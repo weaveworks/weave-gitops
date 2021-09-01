39f113440e8f44c9565b8af918a4bd3eebbbc4b8
AlinaGoaga <goaga.alina@gmail.com>
2021-09-01 14:01:49 +0100

    update loader style and add it to exports - clean up

--- ui/components/Page.tsx
+++ ui/components/Page.tsx
@@ -2,7 +2,7 @@ import { Breadcrumbs } from "@material-ui/core";
 import { Alert, AlertTitle } from "@material-ui/lab";
 import _ from "lodash";
 import React from "react";
-import styled, { css } from "styled-components";
+import styled from "styled-components";
 import useCommon from "../hooks/common";
 import { PageRoute } from "../lib/types";
 import { formatURL } from "../lib/utils";
