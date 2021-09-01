4601535eb1a1121941a19cd4b68b70bb348a4e0d
Luiz Filho <luizbafilho@gmail.com>
2021-09-01 15:18:46 -0300

    wip

--- pkg/middleware/middleware.go
+++ pkg/middleware/middleware.go
@@ -2,11 +2,14 @@ package middleware
 
 import (
 	"context"
+	"errors"
 	"net/http"
+	"strings"
 
 	"github.com/go-logr/logr"
 	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
 	"github.com/weaveworks/weave-gitops/pkg/logger"
+	"golang.org/x/oauth2"
 )
 
 type statusRecorder struct {
@@ -60,4 +63,55 @@ func WithLogging(log logr.Logger, mux *runtime.ServeMux) http.Handler {
 			l.V(logger.LogLevelError).Info(ServerErrorText)
 		}
 	})
+}
+
+type contextVals struct {
+	Token *oauth2.Token
+}
+
+type key int
+
+const tokenKey key = iota
+
+// Injects the token into the request context to be retrieved later.
+// Use the ExtractToken func inside the server handler where appropriate.
+func WithToken(h http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		tokenStr := r.Header.Get("Authorization")
+		// Token gets specified with the work "token", then the data.
+		// "token abc123def..."
+		tokenSlice := strings.Split(tokenStr, "token ")
+
+		if len(tokenSlice) < 2 {
+			// No token specified. Nothing to be done.
+			// We do NOT return 400 here because there may be some 'unauthenticated' routes (ie /login)
+			h.ServeHTTP(w, r)
+			return
+		}
+
+		// The actual token data
+		token := tokenSlice[1]
+
+		vals := contextVals{Token: &oauth2.Token{AccessToken: token}}
+
+		c := context.WithValue(r.Context(), tokenKey, vals)
+		r = r.WithContext(c)
+		h.ServeHTTP(w, r)
+	})
+}
+
+// Get the token from request context.
+func ExtractToken(ctx context.Context) (*oauth2.Token, error) {
+	c := ctx.Value(tokenKey)
+
+	vals, ok := c.(contextVals)
+	if !ok {
+		return nil, errors.New("could not get token from context")
+	}
+
+	if vals.Token == nil || vals.Token.AccessToken == "" {
+		return nil, errors.New("no token specified")
+	}
+
+	return vals.Token, nil
 }
