4ac880cea71bf479fc158d5371dfd1ef0fcdc9a7
josecordaz <josecordaz@gmail.com>
2021-09-09 13:16:42 -0700

    Added yaml files for running wego-app in the cluster (#715)
    * Building docker image
    
    * tweak config
    
    * setting up wego-app as image name
    
    * removing blank lines
    
    * Removing unecessary dependencies
    
    * Added yaml files for running wego-app in the cluster
    
    * Fixed wego-app deployment image
    
    * Fixed serviceAccountName on wego-app deployment
    
    * Added args to wego-app deployment to start with ui run command
    
    - Using 9001 port instead
    - Fixes on groups in role.yaml
    
    * Added go templating with unit tests
    
    - Replace []byte with nil
    
    * Add InCluster configuration
    
    * Fixed lint issues
    
    * Fixed app crd unit tests
    
    * Removed extra err declaration and using yaml unmarshal instead to be more precise in the assertion of version.Version
    
    * Removed extra npx
    
    * Removed extra npx
    
    * Using appsv1.Deployment instead of custom struct
    
    * Increased INSTALL_PODS_READY_TIMEOUT to 4 minutes
    
    * Handle error on `template.Parse`
    
    * Reverting install pods ready timeout to 3min
    
    - Ignore wego-app on kubectl wait
    
    * Do not skip wego-app when waiting for wego controllers
    
    * Using an array of manifests instead
    
    * Added specific manifest type checks in unit tests
    
    - Using format N * time.TIME
    - Removed extra line
    
    Co-authored-by: Luiz Filho <luizbafilho@gmail.com>

--- manifests/manifests.go
+++ manifests/manifests.go
@@ -1,8 +1,69 @@
 package manifests
 
 import (
+	"bytes"
 	_ "embed"
+	"fmt"
+	"text/template"
+
+	"github.com/pkg/errors"
+
+	"github.com/weaveworks/weave-gitops/cmd/wego/version"
 )
+
+var Manifests [][]byte
 
 //go:embed crds/wego.weave.works_apps.yaml
 var AppCRD []byte
+
+//go:embed wego-app/deployment.yaml
+var WegoAppDeployment []byte
+
+type deploymentParameters struct {
+	Version string
+}
+
+var errInjectingValuesToTemplate = errors.New("error injecting values to template")
+
+// GenerateWegoAppDeploymentManifest generates wego-app deployment manifest from a template
+func GenerateWegoAppDeploymentManifest(deploymentTemplate []byte) ([]byte, error) {
+
+	deploymentValues := deploymentParameters{version.Version}
+
+	template := template.New("DeploymentTemplate")
+	var err error
+	template, err = template.Parse(string(deploymentTemplate))
+	if err != nil {
+		return nil, fmt.Errorf("error parsing template %w", err)
+	}
+
+	deploymentYaml := &bytes.Buffer{}
+	err = template.Execute(deploymentYaml, deploymentValues)
+	if err != nil {
+		return nil, fmt.Errorf("%s %w", errInjectingValuesToTemplate, err)
+	}
+
+	return deploymentYaml.Bytes(), nil
+}
+
+//go:embed wego-app/service-account.yaml
+var WegoAppServiceAccount []byte
+
+//go:embed wego-app/service.yaml
+var WegoAppService []byte
+
+//go:embed wego-app/role.yaml
+var WegoAppRole []byte
+
+//go:embed wego-app/role-binding.yaml
+var WegoAppRoleBinding []byte
+
+func init() {
+	Manifests = [][]byte{
+		AppCRD,
+		WegoAppServiceAccount,
+		WegoAppRoleBinding,
+		WegoAppRole,
+		WegoAppService,
+	}
+}
