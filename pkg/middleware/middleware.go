5df3478fab3a4a53dc01b07be57fe4306f1fad7f
Luiz Filho <luizbafilho@gmail.com>
2021-09-09 14:52:47 -0300

    JWT middleware (#735)

--- pkg/middleware/middleware.go
+++ pkg/middleware/middleware.go
@@ -2,11 +2,16 @@ package middleware
 
 import (
 	"context"
+	"errors"
+	"fmt"
 	"net/http"
+	"strings"
 
 	"github.com/go-logr/logr"
 	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
 	"github.com/weaveworks/weave-gitops/pkg/logger"
+	"github.com/weaveworks/weave-gitops/pkg/services/auth"
+	"golang.org/x/oauth2"
 )
 
 type statusRecorder struct {
@@ -60,4 +65,59 @@ func WithLogging(log logr.Logger, mux *runtime.ServeMux) http.Handler {
 			l.V(logger.LogLevelError).Info(ServerErrorText)
 		}
 	})
+}
+
+type contextVals struct {
+	ProviderToken *oauth2.Token
+}
+
+type key int
+
+const tokenKey key = iota
+
+// Injects the token into the request context to be retrieved later.
+// Use the ExtractToken func inside the server handler where appropriate.
+func WithProviderToken(jwtClient auth.JWTClient, h http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		tokenStr := r.Header.Get("Authorization")
+		tokenSlice := strings.Split(tokenStr, "token ")
+
+		if len(tokenSlice) < 2 {
+			// No token specified. Nothing to be done.
+			// We do NOT return 400 here because there may be some 'unauthenticated' routes (ie /login)
+			h.ServeHTTP(w, r)
+			return
+		}
+
+		// The actual token data
+		token := tokenSlice[1]
+
+		claims, err := jwtClient.VerifyJWT(token)
+		if err != nil {
+			http.Error(w, fmt.Errorf("failed verifying JWT token: %w", err).Error(), http.StatusUnauthorized)
+			return
+		}
+
+		vals := contextVals{ProviderToken: &oauth2.Token{AccessToken: claims.ProviderToken}}
+
+		c := context.WithValue(r.Context(), tokenKey, vals)
+		r = r.WithContext(c)
+		h.ServeHTTP(w, r)
+	})
+}
+
+// Get the token from request context.
+func ExtractProviderToken(ctx context.Context) (*oauth2.Token, error) {
+	c := ctx.Value(tokenKey)
+
+	vals, ok := c.(contextVals)
+	if !ok {
+		return nil, errors.New("could not get token from context")
+	}
+
+	if vals.ProviderToken == nil || vals.ProviderToken.AccessToken == "" {
+		return nil, errors.New("no token specified")
+	}
+
+	return vals.ProviderToken, nil
 }
