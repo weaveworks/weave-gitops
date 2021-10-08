dd46dec798be360315adb98f9955982c60162847
Mark Emeis <mark.emeis@weave.works>
2021-10-08 08:14:54 -0600

    Update beta command documentation

--- cmd/gitops/beta/cmd/add.go
+++ cmd/gitops/beta/cmd/add.go
@@ -5,8 +5,6 @@ This file is part of the Weave GitOps CLI.
 package cmd
 
 import (
-	"fmt"
-
 	"github.com/spf13/cobra"
 	"github.com/weaveworks/weave-gitops/cmd/gitops/beta/cmd/add"
 )
@@ -14,16 +12,7 @@ import (
 // addCmd represents the add command
 var addCmd = &cobra.Command{
 	Use:   "add",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
-	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("add called")
-	},
+	Short: "This is for adding content to the GitOps repo",
 }
 
 func init() {
