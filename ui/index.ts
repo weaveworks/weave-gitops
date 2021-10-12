ad66510f252b9bfae8333beda9b17fbcd9f8613a
Simon <footless@gmail.com>
2021-10-12 18:01:42 +0200

    Export the ApplicationAdd page from the npm module (#870)
    - For consuming libs

--- ui/index.ts
+++ ui/index.ts
@@ -4,12 +4,14 @@ import _useApplications from "./hooks/applications";
 import { Applications as appsClient } from "./lib/api/applications/applications.pb";
 import _Theme from "./lib/theme";
 import _ApplicationDetail from "./pages/ApplicationDetail";
+import _ApplicationAdd from "./pages/ApplicationAdd";
 import _Applications from "./pages/Applications";
 
 export const theme = _Theme;
 export const AppContextProvider = _AppContextProvider;
 export const Applications = _Applications;
 export const ApplicationDetail = _ApplicationDetail;
+export const ApplicationAdd = _ApplicationAdd;
 export const applicationsClient = appsClient;
 export const useApplications = _useApplications;
 export const LoadingPage = _LoadingPage;
