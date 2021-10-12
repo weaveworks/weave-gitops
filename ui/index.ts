44e6c461d0a040c6f6f97393182c323a6c7bf71c
Simon Howe <footless@gmail.com>
2021-10-12 18:02:24 +0200

    Re-format exports

--- ui/index.ts
+++ ui/index.ts
@@ -1,23 +1,23 @@
-import _LoadingPage from "./components/LoadingPage";
-import _AppContextProvider from "./contexts/AppContext";
-import _useApplications from "./hooks/applications";
-import { Applications as appsClient } from "./lib/api/applications/applications.pb";
-import _Theme from "./lib/theme";
-import _ApplicationDetail from "./pages/ApplicationDetail";
-import _ApplicationAdd from "./pages/ApplicationAdd";
-import _Applications from "./pages/Applications";
-import _GithubDeviceAuthModal from "./components/GithubDeviceAuthModal";
-import { getProviderToken as _getProviderToken } from "./lib/utils";
+import { Applications as applicationsClient } from "./lib/api/applications/applications.pb";
+import { getProviderToken } from "./lib/utils";
+import AppContextProvider from "./contexts/AppContext";
+import ApplicationAdd from "./pages/ApplicationAdd";
+import ApplicationDetail from "./pages/ApplicationDetail";
+import Applications from "./pages/Applications";
+import GithubDeviceAuthModal from "./components/GithubDeviceAuthModal";
+import LoadingPage from "./components/LoadingPage";
+import theme from "./lib/theme";
+import useApplications from "./hooks/applications";
 
-export const theme = _Theme;
-export const AppContextProvider = _AppContextProvider;
-export const Applications = _Applications;
-export const ApplicationDetail = _ApplicationDetail;
-export const ApplicationAdd = _ApplicationAdd;
-export const applicationsClient = appsClient;
-export const useApplications = _useApplications;
-export const LoadingPage = _LoadingPage;
-
-// Authy things
-export const GithubDeviceAuthModal = _GithubDeviceAuthModal;
-export const getProviderToken = _getProviderToken;
+export {
+  AppContextProvider,
+  ApplicationAdd,
+  ApplicationDetail,
+  Applications,
+  applicationsClient,
+  getProviderToken,
+  GithubDeviceAuthModal,
+  LoadingPage,
+  theme,
+  useApplications,
+};
