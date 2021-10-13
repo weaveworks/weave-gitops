cddd1e566187cc6ff4170e30216a553796ee216a
Jerry Jackson <jrryjcksn@gmail.com>
2021-10-13 09:20:41 -0600

    fix lint errors in doc command

--- cmd/gitops/docs/cmd.go
+++ cmd/gitops/docs/cmd.go
@@ -7,7 +7,7 @@ import (
 
 var (
 	docDir  string
-	docFunc func()
+	docFunc func() error
 )
 
 var Cmd = &cobra.Command{
@@ -15,18 +15,19 @@ var Cmd = &cobra.Command{
 	Short:   "Generate hard copy of CLI documentation",
 	Example: "gitops docs",
 	Hidden:  true,
-	Run:     runCmd,
+	RunE:    runCmd,
 }
 
-func runCmd(cmd *cobra.Command, args []string) {
-	docFunc()
+func runCmd(cmd *cobra.Command, args []string) error {
+	return docFunc()
 }
 
 func init() {
 	Cmd.Flags().StringVarP(&docDir, "directory", "d", ".", "directory in which to place generated documents")
+
 	docFunc = genDocs
 }
 
-func genDocs() {
-	doc.GenMarkdownTree(Cmd.Root(), docDir)
+func genDocs() error {
+	return doc.GenMarkdownTree(Cmd.Root(), docDir)
 }
