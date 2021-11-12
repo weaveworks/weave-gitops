a035576e89cbc671a808286d442459472d62654f
Sara El-Zayat <st.elzayat@gmail.com>
2021-11-12 16:29:36 +0200

    Refactor

--- pkg/upgrade/upgrade.go
+++ pkg/upgrade/upgrade.go
@@ -6,6 +6,7 @@ import (
 	"errors"
 	"fmt"
 	"io"
+	"os"
 	"path/filepath"
 	"strings"
 	"time"
@@ -18,11 +19,15 @@ import (
 	"github.com/weaveworks/pctl/pkg/catalog"
 	pctl_git "github.com/weaveworks/pctl/pkg/git"
 	"github.com/weaveworks/pctl/pkg/install"
+	"github.com/weaveworks/weave-gitops/cmd/internal"
+	"github.com/weaveworks/weave-gitops/pkg/flux"
 	"github.com/weaveworks/weave-gitops/pkg/git"
 	"github.com/weaveworks/weave-gitops/pkg/git/wrapper"
 	"github.com/weaveworks/weave-gitops/pkg/gitproviders"
 	"github.com/weaveworks/weave-gitops/pkg/logger"
 	"github.com/weaveworks/weave-gitops/pkg/osys"
+	"github.com/weaveworks/weave-gitops/pkg/runner"
+	"github.com/weaveworks/weave-gitops/pkg/services"
 	"github.com/weaveworks/weave-gitops/pkg/services/auth"
 	corev1 "k8s.io/api/core/v1"
 	v1 "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1"
@@ -74,7 +79,7 @@ func Upgrade(ctx context.Context, upgradeValues UpgradeValues, w io.Writer) erro
 
 	gitClient := git.New(nil, wrapper.NewGoGit())
 
-	return upgrade(ctx, upgradeValues, gitClient, kubeClient, auth.InitGitProvider, w)
+	return upgrade(ctx, upgradeValues, gitClient, kubeClient, w)
 }
 
 type InitGitProvider func(
@@ -85,7 +90,7 @@ type InitGitProvider func(
 	getAccountType gitproviders.AccountTypeGetter,
 ) (gitproviders.GitProvider, error)
 
-func upgrade(ctx context.Context, upgradeValues UpgradeValues, gitClient git.Git, kubeClient client.Client, initGitProvider InitGitProvider, w io.Writer) error {
+func upgrade(ctx context.Context, upgradeValues UpgradeValues, gitClient git.Git, kubeClient client.Client, w io.Writer) error {
 	uv, err := buildUpgradeConfigs(ctx, upgradeValues, kubeClient, gitClient, w)
 	if err != nil {
 		return fmt.Errorf("failed to build upgrade configs: %v", err)
@@ -113,17 +118,32 @@ func upgrade(ctx context.Context, upgradeValues UpgradeValues, gitClient git.Git
 		return fmt.Errorf("failed to normalize URL %q: %w", uv.RepoURL, err)
 	}
 
-	authHandler, err := auth.NewAuthCLIHandler(normalizedURL.Provider())
-	if err != nil {
-		return fmt.Errorf("error initializing cli auth handler: %w", err)
-	}
+	// authHandler, err := auth.NewAuthCLIHandler(normalizedURL.Provider())
+	// if err != nil {
+	// 	return fmt.Errorf("error initializing cli auth handler: %w", err)
+	// }
 
 	osysClient := osys.New()
-	logger := logger.NewCLILogger(osysClient.Stdout())
+	logger := internal.NewCLILogger(osysClient.Stdout())
 
-	gitProvider, err := initGitProvider(normalizedURL, osysClient, logger, authHandler, gitproviders.GetAccountType)
+	// gitProvider, err := initGitProvider(normalizedURL, osysClient, logger, authHandler, gitproviders.GetAccountType)
+	// if err != nil {
+	// 	return fmt.Errorf("error obtaining git provider token: %w", err)
+	// }
+
+	// WIP
+	fluxClient := flux.New(osys.New(), &runner.CLIRunner{})
+	factory := services.NewFactory(fluxClient, logger)
+
+	providerClient := internal.NewGitProviderClient(os.Stdout, os.LookupEnv, auth.NewAuthCLIHandler, logger)
+
+	_, gitProvider, err := factory.GetGitClients(ctx, providerClient, services.GitConfigParams{
+		URL:       uv.RepoURL,
+		Namespace: uv.Namespace,
+		DryRun:    uv.DryRun,
+	})
 	if err != nil {
-		return fmt.Errorf("error obtaining git provider token: %w", err)
+		return fmt.Errorf("failed to get git clients: %w", err)
 	}
 
 	path := "helmrelease.yaml"
