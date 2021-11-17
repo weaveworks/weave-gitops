ca5bcb8600a00549589dba8a9cc11ec2ba843768
Jordan Pellizzari <jordan@weave.works>
2021-11-17 10:14:37 -0800

    Fix act() warning in ApplicationDetail test

--- ui/pages/__tests__/ApplicationDetail.test.tsx
+++ ui/pages/__tests__/ApplicationDetail.test.tsx
@@ -1,4 +1,4 @@
-import { fireEvent, render, screen } from "@testing-library/react";
+import { fireEvent, render, screen, waitFor } from "@testing-library/react";
 import "jest-styled-components";
 import * as React from "react";
 import { act } from "react-dom/test-utils";
@@ -40,48 +40,56 @@ describe("ApplicationDetail", () => {
       expect(await screen.findByText("Sync App")).toBeTruthy();
     });
     it("should call a sync request", async () => {
+      const promise = Promise.resolve();
       const capture = jest.fn();
+
+      const apiMock = {
+        GetApplication: () => ({
+          application: {
+            name: "pod-info",
+            namespace: "wego-systems",
+          },
+        }),
+        ListCommits: (): ListCommitsResponse => ({
+          commits: [
+            {
+              hash: "123abc",
+              author: "Example User",
+              date: "2021-09-10T23:45:09Z",
+            },
+          ],
+        }),
+        SyncApplication: (req): SyncApplicationResponse => {
+          capture(req);
+          return {
+            success: true,
+          };
+        },
+      };
+
       await act(async () => {
         render(
           withTheme(
             withContext(
               <ApplicationDetail name="pod-info" />,
               "/application_detail",
-              {
-                GetApplication: () => ({
-                  application: {
-                    name: "pod-info",
-                    namespace: "wego-systems",
-                  },
-                }),
-                ListCommits: (): ListCommitsResponse => ({
-                  commits: [
-                    {
-                      hash: "123abc",
-                      author: "Example User",
-                      date: "2021-09-10T23:45:09Z",
-                    },
-                  ],
-                }),
-                SyncApplication: (req): SyncApplicationResponse => {
-                  capture(req);
-                  return {
-                    success: true,
-                  };
-                },
-              }
+              apiMock
             )
           )
         );
       });
+
       const button = await (
         await screen.findByText("Sync App")
       ).closest("button");
+
       fireEvent(button, new MouseEvent("click", { bubbles: true }));
+
       expect(capture).toHaveBeenCalledWith({
         name: "pod-info",
         namespace: "wego-systems",
       });
+      await waitFor(() => promise);
     });
     // it("should notify user on success", () => "");
     // it("should notify user on failure", () => "");
