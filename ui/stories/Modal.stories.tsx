87845dfc984d6d9d53959996ef6c640ecf4e8a56
Joshua Israel <joshua.israel@weave.works>
2021-11-24 12:30:54 -0500

    transitioned Modal and Modal.stories to new format

--- ui/stories/Modal.stories.tsx
+++ ui/stories/Modal.stories.tsx
@@ -1,47 +1,26 @@
-import { Meta } from "@storybook/react";
+import { Meta, Story } from "@storybook/react";
 import React from "react";
 import Button from "../components/Button";
-import Modal from "../components/Modal";
+import { Modal, Props } from "../components/Modal";
 
 export default {
   title: "Modal",
   component: Modal,
-  argTypes: {
-    title: {
-      description:
-        //PUT IN LINK TO MUI MODAL
-        "`<h2 />` element appearing at the top of the modal",
-      defaultValue: "Title",
-      type: { summary: "string", name: "string", required: false },
-    },
-    description: {
-      description: "`<p />` element appearing below title",
-      defaultValue: "Description",
-      type: { summary: "string", name: "string", required: false },
-    },
-    className: {
-      description: "CSS MUI Overrides or other styling",
-      type: { summary: "string", name: "string", required: false },
-    },
-    bodyClassName: {
-      description: "CSS MUI Overrides or other styling for Modal children",
-      type: { summary: "string", name: "string", required: false },
-    },
-    children: {
-      description: "React Nodes to make up Modal body",
+  parameters: {
+    docs: {
+      description: {
+        component: "MUI Modal - see https://mui.com/components/modal/",
+      },
     },
   },
 } as Meta;
 
-export const Default: React.VFC<{
-  title: "Title";
-  description: "description";
-}> = (args) => {
+const Template: Story<Props> = (args) => {
   const [open, setOpen] = React.useState(false);
   return (
     <div>
       {open ? (
-        <Modal open={open} onClose={() => setOpen(false)} {...args}>
+        <Modal {...args} open={open} onClose={() => setOpen(false)}>
           <div>This is the Modal body!</div>
         </Modal>
       ) : (
@@ -50,3 +29,5 @@ export const Default: React.VFC<{
     </div>
   );
 };
+export const Default = Template.bind({});
+Default.args = { title: "Title", description: "Description" };
