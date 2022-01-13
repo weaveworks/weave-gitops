303d8e7b564b46b103c4befc4da8558ed2d29043
Jose C Ordaz <josecordaz@gmail.com>
2022-01-13 11:08:51 -0800

    Fixed issue where it wasn't showing proper text if flux was not installed

--- pkg/services/check/check.go
+++ pkg/services/check/check.go
@@ -1,124 +1,120 @@
 package check
 
 import (
-	"context"
-	"errors"
-	"fmt"
-	"strings"
+    "context"
+    "errors"
+    "fmt"
+    "strings"
 
-	"github.com/Masterminds/semver/v3"
-	"github.com/weaveworks/weave-gitops/pkg/flux"
-	"github.com/weaveworks/weave-gitops/pkg/kube"
+    "github.com/Masterminds/semver/v3"
+    "github.com/weaveworks/weave-gitops/pkg/flux"
+    "github.com/weaveworks/weave-gitops/pkg/kube"
 )
 
-var ErrFluxNotFound = errors.New("flux is not installed")
 var ErrKubernetesNotFound = errors.New("no kubernetes version found")
 
 const (
-	FluxCompatibleMessage    = "Current flux version is compatible"
-	FluxNotCompatibleMessage = "Current flux version is not compatible"
+    FluxCompatibleMessage    = "Current flux version is compatible"
+    FluxNotCompatibleMessage = "Current flux version is not compatible"
 )
 
 // Pre runs pre-install checks
 func Pre(ctx context.Context, kubeClient kube.Kube, fluxClient flux.Flux, expectedFluxVersion string) (string, error) {
-	output := ""
+    output := ""
 
-	k8sOutput, err := runKubernetesCheck(fluxClient)
-	if err != nil {
-		return "", err
-	}
+    k8sOutput, err := runKubernetesCheck(fluxClient)
+    if err != nil {
+        return "", err
+    }
 
-	output += k8sOutput + "\n"
+    output += k8sOutput + "\n"
 
-	currentFluxVersion, err := getCurrentFluxVersion(ctx, kubeClient)
-	if err != nil {
-		if errors.Is(err, ErrFluxNotFound) {
-			output += "✔ Flux is not installed"
-			return output, nil
-		}
+    currentFluxVersion, err := getCurrentFluxVersion(ctx, kubeClient)
+    if err != nil {
+        if errors.Is(err, kube.ErrNamespaceNotFound) {
+            output += "✔ Flux is not installed"
+            return output, nil
+        }
 
-		return "", fmt.Errorf("failed getting installed flux version: %w", err)
-	}
+        return "", fmt.Errorf("failed getting installed flux version: %w", err)
+    }
 
-	fluxOutput, err := validateFluxVersion(currentFluxVersion, expectedFluxVersion)
-	if err != nil {
-		return "", err
-	}
+    fluxOutput, err := validateFluxVersion(currentFluxVersion, expectedFluxVersion)
+    if err != nil {
+        return "", err
+    }
 
-	output += fluxOutput
+    output += fluxOutput
 
-	return output, nil
+    return output, nil
 }
 
 func runKubernetesCheck(fluxClient flux.Flux) (string, error) {
-	output, err := fluxClient.PreCheck()
-	if err != nil {
-		return "", fmt.Errorf("failed running flux pre check %w", err)
-	}
+    output, err := fluxClient.PreCheck()
+    if err != nil {
+        return "", fmt.Errorf("failed running flux pre check %w", err)
+    }
 
-	lines := strings.Split(output, "\n")
-	for _, line := range lines {
-		if strings.Contains(line, "Kubernetes") {
-			return line, nil
-		}
-	}
+    lines := strings.Split(output, "\n")
+    for _, line := range lines {
+        if strings.Contains(line, "Kubernetes") {
+            return line, nil
+        }
+    }
 
-	return "", ErrKubernetesNotFound
+    return "", ErrKubernetesNotFound
 }
 
 func getCurrentFluxVersion(ctx context.Context, kubeClient kube.Kube) (string, error) {
-	namespace, err := kubeClient.FetchNamespaceWithLabel(ctx, flux.PartOfLabelKey, flux.PartOfLabelValue)
-	if err != nil {
-		return "", err
-	}
+    namespace, err := kubeClient.FetchNamespaceWithLabel(ctx, flux.PartOfLabelKey, flux.PartOfLabelValue)
+    if err != nil {
+        return "", err
+    }
 
-	labels := namespace.GetLabels()
-	if labels[flux.PartOfLabelKey] == flux.PartOfLabelValue {
-		return labels[flux.VersionLabelKey], nil
-	}
+    labels := namespace.GetLabels()
 
-	return "", ErrFluxNotFound
+    return labels[flux.VersionLabelKey], nil
 }
 
 func validateFluxVersion(actualFluxVersion string, expectedFluxVersion string) (string, error) {
-	actualParsedFluxVersion, err := parseVersion(actualFluxVersion)
-	if err != nil {
-		return "", err
-	}
+    actualParsedFluxVersion, err := parseVersion(actualFluxVersion)
+    if err != nil {
+        return "", err
+    }
 
-	expectedParsedFluxVersion, err := parseVersion(expectedFluxVersion)
-	if err != nil {
-		return "", err
-	}
+    expectedParsedFluxVersion, err := parseVersion(expectedFluxVersion)
+    if err != nil {
+        return "", err
+    }
 
-	fluxOutput := ""
+    fluxOutput := ""
 
-	expectedMajor := expectedParsedFluxVersion.Major()
-	expectedMinor := expectedParsedFluxVersion.Minor()
-	constraintFormat := fmt.Sprintf("~%d.%d.x", expectedMajor, expectedMinor)
+    expectedMajor := expectedParsedFluxVersion.Major()
+    expectedMinor := expectedParsedFluxVersion.Minor()
+    constraintFormat := fmt.Sprintf("~%d.%d.x", expectedMajor, expectedMinor)
 
-	constraint, err := semver.NewConstraint(constraintFormat)
-	if err != nil {
-		return "", fmt.Errorf("failed creating semver constraint: %w", err)
-	}
+    constraint, err := semver.NewConstraint(constraintFormat)
+    if err != nil {
+        return "", fmt.Errorf("failed creating semver constraint: %w", err)
+    }
 
-	check := constraint.Check(actualParsedFluxVersion)
-	if check {
-		fluxOutput += fmt.Sprintf("✔ Flux %s ~=%s\n", actualParsedFluxVersion, expectedParsedFluxVersion)
-		fluxOutput += FluxCompatibleMessage
-	} else {
-		fluxOutput += fmt.Sprintf("✗ Flux %s !=%s\n", actualParsedFluxVersion, constraintFormat)
-		fluxOutput += FluxNotCompatibleMessage
-	}
+    check := constraint.Check(actualParsedFluxVersion)
+    if check {
+        fluxOutput += fmt.Sprintf("✔ Flux %s ~=%s\n", actualParsedFluxVersion, expectedParsedFluxVersion)
+        fluxOutput += FluxCompatibleMessage
+    } else {
+        fluxOutput += fmt.Sprintf("✗ Flux %s !=%s\n", actualParsedFluxVersion, constraintFormat)
+        fluxOutput += FluxNotCompatibleMessage
+    }
 
-	return fluxOutput, nil
+    return fluxOutput, nil
 }
 
 func parseVersion(version string) (*semver.Version, error) {
-	versionLessV := strings.TrimPrefix(version, "v")
-	if _, err := semver.StrictNewVersion(versionLessV); err != nil {
-		return nil, err
-	}
+    versionLessV := strings.TrimPrefix(version, "v")
+    if _, err := semver.StrictNewVersion(versionLessV); err != nil {
+        return nil, err
+    }
 
-	return semver.NewVersion(version)
+    return semver.NewVersion(version)
 }
