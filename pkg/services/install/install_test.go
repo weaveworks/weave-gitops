77237754d5f2a89a74e67ba1a7432cd69418977f
Jose C Ordaz <josecordaz@gmail.com>
2022-01-14 09:36:29 -0800

    Added success path for install when auto-merge is false

--- pkg/services/install/install_test.go
+++ pkg/services/install/install_test.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"context"
 	"errors"
+	"fmt"
 
 	"github.com/weaveworks/weave-gitops/pkg/git"
 	"gopkg.in/yaml.v2"
@@ -287,7 +288,153 @@ var _ = Describe("Installer", func() {
 			Expect(err).ShouldNot(HaveOccurred())
 		})
 		It("should succeed with auto-merge=false", func() {
+			fakeKubeClient.GetClusterNameReturns(clusterName, nil)
 
+			fakeFluxClient.InstallReturns(nil, nil)
+
+			fakeKubeClient.ApplyReturns(nil)
+
+			privateVisibility := gitprovider.RepositoryVisibilityPrivate
+			fakeGitProvider.GetRepoVisibilityReturns(&privateVisibility, nil)
+
+			fakeGitProvider.GetDefaultBranchReturns("main", nil)
+
+			fakeGitProvider.CreatePullRequestReturns(nil, nil)
+
+			runtimeManifests := []byte("runtime-manifests")
+			fakeFluxClient.InstallReturns(runtimeManifests, nil)
+
+			wegoAppManifests, err := manifests.GenerateWegoAppManifests(manifests.Params{AppVersion: "v0.0.0", Namespace: testNamespace})
+			Expect(err).ShouldNot(HaveOccurred())
+
+			wegoAppManifest := bytes.Join(wegoAppManifests, []byte("---\n"))
+
+			systemKustomizationResource := []byte("system kustomization resource")
+			fakeFluxClient.CreateKustomizationReturnsOnCall(0, systemKustomizationResource, nil)
+			userKustomizationResource := []byte("user kustomization resource")
+			fakeFluxClient.CreateKustomizationReturnsOnCall(1, userKustomizationResource, nil)
+
+			fakeFluxClient.CreateKustomizationReturnsOnCall(2, systemKustomizationResource, nil)
+			fakeFluxClient.CreateKustomizationReturnsOnCall(3, userKustomizationResource, nil)
+
+			gitopsConfigMap, err := models.GitopsConfigMap(testNamespace, testNamespace)
+			Expect(err).ShouldNot(HaveOccurred())
+
+			wegoConfigManifest, err := yaml.Marshal(gitopsConfigMap)
+			Expect(err).ShouldNot(HaveOccurred())
+
+			systemKustomization := models.CreateKustomize(clusterName, testNamespace, models.RuntimePath, models.SourcePath, models.SystemKustResourcePath, models.UserKustResourcePath)
+
+			systemKustomizationManifest, err := yaml.Marshal(systemKustomization)
+			Expect(err).ShouldNot(HaveOccurred())
+
+			gitSource := []byte("git source")
+			fakeFluxClient.CreateSourceGitReturns(gitSource, nil)
+
+			expectedManifests := []models.Manifest{
+				{
+					Path:    git.GetSystemQualifiedPath(clusterName, models.AppCRDPath),
+					Content: manifests.AppCRD,
+				},
+				{
+					Path:    git.GetSystemQualifiedPath(clusterName, models.RuntimePath),
+					Content: runtimeManifests,
+				},
+				{
+					Path:    git.GetSystemQualifiedPath(clusterName, models.SystemKustResourcePath),
+					Content: systemKustomizationResource,
+				},
+				{
+					Path:    git.GetSystemQualifiedPath(clusterName, models.UserKustResourcePath),
+					Content: userKustomizationResource,
+				},
+				{
+					Path:    git.GetSystemQualifiedPath(clusterName, models.WegoAppPath),
+					Content: wegoAppManifest,
+				},
+				{
+					Path:    git.GetSystemQualifiedPath(clusterName, models.WegoConfigPath),
+					Content: wegoConfigManifest,
+				},
+				{
+					Path:    git.GetSystemQualifiedPath(clusterName, models.SystemKustomizationPath),
+					Content: systemKustomizationManifest,
+				},
+				{
+					Path:    git.GetSystemQualifiedPath(clusterName, models.SourcePath),
+					Content: gitSource,
+				},
+			}
+
+			applyIndex := 0
+			fakeKubeClient.ApplyCalls(func(ctx context.Context, manifest []byte, namespace string) error {
+
+				if applyIndex <= 6 {
+
+					Expect(namespace).Should(Equal(namespace))
+
+					if applyIndex != 0 {
+						partOfPreviousManifest := bytes.Contains(expectedManifests[applyIndex-1].Content, manifest)
+						if partOfPreviousManifest {
+							Expect(string(expectedManifests[applyIndex-1].Content)).Should(ContainSubstring(string(manifest)))
+							return nil
+						}
+					}
+
+					Expect(string(expectedManifests[applyIndex].Content)).Should(ContainSubstring(string(manifest)))
+
+					applyIndex++
+				}
+				return nil
+			})
+
+			expectedPullRequestInfo := gitproviders.PullRequestInfo{
+				Title:         fmt.Sprintf("GitOps associate %s", clusterName),
+				Description:   fmt.Sprintf("Add gitops automation manifests for cluster %s", clusterName),
+				CommitMessage: gitopswriter.ClusterCommitMessage,
+				NewBranch:     models.GetClusterHash(clusterName),
+				TargetBranch:  "main",
+				Files:         models.ConvertManifestsToCommitFiles(expectedManifests),
+			}
+
+			fakeGitProvider.CreatePullRequestCalls(func(ctx context.Context, url gitproviders.RepoURL, info gitproviders.PullRequestInfo) (gitprovider.PullRequest, error) {
+				Expect(url).To(Equal(configRepo))
+				Expect(info.Title).To(Equal(expectedPullRequestInfo.Title))
+				Expect(info.Description).To(Equal(expectedPullRequestInfo.Description))
+				Expect(info.CommitMessage).To(Equal(expectedPullRequestInfo.CommitMessage))
+				Expect(info.NewBranch).To(Equal(expectedPullRequestInfo.NewBranch))
+				Expect(info.TargetBranch).To(Equal(expectedPullRequestInfo.TargetBranch))
+				for ind, manifest := range info.Files {
+					Expect(*manifest.Path).Should(Equal(expectedManifests[ind].Path))
+					Expect(*manifest.Content).Should(Equal(string(expectedManifests[ind].Content)))
+				}
+				return NewFakePullRequest("test", "test", 1), nil
+			})
+
+			err = installer.Install(testNamespace, configRepo, false)
+			Expect(err).ShouldNot(HaveOccurred())
 		})
 	})
 })
+
+type fakePullRequest struct {
+	pullRequestInfo gitprovider.PullRequestInfo
+}
+
+func (fc *fakePullRequest) APIObject() interface{} {
+	return &fc.pullRequestInfo
+}
+
+func (fc *fakePullRequest) Get() gitprovider.PullRequestInfo {
+	return fc.pullRequestInfo
+}
+
+func NewFakePullRequest(owner string, repoName string, prNumber int) gitprovider.PullRequest {
+	return &fakePullRequest{
+		pullRequestInfo: gitprovider.PullRequestInfo{
+			WebURL: fmt.Sprintf("https://github.com/%s/%s/pull/%d", owner, repoName, prNumber),
+			Merged: false,
+			Number: 1,
+		},
+	}
+}
