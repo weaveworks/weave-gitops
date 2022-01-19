b4edd8f2b810ee4888b5015359b46f6e5d854803
Jose C Ordaz <josecordaz@gmail.com>
2022-01-19 14:08:57 -0800

    Fixed references to old function
    A function was renamed but their calls were not

--- pkg/services/install/install_test.go
+++ pkg/services/install/install_test.go
@@ -285,7 +285,7 @@ var _ = Describe("Installer", func() {
 			wegoConfigManifest, err := yaml.Marshal(gitopsConfigMap)
 			Expect(err).ShouldNot(HaveOccurred())
 
-			systemKustomization := models.CreateKustomize(clusterName, testNamespace, models.RuntimePath, models.SourcePath, models.SystemKustResourcePath, models.UserKustResourcePath, models.WegoAppPath)
+			systemKustomization := models.CreateKustomization(clusterName, testNamespace, models.RuntimePath, models.SourcePath, models.SystemKustResourcePath, models.UserKustResourcePath, models.WegoAppPath)
 
 			systemKustomizationManifest, err := yaml.Marshal(systemKustomization)
 			Expect(err).ShouldNot(HaveOccurred())
@@ -406,7 +406,7 @@ var _ = Describe("Installer", func() {
 			wegoConfigManifest, err := yaml.Marshal(gitopsConfigMap)
 			Expect(err).ShouldNot(HaveOccurred())
 
-			systemKustomization := models.CreateKustomize(clusterName, testNamespace, models.RuntimePath, models.SourcePath, models.SystemKustResourcePath, models.UserKustResourcePath)
+			systemKustomization := models.CreateKustomization(clusterName, testNamespace, models.RuntimePath, models.SourcePath, models.SystemKustResourcePath, models.UserKustResourcePath)
 
 			systemKustomizationManifest, err := yaml.Marshal(systemKustomization)
 			Expect(err).ShouldNot(HaveOccurred())
