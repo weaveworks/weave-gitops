53a80759dbf006a7cf467fd9891295f07ac88a2e
James Wilson <jwils22@gmail.com>
2022-01-21 21:49:02 -0500

    New proto endpoints

---
+++ core/server/server.go
@@ -0,0 +1,44 @@
+package server
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
+	"github.com/weaveworks/weave-gitops/core/clientset"
+	"github.com/weaveworks/weave-gitops/core/gitops/app"
+	"github.com/weaveworks/weave-gitops/core/gitops/kustomize"
+	"github.com/weaveworks/weave-gitops/core/gitops/source"
+	pb "github.com/weaveworks/weave-gitops/pkg/api/app"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+func Hydrate(ctx context.Context, mux *runtime.ServeMux) error {
+	appKubeCreator := app.NewKubeCreator()
+	appFetcher := app.NewKubeAppFetcher()
+
+	kustCreator := kustomize.NewK8sCreator()
+	kustFetcher := kustomize.NewKustomizationFetcher()
+
+	sourceCreator := source.NewKubeCreator()
+	sourceFetcher := source.NewSourceFetcher()
+
+	clientSet := clientset.NewClientSets()
+
+	appsServer := NewAppServer(clientSet, appKubeCreator, kustCreator, sourceCreator, appFetcher, kustFetcher, sourceFetcher)
+	if err := pb.RegisterAppsHandlerServer(ctx, mux, appsServer); err != nil {
+		return fmt.Errorf("could not register new app server: %w", err)
+	}
+
+	fluxServer := NewFluxServer(clientSet, kustCreator, sourceCreator, kustFetcher, sourceFetcher)
+	if err := pb.RegisterFluxHandlerServer(ctx, mux, fluxServer); err != nil {
+		return fmt.Errorf("could not register new kustomization server: %w", err)
+	}
+
+	return nil
+}
+
+func intervalDuration(input *pb.Interval) metav1.Duration {
+	return metav1.Duration{Duration: time.Duration(input.Hours)*time.Hour + time.Duration(input.Minutes)*time.Minute + time.Duration(input.Seconds)*time.Second}
+}
