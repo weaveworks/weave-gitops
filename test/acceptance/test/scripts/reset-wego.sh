4b8eda1220656675929071370aa78c14b5124e99
rokshana-b <76788247+rokshana-b@users.noreply.github.com>
2022-01-25 11:58:23 -0500

    Change nightly tests and add weekly tests (#1336)
    * add gitlab tests runs
    
    * add function to cleanup clusters

--- test/acceptance/test/scripts/reset-wego.sh
+++ test/acceptance/test/scripts/reset-wego.sh
@@ -31,3 +31,13 @@ echo "Delete any running applications"
 kubectl delete apps -n $ns --all
 echo "Delete all secrets"
 for s in $(kubectl get secrets -n $ns| grep weave-gitops-|cut -d' ' -f1); do kubectl delete secrets $s -n $ns; done
+
+echo "Deleting resources from all my-nginx-* namespaces..."
+namespaces="$(kubectl get ns -A --no-headers -o custom-columns=":metadata.name" | grep nginx)";
+for ns in $namespaces
+do
+    kubectl delete all --all -n $ns
+    echo "All resources have been deleted from namespace $ns, proceeding to delete empty namespace..."
+    kubectl delete ns $ns
+done
+echo "Test Script run successful :)"
