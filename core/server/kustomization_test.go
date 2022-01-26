17b692a1a33540f0c40078c65cff1ce76c49a874
Jordan Pellizzari <jordan@weave.works>
2022-01-26 13:56:01 -0800

    Add more create kustomization test coverage

--- core/server/kustomization_test.go
+++ core/server/kustomization_test.go
@@ -2,17 +2,24 @@ package server
 
 import (
 	"context"
+	"fmt"
 	"testing"
 
 	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/types"
 	"k8s.io/apimachinery/pkg/util/rand"
+	"sigs.k8s.io/controller-runtime/pkg/client"
 
+	kustomizev1 "github.com/fluxcd/kustomize-controller/api/v1beta2"
+	"github.com/google/go-cmp/cmp"
+	"github.com/google/go-cmp/cmp/cmpopts"
 	. "github.com/onsi/gomega"
+	stypes "github.com/weaveworks/weave-gitops/core/server/types"
 	pb "github.com/weaveworks/weave-gitops/pkg/api/app"
 	"github.com/weaveworks/weave-gitops/pkg/kube"
 )
 
-func TestCreateKustomization_App_Association(t *testing.T) {
+func TestCreateKustomization(t *testing.T) {
 	g := NewGomegaWithT(t)
 
 	ctx := context.Background()
@@ -21,30 +28,80 @@ func TestCreateKustomization_App_Association(t *testing.T) {
 	defer cleanup()
 
 	_, k, err := kube.NewKubeHTTPClientWithConfig(k8sEnv.Rest, "")
-	if err != nil {
-		t.Fatal(err)
-	}
+	g.Expect(err).NotTo(HaveOccurred())
 
+	t.Run("with app association", func(t *testing.T) {
+		ns := newNamespace(ctx, k, g)
+
+		r := &pb.AddKustomizationReq{
+			Name:      "mykustomization",
+			Namespace: ns.Name,
+			AppName:   "someapp",
+			SourceRef: &pb.SourceRef{
+				Kind: pb.SourceRef_GitRepository,
+				Name: "othersource",
+			},
+		}
+
+		res, err := c.AddKustomization(ctx, r)
+		g.Expect(err).NotTo(HaveOccurred())
+
+		g.Expect(res.Success).To(BeTrue())
+
+		actual := &kustomizev1.Kustomization{}
+
+		g.Expect(k.Get(ctx, types.NamespacedName{Name: r.Name, Namespace: ns.Name}, actual)).To(Succeed())
+
+		expected := stypes.ProtoToKustomization(r)
+
+		opt := cmpopts.IgnoreFields(kustomizev1.Kustomization{}, diffIgnoredFields...)
+		diff := cmp.Diff(*actual, expected, opt)
+
+		if diff != "" {
+			t.Error(fmt.Errorf("(-actual +expected):\n%s", diff))
+		}
+	})
+
+	t.Run("no app association", func(t *testing.T) {
+		ns := newNamespace(ctx, k, g)
+
+		r := &pb.AddKustomizationReq{
+			Name:      "mykustomization",
+			Namespace: ns.Name,
+			AppName:   "",
+			SourceRef: &pb.SourceRef{
+				Kind: pb.SourceRef_GitRepository,
+				Name: "othersource",
+			},
+		}
+
+		res, err := c.AddKustomization(ctx, r)
+		g.Expect(err).NotTo(HaveOccurred())
+
+		g.Expect(res.Success).To(BeTrue())
+
+		actual := &kustomizev1.Kustomization{}
+
+		g.Expect(k.Get(ctx, types.NamespacedName{Name: r.Name, Namespace: ns.Name}, actual)).To(Succeed())
+
+		expected := stypes.ProtoToKustomization(r)
+
+		opt := cmpopts.IgnoreFields(kustomizev1.Kustomization{}, diffIgnoredFields...)
+		diff := cmp.Diff(*actual, expected, opt)
+
+		if diff != "" {
+			t.Error(fmt.Errorf("(-actual +expected):\n%s", diff))
+		}
+
+		g.Expect(actual.Labels["app.kubernetes.io/part-of"]).To(Equal(""))
+	})
+}
+
+func newNamespace(ctx context.Context, k client.Client, g *GomegaWithT) *corev1.Namespace {
 	ns := &corev1.Namespace{}
 	ns.Name = "kube-test-" + rand.String(5)
 
-	err = k.Create(ctx, ns)
-	if err != nil {
-		t.Fatal(err)
-	}
+	g.Expect(k.Create(ctx, ns)).To(Succeed())
 
-	r := &pb.AddKustomizationReq{
-		Name:      "mykustomization",
-		Namespace: ns.Name,
-		AppName:   "someapp",
-		SourceRef: &pb.SourceRef{
-			Kind: pb.SourceRef_GitRepository,
-			Name: "othersource",
-		},
-	}
-
-	res, err := c.AddKustomization(ctx, r)
-	g.Expect(err).NotTo(HaveOccurred())
-
-	g.Expect(res.Success).To(BeTrue())
+	return ns
 }
