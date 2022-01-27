7b98853c15e8cdf8e3ab62ce81ef3d2fe4ff37fb
Jordan Pellizzari <jordan@weave.works>
2022-01-27 14:32:44 -0800

    Re-arrange proto files

--- ui/lib/api/app/apps.pb.ts
+++ ui/lib/api/app/apps.pb.ts
@@ -5,7 +5,7 @@
 */
 
 import * as fm from "../applications/fetch.pb"
-import * as Gitops_serverV1Flux from "./flux.pb"
+import * as Gitops_serverV1Automation from "./automation.pb"
 import * as Gitops_serverV1Source from "./source.pb"
 export type App = {
   namespace?: string
@@ -66,14 +66,14 @@ export class Apps {
   static RemoveApp(req: RemoveAppRequest, initReq?: fm.InitReq): Promise<RemoveAppResponse> {
     return fm.fetchReq<RemoveAppRequest, RemoveAppResponse>(`/v1/namespace/${req["namespace"]}/app/${req["name"]}`, {...initReq, method: "DELETE"})
   }
-  static AddKustomization(req: Gitops_serverV1Flux.AddKustomizationReq, initReq?: fm.InitReq): Promise<Gitops_serverV1Flux.AddKustomizationRes> {
-    return fm.fetchReq<Gitops_serverV1Flux.AddKustomizationReq, Gitops_serverV1Flux.AddKustomizationRes>(`/v1/namespace/${req["namespace"]}/app/${req["appName"]}/kustomization`, {...initReq, method: "POST", body: JSON.stringify(req)})
+  static AddKustomization(req: Gitops_serverV1Automation.AddKustomizationReq, initReq?: fm.InitReq): Promise<Gitops_serverV1Automation.AddKustomizationRes> {
+    return fm.fetchReq<Gitops_serverV1Automation.AddKustomizationReq, Gitops_serverV1Automation.AddKustomizationRes>(`/v1/namespace/${req["namespace"]}/app/${req["appName"]}/kustomization`, {...initReq, method: "POST", body: JSON.stringify(req)})
   }
-  static ListKustomizations(req: Gitops_serverV1Flux.ListKustomizationsReq, initReq?: fm.InitReq): Promise<Gitops_serverV1Flux.ListKustomizationsRes> {
-    return fm.fetchReq<Gitops_serverV1Flux.ListKustomizationsReq, Gitops_serverV1Flux.ListKustomizationsRes>(`/v1/namespace/${req["namespace"]}/app/${req["appName"]}/kustomization?${fm.renderURLSearchParams(req, ["namespace", "appName"])}`, {...initReq, method: "GET"})
+  static ListKustomizations(req: Gitops_serverV1Automation.ListKustomizationsReq, initReq?: fm.InitReq): Promise<Gitops_serverV1Automation.ListKustomizationsRes> {
+    return fm.fetchReq<Gitops_serverV1Automation.ListKustomizationsReq, Gitops_serverV1Automation.ListKustomizationsRes>(`/v1/namespace/${req["namespace"]}/app/${req["appName"]}/kustomization?${fm.renderURLSearchParams(req, ["namespace", "appName"])}`, {...initReq, method: "GET"})
   }
-  static RemoveKustomizations(req: Gitops_serverV1Flux.RemoveKustomizationReq, initReq?: fm.InitReq): Promise<Gitops_serverV1Flux.RemoveKustomizationRes> {
-    return fm.fetchReq<Gitops_serverV1Flux.RemoveKustomizationReq, Gitops_serverV1Flux.RemoveKustomizationRes>(`/v1/namespace/${req["namespace"]}/app/${req["appName"]}/kustomization/${req["kustomizationName"]}`, {...initReq, method: "DELETE"})
+  static RemoveKustomizations(req: Gitops_serverV1Automation.RemoveKustomizationReq, initReq?: fm.InitReq): Promise<Gitops_serverV1Automation.RemoveKustomizationRes> {
+    return fm.fetchReq<Gitops_serverV1Automation.RemoveKustomizationReq, Gitops_serverV1Automation.RemoveKustomizationRes>(`/v1/namespace/${req["namespace"]}/app/${req["appName"]}/kustomization/${req["kustomizationName"]}`, {...initReq, method: "DELETE"})
   }
   static AddGitRepository(req: Gitops_serverV1Source.AddGitRepositoryReq, initReq?: fm.InitReq): Promise<Gitops_serverV1Source.AddGitRepositoryRes> {
     return fm.fetchReq<Gitops_serverV1Source.AddGitRepositoryReq, Gitops_serverV1Source.AddGitRepositoryRes>(`/v1/namespace/${req["namespace"]}/app/${req["appName"]}/gitrepository`, {...initReq, method: "POST", body: JSON.stringify(req)})
---
+++ ui/lib/api/app/automation.pb.ts
@@ -0,0 +1,47 @@
+/* eslint-disable */
+// @ts-nocheck
+/*
+* This file is a generated Typescript file for GRPC Gateway, DO NOT MODIFY
+*/
+
+import * as Gitops_serverV1Source from "./source.pb"
+export type Kustomization = {
+  namespace?: string
+  name?: string
+  path?: string
+  sourceRef?: Gitops_serverV1Source.SourceRef
+  interval?: Gitops_serverV1Source.Interval
+}
+
+export type AddKustomizationReq = {
+  namespace?: string
+  appName?: string
+  name?: string
+  path?: string
+  sourceRef?: Gitops_serverV1Source.SourceRef
+  interval?: Gitops_serverV1Source.Interval
+}
+
+export type AddKustomizationRes = {
+  success?: boolean
+  kustomization?: Kustomization
+}
+
+export type ListKustomizationsReq = {
+  namespace?: string
+  appName?: string
+}
+
+export type ListKustomizationsRes = {
+  kustomizations?: Kustomization[]
+}
+
+export type RemoveKustomizationReq = {
+  namespace?: string
+  appName?: string
+  kustomizationName?: string
+}
+
+export type RemoveKustomizationRes = {
+  success?: boolean
+}
