6457a502b1212e4d5da0da19f22a3871d8b90884
Jordan Pellizzari <jordan@weave.works>
2022-01-28 10:05:43 -0800

    Revert "Revert "Initial work implementing HelmRepository to the API (#1352)" (#1363)"
    This reverts commit 61f202bb6229642a440e18ecfd662729fd9b3281.

---
+++ core/server/types/helmrepository.go
@@ -0,0 +1,51 @@
+package types
+
+import (
+	"time"
+
+	"github.com/fluxcd/source-controller/api/v1beta1"
+	pb "github.com/weaveworks/weave-gitops/pkg/api/app"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+func ProtoToHelmRepository(repositoryReq *pb.AddHelmRepositoryReq) v1beta1.HelmRepository {
+	labels := map[string]string{
+		ManagedByLabel: managedByWeaveGitops,
+		CreatedByLabel: createdBySourceController,
+	}
+
+	if repositoryReq.AppName != "" {
+		labels[PartOfLabel] = repositoryReq.AppName
+	}
+
+	return v1beta1.HelmRepository{
+		TypeMeta: metav1.TypeMeta{
+			Kind:       v1beta1.HelmRepositoryKind,
+			APIVersion: v1beta1.GroupVersion.Identifier(),
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      repositoryReq.Name,
+			Namespace: repositoryReq.Namespace,
+			Labels:    labels,
+		},
+		Spec: v1beta1.HelmRepositorySpec{
+			URL:      repositoryReq.Url,
+			Interval: metav1.Duration{Duration: time.Minute * 1},
+			Timeout:  &metav1.Duration{Duration: time.Minute * 1},
+		},
+		Status: v1beta1.HelmRepositoryStatus{},
+	}
+}
+
+func HelmRepositoryToProto(repository *v1beta1.HelmRepository) *pb.HelmRepository {
+	hr := &pb.HelmRepository{
+		Name:      repository.Name,
+		Namespace: repository.Namespace,
+		Url:       repository.Spec.URL,
+		Interval: &pb.Interval{
+			Minutes: 1,
+		},
+	}
+
+	return hr
+}
