c0a4c489655868fae75dc0f925ff2e37e02506b7
josecordaz <josecordaz@gmail.com>
2022-01-28 09:40:44 -0800

    Initial work implementing HelmRepository to the API (#1352)
    * New proto endpoints
    
    * Added integration tests
    
    * V2 workflow added
    
    * Use controller-runtime client in app server
    
    * Fix whitepace and unused linting errors
    
    * Add kustomization tests
    
    * Fix flaky test by disabling Prometheus metrics (#1341)
    
    The test suites for some of our packages spin up parts of a Kubernetes
    cluster using the `envtest` library, so that we have something more
    solid than mocks to test against. One problem this was causing was that
    running tests for both the `kube` and `server` packages at once would
    cause one of those test suites to fail.
    
    Side-note: it's possible to reproduce this problem a little more
    efficiently by spinning up a fake web server to sit on port 8080, for
    example using Python's built-in web server:
    
        python3 -m http.server 8080
    
    After some digging, I found out that the reason for this is that both
    environments were trying to spin up a Prometheus metrics handler
    listening on port 8080. If they both tried to do this at once, one would
    fail, causing the entire test suite for that package to fail too
    (including some nil-pointer panics during the clean up, which might be
    worth tidying up in a future pull request).
    
    Setting the metrics bind address to the string "0" disables metrics for
    the test clusters (the documentation for which is buried in a comment in
    the Kubernetes docs[1]), which fixes this particular problem. Testing on
    my local machine, there didn't seem to be any other fixed port
    assignments cropping up to take its place.
    
    [1]: https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.10.2/pkg/manager
    
    * Use suite test for kubebuilder setup/teardown
    
    * Add more create kustomization test coverage
    
    * Skip acceptance test lint
    
    * Split k8s resources into separate files
    
    * Initial work implementing HelmRepository to the API
    
    * Added weave related labels to helm repository resource
    
    * Use app name to list helm repositories
    
    * Added unit test for ListHelmRepositories
    
    * Added missing namespace parameter when listing helm repositories
    
    Co-authored-by: James Wilson <jwils22@gmail.com>
    Co-authored-by: Jordan Pellizzari <jordan@weave.works>
    Co-authored-by: David Thompson <32775+dhwthompson@users.noreply.github.com>

--- core/server/sources.go
+++ core/server/sources.go
@@ -5,7 +5,6 @@ import (
 
 	sourcev1 "github.com/fluxcd/source-controller/api/v1beta1"
 	"github.com/weaveworks/weave-gitops/core/server/types"
-	stypes "github.com/weaveworks/weave-gitops/core/server/types"
 	pb "github.com/weaveworks/weave-gitops/pkg/api/app"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
@@ -18,7 +17,7 @@ func (as *appServer) AddGitRepository(ctx context.Context, msg *pb.AddGitReposit
 		return nil, doClientError(err)
 	}
 
-	src := stypes.ProtoToGitRepository(msg)
+	src := types.ProtoToGitRepository(msg)
 
 	if err := k8s.Create(ctx, src); err != nil {
 		return nil, status.Errorf(codes.Internal, "creating source for app %q: %s", msg.AppName, err.Error())
@@ -26,7 +25,7 @@ func (as *appServer) AddGitRepository(ctx context.Context, msg *pb.AddGitReposit
 
 	return &pb.AddGitRepositoryRes{
 		Success:       true,
-		GitRepository: stypes.GitRepositoryToProto(src),
+		GitRepository: types.GitRepositoryToProto(src),
 	}, nil
 }
 
@@ -53,10 +52,61 @@ func (as *appServer) ListGitRepositories(ctx context.Context, msg *pb.ListGitRep
 
 	var results []*pb.GitRepository
 	for _, repository := range list.Items {
-		results = append(results, stypes.GitRepositoryToProto(&repository))
+		results = append(results, types.GitRepositoryToProto(&repository))
 	}
 
 	return &pb.ListGitRepositoryRes{
 		GitRepositories: results,
+	}, nil
+}
+
+func (as *appServer) AddHelmRepository(ctx context.Context, msg *pb.AddHelmRepositoryReq) (*pb.AddHelmRepositoryRes, error) {
+	k8s, err := as.k8s.Client(ctx)
+	if err != nil {
+		return nil, doClientError(err)
+	}
+
+	src := types.ProtoToHelmRepository(msg)
+
+	if err := k8s.Create(ctx, &src); err != nil {
+		return nil, status.Errorf(codes.Internal, "creating source for helm repository %q: %s", msg.Name, err.Error())
+	}
+
+	return &pb.AddHelmRepositoryRes{
+		Success:        true,
+		HelmRepository: types.HelmRepositoryToProto(&src),
+	}, nil
+}
+
+func (as *appServer) ListHelmRepositories(ctx context.Context, msg *pb.ListHelmRepositoryReq) (*pb.ListHelmRepositoryRes, error) {
+	k8s, err := as.k8s.Client(ctx)
+	if err != nil {
+		return nil, doClientError(err)
+	}
+
+	list := &sourcev1.HelmRepositoryList{}
+
+	var opts client.MatchingLabels
+	if msg.AppName != "" {
+		opts = client.MatchingLabels{
+			types.PartOfLabel: msg.AppName,
+		}
+	}
+
+	if err := k8s.List(ctx, list, &opts, client.InNamespace(msg.Namespace)); err != nil {
+		return nil, status.Errorf(codes.Internal, "unable to list helm repositories for app %s: %s", msg.AppName, err.Error())
+	}
+
+	if err != nil {
+		return nil, status.Errorf(codes.Internal, "unable to get helm repository list: %s", err.Error())
+	}
+
+	var results []*pb.HelmRepository
+	for _, repository := range list.Items {
+		results = append(results, types.HelmRepositoryToProto(&repository))
+	}
+
+	return &pb.ListHelmRepositoryRes{
+		HelmRepositories: results,
 	}, nil
 }
