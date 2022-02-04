e93d771f566cfc2594afd14a026d1302e94c0e26
Gergely Brautigam <182850+Skarlso@users.noreply.github.com>
2022-02-04 16:48:19 +0100

    Pend the profiles test until we can figure out a better way to run them

--- test/acceptance/test/profiles_test.go
+++ test/acceptance/test/profiles_test.go
@@ -27,7 +27,7 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/client"
 )
 
-var _ = Describe("Weave GitOps Profiles API", func() {
+var _ = PDescribe("Weave GitOps Profiles API", func() {
 	var (
 		namespace        = "test-namespace"
 		clusterName      string
