cc070f7acf39953e76b7506267e3090e651320d8
Robin Sonefors <robin.sonefors@weave.works>
2022-02-15 17:30:04 +0000

    Delete GetApplication, ListApplications endpoints
    This removes the endpoints, the backend implementation, and the
    frontend bits that uses or depends on the endpoints.
    
    There's bits here - reconciliation graph and commits table - that
    we could potentially want to resuscitate in the future, but at the
    moment they seem to be entirely expecting an Application as their
    input, so I'm deleting them here. If we want to use the code, it's
    still in git history.
    
    Technically, this "breaks" enterprise, since they call
    listApplications in their UI code - but they'll need to request
    whatever new application API we write anyway.

--- ui/hooks/applications.ts
+++ ui/hooks/applications.ts
@@ -1,11 +1,7 @@
 import _ from "lodash";
-import { useContext, useEffect, useState } from "react";
-import { getProviderToken } from "..";
+import { useContext, useEffect } from "react";
 import { AppContext } from "../contexts/AppContext";
 import {
-  Application,
-  GetApplicationRequest,
-  GetApplicationResponse,
   GitProvider,
   ListCommitsRequest,
   ListCommitsResponse,
@@ -13,7 +9,6 @@ import {
 } from "../lib/api/applications/applications.pb";
 import { makeHeaders, RequestStateWithToken, useRequestState } from "./common";
 
-const WeGONamespace = "wego-system";
 
 export function useParseRepoURL(url: string) {
   const { applicationsClient } = useContext(AppContext);
@@ -32,7 +27,7 @@ type ListCommitsReturnType = RequestStateWithToken<
 >;
 
 export function useListCommits(): ListCommitsReturnType {
-  const { applicationsClient, getProviderToken } = useContext(AppContext);
+  const { applicationsClient } = useContext(AppContext);
   const [res, loading, error, req] = useRequestState<ListCommitsResponse>();
 
   return [
@@ -40,57 +35,8 @@ export function useListCommits(): ListCommitsReturnType {
     loading,
     error,
     (provider: GitProvider, body: ListCommitsRequest) => {
-      const headers = makeHeaders(_.bind(getProviderToken, this, provider));
+      const headers = makeHeaders(_.bind(_, this, provider));
       req(applicationsClient.ListCommits(body, { headers }));
     },
   ];
-}
-
-export function useAppGet() {
-  const { applicationsClient } = useContext(AppContext);
-  const [res, loading, error, req] = useRequestState<GetApplicationResponse>();
-
-  return [
-    res,
-    loading,
-    error,
-    (body: GetApplicationRequest) =>
-      req(applicationsClient.GetApplication(body)),
-  ];
-}
-
-export default function useApplications() {
-  const { applicationsClient, doAsyncError } = useContext(AppContext);
-  const [loading, setLoading] = useState(true);
-
-  const listApplications = (namespace: string = WeGONamespace) => {
-    setLoading(true);
-
-    return applicationsClient
-      .ListApplications({ namespace: namespace })
-      .then((res) => res.applications)
-      .catch((err) => doAsyncError(err.message, err.detail))
-      .finally(() => setLoading(false));
-  };
-
-  const getApplication = (name: string) => {
-    setLoading(true);
-
-    return applicationsClient
-      .GetApplication({ name, namespace: WeGONamespace })
-      .then((res) => res.application)
-      .catch((err) => doAsyncError("Error fetching application", err.message))
-      .finally(() => setLoading(false));
-  };
-
-  const listCommits = (app: Application) => {
-    return applicationsClient.ListCommits({ ...app });
-  };
-
-  return {
-    loading,
-    listApplications,
-    listCommits,
-    getApplication,
-  };
 }
