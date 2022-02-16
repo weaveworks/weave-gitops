4fc562e850455f4980aeeca527d443abb4279414
Sam Cook <sam.cook@weave.works>
2022-02-16 12:31:56 +0000

    Delete ui/run command
    This will be moved to a standalone binary

--- cmd/gitops/root/cmd.go
+++ cmd/gitops/root/cmd.go
@@ -16,7 +16,6 @@ import (
 	"github.com/weaveworks/weave-gitops/cmd/gitops/docs"
 	"github.com/weaveworks/weave-gitops/cmd/gitops/get"
 	"github.com/weaveworks/weave-gitops/cmd/gitops/install"
-	"github.com/weaveworks/weave-gitops/cmd/gitops/ui"
 	"github.com/weaveworks/weave-gitops/cmd/gitops/upgrade"
 	"github.com/weaveworks/weave-gitops/cmd/gitops/version"
 	fluxBin "github.com/weaveworks/weave-gitops/pkg/flux"
@@ -128,7 +127,6 @@ func RootCmd(client *resty.Client) *cobra.Command {
 
 	rootCmd.AddCommand(install.Cmd)
 	rootCmd.AddCommand(version.Cmd)
-	rootCmd.AddCommand(ui.NewCommand())
 	rootCmd.AddCommand(get.GetCommand(&options.endpoint, client))
 	rootCmd.AddCommand(add.GetCommand(&options.endpoint, client))
 	rootCmd.AddCommand(delete.DeleteCommand(&options.endpoint, client))
