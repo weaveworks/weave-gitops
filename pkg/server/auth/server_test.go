8a246aec60f370582658246d67a17055ed62f312
Sara El-Zayat <sara@weave.works>
2022-02-21 14:44:29 +0200

    Add failure test

--- pkg/server/auth/server_test.go
+++ pkg/server/auth/server_test.go
@@ -5,7 +5,6 @@ import (
 	"net/http"
 	"net/http/httptest"
 	"testing"
-	"time"
 
 	"github.com/go-logr/logr"
 	"github.com/oauth2-proxy/mockoidc"
@@ -30,14 +29,6 @@ func TestLogoutSuccess(t *testing.T) {
 
 	assert.Nil(t, err)
 
-	cookie := &http.Cookie{
-		Name:     "id_token",
-		Value:    "foo",
-		Path:     "/",
-		Expires:  time.Now().UTC().Add(5),
-		HttpOnly: true,
-	}
-
 	w := httptest.NewRecorder()
 
 	req := httptest.NewRequest(http.MethodPost, "https://example.com/logout", nil)
@@ -48,6 +39,8 @@ func TestLogoutSuccess(t *testing.T) {
 		t.Errorf("expected status to be 200 but got %v instead", resp.StatusCode)
 	}
 
+	cookie := &http.Cookie{}
+
 	for _, c := range resp.Cookies() {
 		if c.Name == auth.IDTokenCookieName {
 			cookie = c
@@ -56,4 +49,32 @@ func TestLogoutSuccess(t *testing.T) {
 	}
 
 	assert.Equal(t, cookie.Value, "")
+}
+
+func TestLogoutWithWrongMethod(t *testing.T) {
+	m, err := mockoidc.Run()
+	if err != nil {
+		t.Errorf("failed to create mock OIDC server: %v", err)
+	}
+
+	s, err := auth.NewAuthServer(context.Background(), logr.Discard(), http.DefaultClient, auth.AuthConfig{
+		OIDCConfig: auth.OIDCConfig{
+			IssuerURL: m.Config().Issuer,
+		},
+		CookieConfig: auth.CookieConfig{
+			CookieDuration: 5,
+		},
+	})
+
+	assert.Nil(t, err)
+
+	w := httptest.NewRecorder()
+
+	req := httptest.NewRequest(http.MethodGet, "https://example.com/logout", nil)
+	s.Logout().ServeHTTP(w, req)
+
+	resp := w.Result()
+	if resp.StatusCode != http.StatusMethodNotAllowed {
+		t.Errorf("expected status to be 405 but got %v instead", resp.StatusCode)
+	}
 }
