f749893f57b11ce4906afc527c6d6da9d42b4e55
Jordan Pellizzari <jordan@weave.works>
2022-02-22 10:57:10 -0800

    Add GetChildObjects API endpoint

--- core/server/fluxruntime_test.go
+++ core/server/fluxruntime_test.go
@@ -2,6 +2,7 @@ package server
 
 import (
 	"context"
+	"fmt"
 	"testing"
 
 	. "github.com/onsi/gomega"
@@ -10,6 +11,7 @@ import (
 	appsv1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/types"
 )
 
 func TestGetReconciledObjects(t *testing.T) {
@@ -70,4 +72,83 @@ func TestGetReconciledObjects(t *testing.T) {
 	first := res.Objects[0]
 	g.Expect(first.GroupVersionKind.Kind).To(Equal("Deployment"))
 	g.Expect(first.Name).To(Equal(reconciledObj.Name))
+}
+
+func TestGetChildObjects(t *testing.T) {
+	g := NewGomegaWithT(t)
+
+	ctx := context.Background()
+
+	c, cleanup := makeGRPCServer(k8sEnv.Rest, t)
+	defer cleanup()
+
+	_, k, err := kube.NewKubeHTTPClientWithConfig(k8sEnv.Rest, "")
+	g.Expect(err).NotTo(HaveOccurred())
+
+	automationName := "my-automation"
+	ns := newNamespace(ctx, k, g)
+
+	deployment := &appsv1.Deployment{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "my-deployment",
+			Namespace: ns.Name,
+		},
+		Spec: appsv1.DeploymentSpec{
+			Selector: &metav1.LabelSelector{
+				MatchLabels: map[string]string{
+					"app": automationName,
+				},
+			},
+			Template: corev1.PodTemplateSpec{
+				ObjectMeta: metav1.ObjectMeta{
+					Labels: map[string]string{"app": automationName},
+				},
+				Spec: corev1.PodSpec{
+					Containers: []corev1.Container{{
+						Name:  "nginx",
+						Image: "nginx",
+					}},
+				},
+			},
+		},
+	}
+	g.Expect(k.Create(ctx, deployment)).Should(Succeed())
+	// Get after Create to get a populated UID
+	g.Expect(k.Get(ctx, types.NamespacedName{Name: deployment.Name, Namespace: deployment.Namespace}, deployment)).To(Succeed())
+
+	rs := &appsv1.ReplicaSet{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      fmt.Sprintf("%s-123abcd", automationName),
+			Namespace: ns.Name,
+		},
+		Spec: appsv1.ReplicaSetSpec{
+			Template: deployment.Spec.Template,
+			Selector: deployment.Spec.Selector,
+		},
+	}
+
+	rs.SetOwnerReferences([]metav1.OwnerReference{{
+		UID:        deployment.UID,
+		APIVersion: appsv1.SchemeGroupVersion.String(),
+		Kind:       "Deployment",
+		Name:       deployment.Name,
+	}})
+
+	g.Expect(k.Create(ctx, rs)).Should(Succeed())
+
+	res, err := c.GetChildObjects(ctx, &pb.GetChildObjectsRequest{
+		ParentUid: string(deployment.UID),
+		Namespace: ns.Name,
+		GroupVersionKind: &pb.GroupVersionKind{
+			Group:   "apps",
+			Version: "v1",
+			Kind:    "ReplicaSet",
+		},
+	})
+	g.Expect(err).NotTo(HaveOccurred())
+	g.Expect(res.Objects).To(HaveLen(1))
+
+	first := res.Objects[0]
+	g.Expect(first.GroupVersionKind.Kind).To(Equal("ReplicaSet"))
+	g.Expect(first.Name).To(Equal(rs.Name))
 }
