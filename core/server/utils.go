19a4faa23bb6e99c8094e8ecf2885bb2dcc42747
Balazs Nadasdi <balazs@weave.works>
2022-02-22 17:10:52 +0100

    feat: Create endpoint for fetching the flux namespace
    Add new endpoint to query flux namespace. For simplicity, no arguments
    on the request and returns only the name of the namespace. That fulfills
    the original request so we can remove the constant[^1] from the UI.
    
    == Considered solutions
    
    1. Create a general endpoint
    
    The consumer could query namespaces and filter them with provided
    filters.
    
    Dropped this solution, because it could open up some weird surfaces
    where the client can query arbitrary filters.
    
    2. Create a general endpoint with strict filtering options
    
    The consumer could query namespaces with a set of filter options and if
    they are not specified, throw back an error.
    
    Dropped this solution, because changing the filtering rules would
    require code change. The final solution in this change-set has the same
    issue, but less complex. If we don't get extra value out of the
    complexity, it's not worth it.
    
    == Follow-up
    
    If/when we need a complex endpoint to query all namespaces with filter
    options, then we can add one with a plan like:
    
    * Who/what will use it?
    * What is the desired input and output?
    * How to achieve this?
    * What limitations do we want to implement?
    * How much freedom do we want to give to the consumer?
    
    == Other changes
    
    * `make proto` updated a lot of auto-generated files.
    * In `tools/dependencies.toml`, removed the system-wide installation of
      a tool, because the last thing I want from a project to download a
      binary and place it somewhere with root permission. I'm not giving
      root permissions just to install dependencies.
    * Use `tools/bin/tilt` in `Makefile` (see previous entry).
    * Replaced the logic behind `getMatchingLabels`. Using the `with option`
      pattern, we can build up more complex labeling queries, and the
      codebase will remain clean.
    
    ```go
    match := matchLabel(
      withPartOfLabel(appName),
      withInstanceLabel(appInstance),
      withManagedByLabel(appManager),
    )
    ```
    
    [^1]: https://github.com/weaveworks/weave-gitops/pull/1451/files#diff-35acf2adc3a7b1021fb41d3473b47e030d3708ba2e8ff7a4b76c13ee80b4076cR42
    
    Fixes #1465

--- core/server/utils.go
+++ core/server/utils.go
@@ -1,17 +1,9 @@
 package server
 
 import (
-	"github.com/weaveworks/weave-gitops/core/server/types"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 )
 
 func getMatchingLabels(appName string) client.MatchingLabels {
-	var opts client.MatchingLabels
-	if appName != "" {
-		opts = client.MatchingLabels{
-			types.PartOfLabel: appName,
-		}
-	}
-
-	return opts
+	return matchLabel(withPartOfLabel(appName))
 }
