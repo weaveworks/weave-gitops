61b11140c88d5ad6020026d4d1ada1ab684c653b
AlinaGoaga <35202557+AlinaGoaga@users.noreply.github.com>
2022-02-23 17:20:35 +0000

    Update ui/contexts/AuthContext.tsx
    Co-authored-by: Simon <footless@gmail.com>

--- ui/contexts/AuthContext.tsx
+++ ui/contexts/AuthContext.tsx
@@ -108,6 +108,9 @@ export default function AuthContextProvider({ children }) {
   }, []);
 
   React.useEffect(() => {
+    if (!authFlag) {
+      return null;
+    }
     getUserInfo();
     return history.listen(getUserInfo);
   }, [getUserInfo, history]);
