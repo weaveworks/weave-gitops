b258a7eccb4aa41e55836523977d6ce27ef5a255
AlinaGoaga <goaga.alina@gmail.com>
2022-02-28 16:11:53 +0000

    Check ci with data url

--- ui/App.tsx
+++ ui/App.tsx
@@ -87,6 +87,7 @@ const App = () => (
 );
 
 export default function AppContainer() {
+  console.log(process.env.PROXY_INSECURE);
   return (
     <MuiThemeProvider theme={muiTheme}>
       <ThemeProvider theme={theme}>
