a37c223313ee340a9cd76044708f3047e346c7ce
Jordan Pellizzari <jordan@weave.works>
2022-03-04 11:47:39 -0800

    Fix Breadcrumbs routing issue

--- ui/components/Breadcrumbs.tsx
+++ ui/components/Breadcrumbs.tsx
@@ -25,13 +25,9 @@ export const Breadcrumbs = () => {
   const label = getPageLabel(parentValue);
   const parsed = qs.parse(search);
 
-  if (!parentValue) {
-    throw new Error("invalid route");
-  }
-
   return (
     <Flex align>
-      <CrumbLink to={parentValue} textProps={{ bold: true }}>
+      <CrumbLink to={parentValue || ""} textProps={{ bold: true }}>
         {label}
       </CrumbLink>
 
