4bab8be40b7fbf4a95583469e675a9506a6aebd8
Joshua Israel <joshua.israel@weave.works>
2022-03-08 15:57:59 -0500

    initial info list

--- ui/pages/v2/HelmReleaseDetail.tsx
+++ ui/pages/v2/HelmReleaseDetail.tsx
@@ -1,11 +1,16 @@
 import * as React from "react";
 import styled from "styled-components";
 import Heading from "../../components/Heading";
+import InfoList from "../../components/InfoList";
+import Interval from "../../components/Interval";
+import KubeStatusIndicator from "../../components/KubeStatusIndicator";
+import Link from "../../components/Link";
 import Page from "../../components/Page";
 import ReconciledObjectsTable from "../../components/ReconciledObjectsTable";
 import { useGetHelmRelease } from "../../hooks/automations";
 import { AutomationKind } from "../../lib/api/core/types.pb";
-import { WeGONamespace } from "../../lib/types";
+import { formatURL } from "../../lib/nav";
+import { V2Routes, WeGONamespace } from "../../lib/types";
 
 type Props = {
   name: string;
@@ -20,11 +25,44 @@ function HelmReleaseDetail({ className, name }: Props) {
   const { data, isLoading, error } = useGetHelmRelease(name);
   const helmRelease = data?.helmRelease;
 
+  export type HelmRelease = {
+    releaseName?: string;
+    namespace?: string;
+    name?: string;
+    interval?: Interval;
+    helmChart?: HelmChart;
+    conditions?: Condition[];
+    inventory?: GroupVersionKind[];
+  };
+
   return (
     <Page loading={isLoading} error={error} className={className}>
       <Info>
         <Heading level={1}>{helmRelease?.name}</Heading>
         <Heading level={2}>{helmRelease?.namespace}</Heading>
+        <InfoList
+          items={[
+            [
+              "Source",
+              <Link
+                to={formatURL(V2Routes.GitRepo, {
+                  name: helmRelease?.helmChart?.sourceRef.name,
+                })}
+              >
+                {helmRelease?.helmChart?.sourceRef.kind}/
+                {helmRelease?.helmChart?.sourceRef.name}
+              </Link>,
+            ],
+            [
+              "Status",
+              <KubeStatusIndicator conditions={helmRelease?.conditions} />,
+            ],
+            ["Chart", helmRelease?.helmChart?.chart],
+            ["Applied Revision", helmRelease?.helmChart?.version],
+            ["Cluster", "Default"],
+            ["Interval", <Interval interval={helmRelease?.interval} />],
+          ]}
+        />
       </Info>
       <ReconciledObjectsTable
         kinds={helmRelease?.inventory}
