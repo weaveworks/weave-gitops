9d814e16d6aa10f275ec92c77d0b390ad9aa2604
Luiz Filho <luizbafilho@gmail.com>
2022-03-28 14:03:07 -0300

    Adding tests to clusters client

--- core/clustersmngr/client_test.go
+++ core/clustersmngr/client_test.go
@@ -9,6 +9,7 @@ import (
 
 	sourcev1 "github.com/fluxcd/source-controller/api/v1beta1"
 	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/types"
 	"k8s.io/apimachinery/pkg/util/rand"
@@ -23,12 +24,14 @@ func TestClientGet(t *testing.T) {
 	g := NewGomegaWithT(t)
 	ns := createNamespace(g)
 
+	clusterName := "mycluster"
+
 	appName := "myapp" + rand.String(5)
 
 	clientsPool := clustersmngr.NewClustersClientsPool()
 
 	err := clientsPool.Add(&auth.UserPrincipal{}, clustersmngr.Cluster{
-		Name:      appName,
+		Name:      clusterName,
 		Server:    k8sEnv.Rest.Host,
 		TLSConfig: k8sEnv.Rest.TLSClientConfig,
 	})
@@ -53,20 +56,21 @@ func TestClientGet(t *testing.T) {
 
 	k := &kustomizev1.Kustomization{}
 
-	g.Expect(clustersClient.Get(ctx, appName, types.NamespacedName{Name: appName, Namespace: ns.Name}, k)).To(Succeed())
+	g.Expect(clustersClient.Get(ctx, clusterName, types.NamespacedName{Name: appName, Namespace: ns.Name}, k)).To(Succeed())
 	g.Expect(k.Name).To(Equal(appName))
 }
 
-func TestClientGenericList(t *testing.T) {
+func TestClientList(t *testing.T) {
 	g := NewGomegaWithT(t)
 	ns := createNamespace(g)
 
+	clusterName := "mycluster"
 	appName := "myapp" + rand.String(5)
 
 	clientsPool := clustersmngr.NewClustersClientsPool()
 
 	err := clientsPool.Add(&auth.UserPrincipal{}, clustersmngr.Cluster{
-		Name:      appName,
+		Name:      clusterName,
 		Server:    k8sEnv.Rest.Host,
 		TLSConfig: k8sEnv.Rest.TLSClientConfig,
 	})
@@ -91,8 +95,8 @@ func TestClientGenericList(t *testing.T) {
 	cklist := &clustersmngr.ClusteredKustomizationList{}
 
 	g.Expect(clustersClient.List(ctx, cklist, client.InNamespace(ns.Name))).To(Succeed())
-	g.Expect(cklist.Lists()[appName].Items).To(HaveLen(1))
-	g.Expect(cklist.Lists()[appName].Items[0].Name).To(Equal(appName))
+	g.Expect(cklist.Lists()[clusterName].Items).To(HaveLen(1))
+	g.Expect(cklist.Lists()[clusterName].Items[0].Name).To(Equal(appName))
 
 	bucket := &sourcev1.GitRepository{
 		ObjectMeta: v1.ObjectMeta{
@@ -112,8 +116,170 @@ func TestClientGenericList(t *testing.T) {
 	cgrlist := &clustersmngr.ClusteredGitRepositoryList{}
 
 	g.Expect(clustersClient.List(ctx, cgrlist)).To(Succeed())
-	g.Expect(cgrlist.Lists()[appName].Items).To(HaveLen(1))
-	g.Expect(cgrlist.Lists()[appName].Items[0].Name).To(Equal(appName))
+	g.Expect(cgrlist.Lists()[clusterName].Items).To(HaveLen(1))
+	g.Expect(cgrlist.Lists()[clusterName].Items[0].Name).To(Equal(appName))
+}
+
+func TestClientCreate(t *testing.T) {
+	g := NewGomegaWithT(t)
+	ns := createNamespace(g)
+
+	clusterName := "mycluster"
+	appName := "myapp" + rand.String(5)
+
+	clientsPool := clustersmngr.NewClustersClientsPool()
+
+	err := clientsPool.Add(&auth.UserPrincipal{}, clustersmngr.Cluster{
+		Name:      clusterName,
+		Server:    k8sEnv.Rest.Host,
+		TLSConfig: k8sEnv.Rest.TLSClientConfig,
+	})
+	g.Expect(err).To(BeNil())
+
+	clustersClient := clustersmngr.NewClient(clientsPool)
+
+	kust := &kustomizev1.Kustomization{
+		ObjectMeta: v1.ObjectMeta{
+			Name:      appName,
+			Namespace: ns.Name,
+		},
+		Spec: kustomizev1.KustomizationSpec{
+			SourceRef: kustomizev1.CrossNamespaceSourceReference{
+				Kind: "GitRepository",
+			},
+		},
+	}
+	ctx := context.Background()
+
+	g.Expect(clustersClient.Create(ctx, clusterName, kust)).To(Succeed())
+
+	k := &kustomizev1.Kustomization{}
+	g.Expect(clustersClient.Get(ctx, clusterName, types.NamespacedName{Name: appName, Namespace: ns.Name}, k)).To(Succeed())
+	g.Expect(k.Name).To(Equal(appName))
+}
+
+func TestClientDelete(t *testing.T) {
+	g := NewGomegaWithT(t)
+	ns := createNamespace(g)
+
+	clusterName := "mycluster"
+	appName := "myapp" + rand.String(5)
+
+	clientsPool := clustersmngr.NewClustersClientsPool()
+
+	err := clientsPool.Add(&auth.UserPrincipal{}, clustersmngr.Cluster{
+		Name:      clusterName,
+		Server:    k8sEnv.Rest.Host,
+		TLSConfig: k8sEnv.Rest.TLSClientConfig,
+	})
+	g.Expect(err).To(BeNil())
+
+	clustersClient := clustersmngr.NewClient(clientsPool)
+
+	kust := &kustomizev1.Kustomization{
+		ObjectMeta: v1.ObjectMeta{
+			Name:      appName,
+			Namespace: ns.Name,
+		},
+		Spec: kustomizev1.KustomizationSpec{
+			SourceRef: kustomizev1.CrossNamespaceSourceReference{
+				Kind: "GitRepository",
+			},
+		},
+	}
+	ctx := context.Background()
+
+	g.Expect(k8sEnv.Client.Create(ctx, kust)).To(Succeed())
+
+	g.Expect(clustersClient.Delete(ctx, clusterName, kust)).To(Succeed())
+}
+
+func TestClientUpdate(t *testing.T) {
+	g := NewGomegaWithT(t)
+	ns := createNamespace(g)
+
+	clusterName := "mycluster"
+	appName := "myapp" + rand.String(5)
+
+	clientsPool := clustersmngr.NewClustersClientsPool()
+
+	err := clientsPool.Add(&auth.UserPrincipal{}, clustersmngr.Cluster{
+		Name:      clusterName,
+		Server:    k8sEnv.Rest.Host,
+		TLSConfig: k8sEnv.Rest.TLSClientConfig,
+	})
+	g.Expect(err).To(BeNil())
+
+	clustersClient := clustersmngr.NewClient(clientsPool)
+
+	kust := &kustomizev1.Kustomization{
+		ObjectMeta: v1.ObjectMeta{
+			Name:      appName,
+			Namespace: ns.Name,
+		},
+		Spec: kustomizev1.KustomizationSpec{
+			Path: "/foo",
+			SourceRef: kustomizev1.CrossNamespaceSourceReference{
+				Kind: "GitRepository",
+			},
+		},
+	}
+	ctx := context.Background()
+	g.Expect(k8sEnv.Client.Create(ctx, kust)).To(Succeed())
+
+	kust.Spec.Path = "/bar"
+	g.Expect(clustersClient.Update(ctx, clusterName, kust)).To(Succeed())
+
+	k := &kustomizev1.Kustomization{}
+	g.Expect(k8sEnv.Client.Get(ctx, types.NamespacedName{Name: appName, Namespace: ns.Name}, k)).To(Succeed())
+	g.Expect(k.Spec.Path).To(Equal("/bar"))
+}
+
+func TestClientPatch(t *testing.T) {
+	g := NewGomegaWithT(t)
+	ns := createNamespace(g)
+
+	clusterName := "mycluster"
+	appName := "myapp" + rand.String(5)
+
+	clientsPool := clustersmngr.NewClustersClientsPool()
+
+	err := clientsPool.Add(&auth.UserPrincipal{}, clustersmngr.Cluster{
+		Name:      clusterName,
+		Server:    k8sEnv.Rest.Host,
+		TLSConfig: k8sEnv.Rest.TLSClientConfig,
+	})
+	g.Expect(err).To(BeNil())
+
+	clustersClient := clustersmngr.NewClient(clientsPool)
+
+	kust := &kustomizev1.Kustomization{
+		TypeMeta: metav1.TypeMeta{
+			Kind:       kustomizev1.KustomizationKind,
+			APIVersion: kustomizev1.GroupVersion.String(),
+		},
+		ObjectMeta: v1.ObjectMeta{
+			Name:      appName,
+			Namespace: ns.Name,
+		},
+		Spec: kustomizev1.KustomizationSpec{
+			Path: "/foo",
+			SourceRef: kustomizev1.CrossNamespaceSourceReference{
+				Kind: "GitRepository",
+			},
+		},
+	}
+
+	ctx := context.Background()
+	opt := []client.PatchOption{
+		client.ForceOwnership,
+		client.FieldOwner("test"),
+	}
+	g.Expect(clustersClient.Patch(ctx, clusterName, kust, client.Apply, opt...)).To(Succeed())
+
+	k := &kustomizev1.Kustomization{}
+	g.Expect(k8sEnv.Client.Get(ctx, types.NamespacedName{Name: appName, Namespace: ns.Name}, k)).To(Succeed())
+	g.Expect(k.Spec.Path).To(Equal("/foo"))
 }
 
 func createNamespace(g *GomegaWithT) *corev1.Namespace {
