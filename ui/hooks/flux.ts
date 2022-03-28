d235456eb43eecd2591a25d10c86239c69156fa2
Jordan Pellizzari <jordan@weave.works>
2022-03-28 15:47:32 -0700

    Add reconciled objects table to kustomization detail page

--- ui/hooks/flux.ts
+++ ui/hooks/flux.ts
@@ -1,8 +1,12 @@
-import { useContext, useEffect, useState } from "react";
+import { useContext } from "react";
 import { useQuery } from "react-query";
 import { AppContext } from "../contexts/AppContext";
 import { ListFluxRuntimeObjectsResponse } from "../lib/api/core/core.pb";
-import { AutomationKind, GroupVersionKind } from "../lib/api/core/types.pb";
+import {
+  AutomationKind,
+  GroupVersionKind,
+  UnstructuredObject,
+} from "../lib/api/core/types.pb";
 import { getChildren } from "../lib/graph";
 import { RequestError, WeGONamespace } from "../lib/types";
 
@@ -23,19 +27,10 @@ export function useGetReconciledObjects(
   kinds: GroupVersionKind[]
 ) {
   const { api } = useContext(AppContext);
-  const [res, setRes] = useState({ data: null, isLoading: false, error: null });
 
-  useEffect(() => {
-    if (!name || !namespace || !type || kinds.length === 0) {
-      return;
-    }
-
-    setRes({ ...res, isLoading: true });
-
-    getChildren(api, name, namespace, kinds)
-      .then((res) => setRes({ data: res, isLoading: false, error: null }))
-      .catch((e) => setRes({ data: null, error: e, isLoading: false }));
-  }, [type, kinds]);
-
-  return res;
+  return useQuery<UnstructuredObject[], RequestError>(
+    ["reconciled_objects", { name, namespace, type, kinds }],
+    () => getChildren(api, name, namespace, kinds),
+    { retry: false, refetchOnWindowFocus: false, refetchInterval: 5000 }
+  );
 }
--- ui/pages/v2/KustomizationDetail.tsx
+++ ui/pages/v2/KustomizationDetail.tsx
@@ -10,6 +10,7 @@ import Interval from "../../components/Interval";
 import Page from "../../components/Page";
 import PageStatus from "../../components/PageStatus";
 import ReconciledObjectsTable from "../../components/ReconciledObjectsTable";
+import ReconciliationGraph from "../../components/ReconciliationGraph";
 import SourceLink from "../../components/SourceLink";
 import Spacer from "../../components/Spacer";
 import { useGetKustomization } from "../../hooks/automations";
@@ -59,10 +60,10 @@ function KustomizationDetail({ className, name }: Props) {
         <HashRouterTabs history={hashHistory} defaultPath="/details">
           <HashRouterTab name="Details" path="/details">
             <ReconciledObjectsTable
-              kinds={kustomization?.inventory}
-              automationName={kustomization?.name}
-              namespace={WeGONamespace}
               automationKind={AutomationKind.KustomizationAutomation}
+              automationName={kustomization?.name}
+              kinds={kustomization?.inventory}
+              namespace={WeGONamespace}
             />
           </HashRouterTab>
           <HashRouterTab name="Events" path="/events">
@@ -72,6 +73,15 @@ function KustomizationDetail({ className, name }: Props) {
                 name,
                 namespace: kustomization?.namespace,
               }}
+            />
+          </HashRouterTab>
+          <HashRouterTab name="Reconciliation Graph" path="/graph">
+            <ReconciliationGraph
+              automationKind={AutomationKind.KustomizationAutomation}
+              automationName={kustomization?.name}
+              kinds={kustomization?.inventory}
+              parentObject={data?.kustomization}
+              namespace={WeGONamespace}
             />
           </HashRouterTab>
         </HashRouterTabs>
