fa397f51a2ac993d52208bc10a9741f968517657
Luiz Filho <luizbafilho@gmail.com>
2022-03-31 10:25:59 -0300

    Updating sources handlers to use ClustersClient (#1841)

--- core/server/sources.go
+++ core/server/sources.go
@@ -4,25 +4,34 @@ import (
 	"context"
 
 	sourcev1 "github.com/fluxcd/source-controller/api/v1beta1"
+	"github.com/weaveworks/weave-gitops/core/clustersmngr"
 	"github.com/weaveworks/weave-gitops/core/server/types"
 	pb "github.com/weaveworks/weave-gitops/pkg/api/core"
+	"sigs.k8s.io/controller-runtime/pkg/client"
 )
 
 func (cs *coreServer) ListGitRepositories(ctx context.Context, msg *pb.ListGitRepositoriesRequest) (*pb.ListGitRepositoriesResponse, error) {
-	k8s, err := cs.k8s.Client(ctx)
-	if err != nil {
-		return nil, doClientError(err)
-	}
+	clustersClient := clustersmngr.ClientFromCtx(ctx)
 
-	l := &sourcev1.GitRepositoryList{}
+	clist := clustersmngr.NewClusteredList(func() client.ObjectList {
+		return &sourcev1.GitRepositoryList{}
+	})
 
-	if err := list(ctx, k8s, temporarilyEmptyAppName, msg.Namespace, l); err != nil {
+	if err := clustersClient.ClusteredList(ctx, clist, client.InNamespace(msg.Namespace)); err != nil {
 		return nil, err
 	}
 
 	var results []*pb.GitRepository
-	for _, repository := range l.Items {
-		results = append(results, types.GitRepositoryToProto(&repository))
+
+	for _, l := range clist.Lists() {
+		list, ok := l.(*sourcev1.GitRepositoryList)
+		if !ok {
+			continue
+		}
+
+		for _, repository := range list.Items {
+			results = append(results, types.GitRepositoryToProto(&repository))
+		}
 	}
 
 	return &pb.ListGitRepositoriesResponse{
@@ -31,20 +40,27 @@ func (cs *coreServer) ListGitRepositories(ctx context.Context, msg *pb.ListGitRe
 }
 
 func (cs *coreServer) ListHelmRepositories(ctx context.Context, msg *pb.ListHelmRepositoriesRequest) (*pb.ListHelmRepositoriesResponse, error) {
-	k8s, err := cs.k8s.Client(ctx)
-	if err != nil {
-		return nil, doClientError(err)
-	}
+	clustersClient := clustersmngr.ClientFromCtx(ctx)
 
-	l := &sourcev1.HelmRepositoryList{}
+	clist := clustersmngr.NewClusteredList(func() client.ObjectList {
+		return &sourcev1.HelmRepositoryList{}
+	})
 
-	if err := list(ctx, k8s, temporarilyEmptyAppName, msg.Namespace, l); err != nil {
+	if err := clustersClient.ClusteredList(ctx, clist, client.InNamespace(msg.Namespace)); err != nil {
 		return nil, err
 	}
 
 	var results []*pb.HelmRepository
-	for _, repository := range l.Items {
-		results = append(results, types.HelmRepositoryToProto(&repository))
+
+	for _, l := range clist.Lists() {
+		list, ok := l.(*sourcev1.HelmRepositoryList)
+		if !ok {
+			continue
+		}
+
+		for _, repository := range list.Items {
+			results = append(results, types.HelmRepositoryToProto(&repository))
+		}
 	}
 
 	return &pb.ListHelmRepositoriesResponse{
@@ -53,20 +69,27 @@ func (cs *coreServer) ListHelmRepositories(ctx context.Context, msg *pb.ListHelm
 }
 
 func (cs *coreServer) ListHelmCharts(ctx context.Context, msg *pb.ListHelmChartsRequest) (*pb.ListHelmChartsResponse, error) {
-	k8s, err := cs.k8s.Client(ctx)
-	if err != nil {
-		return nil, doClientError(err)
-	}
+	clustersClient := clustersmngr.ClientFromCtx(ctx)
 
-	l := &sourcev1.HelmChartList{}
+	clist := clustersmngr.NewClusteredList(func() client.ObjectList {
+		return &sourcev1.HelmChartList{}
+	})
 
-	if err := list(ctx, k8s, temporarilyEmptyAppName, msg.Namespace, l); err != nil {
+	if err := clustersClient.ClusteredList(ctx, clist, client.InNamespace(msg.Namespace)); err != nil {
 		return nil, err
 	}
 
 	var results []*pb.HelmChart
-	for _, repository := range l.Items {
-		results = append(results, types.HelmChartToProto(&repository))
+
+	for _, l := range clist.Lists() {
+		list, ok := l.(*sourcev1.HelmChartList)
+		if !ok {
+			continue
+		}
+
+		for _, repository := range list.Items {
+			results = append(results, types.HelmChartToProto(&repository))
+		}
 	}
 
 	return &pb.ListHelmChartsResponse{
@@ -75,20 +98,27 @@ func (cs *coreServer) ListHelmCharts(ctx context.Context, msg *pb.ListHelmCharts
 }
 
 func (cs *coreServer) ListBuckets(ctx context.Context, msg *pb.ListBucketRequest) (*pb.ListBucketsResponse, error) {
-	k8s, err := cs.k8s.Client(ctx)
-	if err != nil {
-		return nil, doClientError(err)
-	}
+	clustersClient := clustersmngr.ClientFromCtx(ctx)
 
-	l := &sourcev1.BucketList{}
+	clist := clustersmngr.NewClusteredList(func() client.ObjectList {
+		return &sourcev1.BucketList{}
+	})
 
-	if err := list(ctx, k8s, temporarilyEmptyAppName, msg.Namespace, l); err != nil {
+	if err := clustersClient.ClusteredList(ctx, clist, client.InNamespace(msg.Namespace)); err != nil {
 		return nil, err
 	}
 
 	var results []*pb.Bucket
-	for _, bucket := range l.Items {
-		results = append(results, types.BucketToProto(&bucket))
+
+	for _, l := range clist.Lists() {
+		list, ok := l.(*sourcev1.BucketList)
+		if !ok {
+			continue
+		}
+
+		for _, bucket := range list.Items {
+			results = append(results, types.BucketToProto(&bucket))
+		}
 	}
 
 	return &pb.ListBucketsResponse{
