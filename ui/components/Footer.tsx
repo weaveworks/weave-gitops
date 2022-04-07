716d92e1fc1741c028c0bb0a965196cfaa9cc02c
Joshua Israel <joshua.israel@weave.works>
2022-04-07 12:36:11 -0400

    update with copyright and version

--- ui/components/Footer.tsx
+++ ui/components/Footer.tsx
@@ -1,19 +1,39 @@
 import * as React from "react";
 import styled from "styled-components";
+import p from "../../package.json";
+import Flex from "./Flex";
 import Link from "./Link";
 
 type Props = {
   className?: string;
 };
 
+const RightFoot = styled(Flex)`
+  padding-right: ${(props) => props.theme.spacing.medium};
+  width: 275px;
+`;
+
 function Footer({ className }: Props) {
   return (
-    <footer className={className}>
-      Need help? Contact us at{" "}
-      <Link newTab href="mailto:support@weave.works">
-        support@weave.works
-      </Link>
-    </footer>
+    <Flex as="footer" wide between className={className}>
+      <Flex>
+        Need help? Contact us at{" "}
+        <Link newTab href="mailto:support@weave.works">
+          support@weave.works
+        </Link>
+      </Flex>
+      <RightFoot>
+        {process.env.NODE_ENV !== "test" && (
+          <Link
+            newTab
+            href={`https://github.com/weaveworks/weave-gitops/releases/tag/v${p.version}`}
+          >
+            v{p.version}
+          </Link>
+        )}
+        © 2022 Weaveworks
+      </RightFoot>
+    </Flex>
   );
 }
 
