495133cc9cd2be9f2b000db27a060b4cc91389a9
Jose C Ordaz <josecordaz@gmail.com>
2022-04-20 14:35:32 -0700

    Added more comments to clarify logic

--- core/server/pagination.go
+++ core/server/pagination.go
@@ -20,6 +20,9 @@ func GetNextPage(namespaceList []v1.Namespace, pageSize int32, pageToken string,
 	currentK8sPageToken := ""
 	itemsLeft := pageSize
 
+	// If there is information in pageToken we need to decode it to know the exact
+	// place we are going to start querying from. We will get the namespace and the
+	// k8s continue token, meaning "which namespace should we fetch from next and from which page"
 	if pageToken != "" {
 		var pageTokenInfo PageTokenInfo
 
@@ -32,6 +35,8 @@ func GetNextPage(namespaceList []v1.Namespace, pageSize int32, pageToken string,
 		currentK8sPageToken = pageTokenInfo.K8sPageToken
 	}
 
+	// We don't need to iterate over namespaces from the same place all the time
+	// It all will depend on the namespace index coming in pageToken parameter.
 	for nsIndex, ns := range namespaceList[currentNamespaceIndex:] {
 		nextPageToken, pageLen, err := getRawPage(ns.Name, itemsLeft, currentK8sPageToken)
 		if err != nil {
@@ -41,6 +46,11 @@ func GetNextPage(namespaceList []v1.Namespace, pageSize int32, pageToken string,
 		itemsLeft = itemsLeft - pageLen
 		globalNamespaceIndex := nsIndex + currentNamespaceIndex
 
+		// We will return here every time k8s api responds with a NOT empty string in nextPageToken.
+		// This is because the k8s api responds that way when we have fetched the resources we wanted,
+		// in this case itemsLeft.
+		// When will it fall in this condition?:
+		// 1.- We have fetched the itemLeft number of resources AND there are more items to fetch in this particular namespace
 		if nextPageToken != "" {
 			newPageToken, err = getPageTokenInfoBase64(globalNamespaceIndex, nextPageToken, ns.Name)
 			if err != nil {
@@ -50,6 +60,14 @@ func GetNextPage(namespaceList []v1.Namespace, pageSize int32, pageToken string,
 			break
 		}
 
+		// There might be cases where we requested exactly the number of resources in the namespace.
+		// In this case nextPageToken will be empty forcing us to verify in a different way
+		// if we already have fetched the resources requested. That is why we check for itemsLeft here.
+		// But only as long as there are more namespaces to fetch, otherwise we need to return an empty string
+		// Which will happen in the next attempt of the namespace loop in the iteration.
+		// When will it fall in this condition?:
+		//  1.- We have fetched the requested number of resources and there is no more items to fetch in the current namespace.
+		// But there are more namespaces to fetch from in `namespaceList`
 		if itemsLeft == 0 && globalNamespaceIndex+1 < len(namespaceList) {
 			newPageToken, err = getPageTokenInfoBase64(globalNamespaceIndex+1, "", namespaceList[globalNamespaceIndex+1].Name)
 			if err != nil {
@@ -58,7 +76,12 @@ func GetNextPage(namespaceList []v1.Namespace, pageSize int32, pageToken string,
 
 			break
 		}
+
+		// If we haven't fetched the items requested in PageSize we need to keep fetching from the next namespace
 	}
+	// At this point it doesn't matter the value of newPageToken
+	// If it is empty it means there are no more namespaces to fetch from
+	// and we have fetched all the resources we could.
 
 	return newPageToken, nil
 }
