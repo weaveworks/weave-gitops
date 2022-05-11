f868b970eec614256cab7b2cddc77921fbe57bd1
AlinaGoaga <goaga.alina@gmail.com>
2022-05-11 13:47:20 +0100

    Install Lottie and use it for Sign In Background

---
+++ ui/components/AnimatedBackground/LottieWrapper.tsx
@@ -0,0 +1,27 @@
+import React, { useEffect, useState } from "react";
+import Lottie from "react-lottie-player/dist/LottiePlayerLight";
+
+const LottieWrapper = () => {
+  const [animationData, setAnimationData] = useState<any>();
+
+  useEffect(() => {
+    import(`../../images/SignInBackground.json`).then(setAnimationData);
+  }, []);
+
+  return (
+    <Lottie
+      play
+      loop={false}
+      speed={0.3}
+      animationData={animationData}
+      rendererSettings={{ preserveAspectRatio: "xMidYMid slice" }}
+      style={{
+        width: "100%",
+        height: "100%",
+        position: "absolute",
+        zIndex: -999,
+      }}
+    />
+  );
+};
+export default LottieWrapper;
--- ui/pages/SignIn.tsx
+++ ui/pages/SignIn.tsx
@@ -4,17 +4,15 @@ import * as React from "react";
 import { Redirect } from "react-router-dom";
 import styled from "styled-components";
 import Alert from "../components/Alert";
+import AnimatedBackground from "../components/AnimatedBackground";
 import Button from "../components/Button";
 import Flex from "../components/Flex";
 import LoadingPage from "../components/LoadingPage";
 import { Auth } from "../contexts/AuthContext";
+import { FeatureFlags } from "../contexts/FeatureFlags";
 import { useFeatureFlags } from "../hooks/featureflags";
 import images from "../lib/images";
 import { theme } from "../lib/theme";
-
-export const SignInPageWrapper = styled(Flex)`
-  background: url(${images.signInBackground});
-`;
 
 export const FormWrapper = styled(Flex)`
   background-color: ${(props) => props.theme.colors.white};
@@ -45,6 +43,7 @@ const Footer = styled(Flex)`
 `;
 
 const AlertWrapper = styled(Alert)`
+  width: auto;
   .MuiAlert-root {
     width: 470px;
     margin-bottom: ${(props) => props.theme.spacing.small};
@@ -62,12 +61,18 @@ const DocsWrapper = styled(Flex)`
 function SignIn() {
   const flags = useFeatureFlags();
 
+  const { loading, error } = React.useContext(FeatureFlags);
+
   if (flags.WEAVE_GITOPS_AUTH_ENABLED === false) {
     return <Redirect to="applications" />;
   }
 
   const formRef = React.useRef<HTMLFormElement>();
-  const { signIn, error, loading } = React.useContext(Auth);
+  const {
+    signIn,
+    error: authError,
+    loading: authLoading,
+  } = React.useContext(Auth);
   const [password, setPassword] = React.useState<string>("");
   const [username, setUsername] = React.useState<string>("");
   const [showPassword, setShowPassword] = React.useState<boolean>(false);
@@ -81,90 +86,120 @@ function SignIn() {
 
   const handleUserPassSubmit = () => signIn({ username, password });
 
+  const authOptions = (
+    <>
+      {error && (
+        <Alert
+          severity="error"
+          title="Error retrieving auth setup details"
+          message={String(error)}
+          center
+        />
+      )}
+      {flags.OIDC_AUTH ? (
+        <Flex wide center>
+          <Button
+            type="submit"
+            onClick={(e) => {
+              e.preventDefault();
+              handleOIDCSubmit();
+            }}
+          >
+            LOGIN WITH OIDC PROVIDER
+          </Button>
+        </Flex>
+      ) : null}
+      {flags.OIDC_AUTH && flags.CLUSTER_USER_AUTH ? (
+        <Divider variant="middle" style={{ margin: theme.spacing.base }} />
+      ) : null}
+      {flags.CLUSTER_USER_AUTH ? (
+        <form
+          ref={formRef}
+          onSubmit={(e) => {
+            e.preventDefault();
+            handleUserPassSubmit();
+          }}
+        >
+          <Flex center align>
+            <Input
+              onChange={(e) => setUsername(e.currentTarget.value)}
+              id="email"
+              type="text"
+              placeholder="Username"
+              value={username}
+            />
+          </Flex>
+          <Flex center align>
+            <Input
+              onChange={(e) => setPassword(e.currentTarget.value)}
+              required
+              id="password"
+              placeholder="Password"
+              type={showPassword ? "text" : "password"}
+              value={password}
+              endAdornment={
+                <InputAdornment position="end">
+                  <IconButton
+                    aria-label="toggle password visibility"
+                    onClick={() => setShowPassword(!showPassword)}
+                  >
+                    {showPassword ? <Visibility /> : <VisibilityOff />}
+                  </IconButton>
+                </InputAdornment>
+              }
+            />
+          </Flex>
+          <Flex center>
+            {!authLoading ? (
+              <Button type="submit" style={{ marginTop: theme.spacing.medium }}>
+                CONTINUE
+              </Button>
+            ) : (
+              <div style={{ margin: theme.spacing.medium }}>
+                <LoadingPage />
+              </div>
+            )}
+          </Flex>
+        </form>
+      ) : null}
+    </>
+  );
+
   return (
-    <SignInPageWrapper tall wide center align column>
-      <FormWrapper center align wrap>
-        {error && (
-          <AlertWrapper
-            severity="error"
-            title="Error signin in"
-            message={`${String(error.status)} ${error.statusText}`}
-            center
-          />
-        )}
+    <Flex
+      tall
+      wide
+      center
+      align
+      column
+      style={{
+        height: "100vh",
+        width: "100vw",
+      }}
+    >
+      <AnimatedBackground />
+      {/* <LottieWrapper /> */}
+      {authError && (
+        <AlertWrapper
+          severity="error"
+          title="Error signin in"
+          message={`${String(authError.status)} ${authError.statusText}`}
+          center
+        />
+      )}
+      <FormWrapper
+        center
+        align
+        wrap
+        style={{
+          zIndex: 999,
+        }}
+      >
         <div>
           <Logo wide center>
             <img src={images.weaveLogo} />
           </Logo>
-          {flags.OIDC_AUTH ? (
-            <Flex wide center>
-              <Button
-                type="submit"
-                onClick={(e) => {
-                  e.preventDefault();
-                  handleOIDCSubmit();
-                }}
-              >
-                LOGIN WITH OIDC PROVIDER
-              </Button>
-            </Flex>
-          ) : null}
-          {flags.OIDC_AUTH && flags.CLUSTER_USER_AUTH ? (
-            <Divider variant="middle" style={{ margin: theme.spacing.base }} />
-          ) : null}
-          {flags.CLUSTER_USER_AUTH ? (
-            <form
-              ref={formRef}
-              onSubmit={(e) => {
-                e.preventDefault();
-                handleUserPassSubmit();
-              }}
-            >
-              <Flex center align>
-                <Input
-                  onChange={(e) => setUsername(e.currentTarget.value)}
-                  id="email"
-                  type="text"
-                  placeholder="Username"
-                  value={username}
-                />
-              </Flex>
-              <Flex center align>
-                <Input
-                  onChange={(e) => setPassword(e.currentTarget.value)}
-                  required
-                  id="password"
-                  placeholder="Password"
-                  type={showPassword ? "text" : "password"}
-                  value={password}
-                  endAdornment={
-                    <InputAdornment position="end">
-                      <IconButton
-                        aria-label="toggle password visibility"
-                        onClick={() => setShowPassword(!showPassword)}
-                      >
-                        {showPassword ? <Visibility /> : <VisibilityOff />}
-                      </IconButton>
-                    </InputAdornment>
-                  }
-                />
-              </Flex>
-              <Flex center>
-                {!loading ? (
-                  <Button
-                    type="submit"
-                    style={{ marginTop: theme.spacing.medium }}
-                  >
-                    CONTINUE
-                  </Button>
-                ) : (
-                  <div style={{ margin: theme.spacing.medium }}>
-                    <LoadingPage />
-                  </div>
-                )}
-              </Flex>
-            </form>
-          ) : null}
+          {loading ? <LoadingPage /> : authOptions}
           <DocsWrapper center align>
             Need help? Have a look at the&nbsp;
             <a
@@ -180,7 +215,7 @@ function SignIn() {
           <img src={images.signInWheel} />
         </Footer>
       </FormWrapper>
-    </SignInPageWrapper>
+    </Flex>
   );
 }
 
