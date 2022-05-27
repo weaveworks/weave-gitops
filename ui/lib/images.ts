a81d358bf4507e1285613e731d287e9802cd883f
Joshua Israel <joshua.israel@weave.works>
2022-05-27 17:46:43 -0400

    update logo and title bar

--- ui/lib/images.ts
+++ ui/lib/images.ts
@@ -1,16 +1,16 @@
 /* eslint-disable */
 // @ts-nocheck
 
-import bg from "data-url:../images/General-backgroundok.png";
 import failedSrc from "data-url:../images/failed.svg";
-import fancyCardBackground from "data-url:../images/fancy-card-background.svg";
 import fancyCardBackgroundBlue from "data-url:../images/fancy-card-background-blue.svg";
 import fancyCardBackgroundOrange from "data-url:../images/fancy-card-background-orange.svg";
+import fancyCardBackground from "data-url:../images/fancy-card-background.svg";
+import bg from "data-url:../images/General-backgroundok.png";
 import logoSrc from "data-url:../images/logo.svg";
 import signInWheel from "data-url:../images/SignInWheel.svg";
 import successSrc from "data-url:../images/success.svg";
 import suspendedSrc from "data-url:../images/suspended.svg";
-import titleSrc from "data-url:../images/Title.svg";
+import titleSrc from "data-url:../images/title.svg";
 import weaveLogo from "data-url:../images/WeaveLogo.svg";
 
 export default {
