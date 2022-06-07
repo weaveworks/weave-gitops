d4fd23b2833544df60ea8719ede563ea4dfc7699
Joshua Israel <65822698+joshri@users.noreply.github.com>
2022-06-07 16:07:30 -0400

    Add number lines to yaml tab (#2273)
    * yaml styling for number line
    
    * overflow scroll
    
    * remove log
    
    * added width to numbers to make indentation work for three digits
    
    * support 4 digits

--- ui/components/YamlView.tsx
+++ ui/components/YamlView.tsx
@@ -4,14 +4,14 @@ import styled from "styled-components";
 type Props = {
   className?: string;
   yaml: string;
-}
+};
 
 function YamlView({ yaml, className }: Props) {
   return (
     <pre className={className}>
-      <code>
-        {yaml}
-      </code>
+      {yaml.split("\n").map((yaml, index) => (
+        <code key={index}>{yaml}</code>
+      ))}
     </pre>
   );
 }
@@ -19,6 +19,36 @@ function YamlView({ yaml, className }: Props) {
 export default styled(YamlView).attrs({
   className: YamlView.name,
 })`
-  font-size: ${props => props.theme.fontSizes.small};
-  white-space: break-spaces;
+  width: calc(100% - ${(props) => props.theme.spacing.medium});
+  font-size: ${(props) => props.theme.fontSizes.small};
+  border: 1px solid ${(props) => props.theme.colors.neutral20};
+  border-radius: 8px;
+  padding: ${(props) => props.theme.spacing.small};
+  overflow: scroll;
+  pre {
+    white-space: pre-wrap;
+  }
+
+  pre::before {
+    counter-reset: listing;
+  }
+
+  code {
+    counter-increment: listing;
+    text-align: left;
+    float: left;
+    clear: left;
+  }
+
+  code::before {
+    width: 28px;
+    color: ${(props) => props.theme.colors.primary10};
+    content: counter(listing);
+    display: inline-block;
+    float: left;
+    height: auto;
+    padding-left: auto;
+    margin-right: ${(props) => props.theme.spacing.small};
+    text-align: right;
+  }
 `;
