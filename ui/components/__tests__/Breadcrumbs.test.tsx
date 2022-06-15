af9bd5145d81d8bb21465fa67ad41dba770225ba
Olga Pudrovska <opudrovs@gmail.com>
2022-06-15 06:45:31 +0200

    Fix the tests and re-generate the snapshots for the `Metadata` and `Breadcrumbs` components.

--- ui/components/__tests__/Breadcrumbs.test.tsx
+++ ui/components/__tests__/Breadcrumbs.test.tsx
@@ -1,43 +1,46 @@
+import "jest-styled-components";
 import React from "react";
 import renderer from "react-test-renderer";
 import { createMockClient, withContext, withTheme } from "../../lib/test-utils";
 import Breadcrumbs from "../Breadcrumbs";
 
-describe("snapshots", () => {
-  it("renders", () => {
-    const tree = renderer
-      .create(
-        withTheme(
-          withContext(<Breadcrumbs />, "/applications", {
-            applicationsClient: createMockClient({}),
-          })
+describe("Breadcrumbs", () => {
+  describe("snapshots", () => {
+    it("renders", () => {
+      const tree = renderer
+        .create(
+          withTheme(
+            withContext(<Breadcrumbs />, "/applications", {
+              applicationsClient: createMockClient({}),
+            })
+          )
         )
-      )
-      .toJSON();
-    expect(tree).toMatchSnapshot();
-  });
-  it("renders child route", () => {
-    const tree = renderer
-      .create(
-        withTheme(
-          withContext(<Breadcrumbs />, "/kustomization?name=flux", {
-            applicationsClient: createMockClient({}),
-          })
+        .toJSON();
+      expect(tree).toMatchSnapshot();
+    });
+    it("renders child route", () => {
+      const tree = renderer
+        .create(
+          withTheme(
+            withContext(<Breadcrumbs />, "/kustomization?name=flux", {
+              applicationsClient: createMockClient({}),
+            })
+          )
         )
-      )
-      .toJSON();
-    expect(tree).toMatchSnapshot();
-  });
-  it("renders on the root page", () => {
-    const tree = renderer
-      .create(
-        withTheme(
-          withContext(<Breadcrumbs />, "/", {
-            applicationsClient: createMockClient({}),
-          })
+        .toJSON();
+      expect(tree).toMatchSnapshot();
+    });
+    it("renders on the root page", () => {
+      const tree = renderer
+        .create(
+          withTheme(
+            withContext(<Breadcrumbs />, "/", {
+              applicationsClient: createMockClient({}),
+            })
+          )
         )
-      )
-      .toJSON();
-    expect(tree).toMatchSnapshot();
+        .toJSON();
+      expect(tree).toMatchSnapshot();
+    });
   });
 });
