4930df7097835e452c054b897942fe9d739d1968
Joshua Israel <joshua.israel@weave.works>
2022-06-30 14:01:17 -0400

    slightly refactored convertImage

--- ui/lib/utils.ts
+++ ui/lib/utils.ts
@@ -130,33 +130,25 @@ export const convertImage = (image: string) => {
   //Github GHCR or Google GCR
   if (prefix === "ghcr.io" || prefix === "gcr.io") return "https://" + noTag;
   //Quay.io
-  else if (prefix === "quay.io") {
+  if (prefix === "quay.io") {
     url = "https://quay.io/repository/";
     return makeUrl(url, split);
   }
   //complex docker prefix case
-  else if (prefix === "docker.io") {
+  if (prefix === "docker.io") {
     url = "https://hub.docker.com/r/";
     //library alias
     if (split[0] === "library") return url + "_/" + split[1];
     //global
-    else if (!split[1]) return url + "_/" + split[0];
+    if (!split[1]) return url + "_/" + split[0];
     //namespaced
-    else {
-      return makeUrl(url, split);
-    }
+    return makeUrl(url, split);
   }
   //docker without prefix
-  else if (prefix === "library")
-    return "https://hub.docker.com/r/_/" + split[0];
+  if (prefix === "library") return "https://hub.docker.com/r/_/" + split[0];
   //this one's at risk if we have to add others - global docker images can just be one word apparently
-  else if (
-    !prefix.includes("public.ecr.aws") &&
-    !prefix.includes("amazonaws.com")
-  ) {
-    if (!split[0]) return "https://hub.docker.com/r/_/" + noTag;
-    else {
-      return "https://hub.docker.com/r/" + noTag;
-    }
-  } else return "";
+  if (!split[0]) {
+    return "https://hub.docker.com/r/_/" + noTag;
+  }
+  return "https://hub.docker.com/r/" + noTag;
 };
