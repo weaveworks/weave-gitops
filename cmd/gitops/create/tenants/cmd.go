199d328f92e9da33921b7f95878c93147893c92f
Sara El-Zayat <sara@weave.works>
2022-07-11 02:49:57 +0200

    remove comments

--- cmd/gitops/create/tenants/cmd.go
+++ cmd/gitops/create/tenants/cmd.go
@@ -1,9 +1,12 @@
 package tenants
 
 import (
+	"context"
+	"fmt"
 	"os"
 
 	"github.com/spf13/cobra"
+	"github.com/weaveworks/weave-gitops/pkg/kube"
 	"github.com/weaveworks/weave-gitops/pkg/tenancy"
 )
 
@@ -29,20 +32,20 @@ func init() {
 
 func createTenantsCmdRunE() func(*cobra.Command, []string) error {
 	return func(cmd *cobra.Command, args []string) error {
-		// ctx := context.Background()
+		ctx := context.Background()
 
-		// kubeClient, err := kube.NewKubeHTTPClient()
-		// if err != nil {
-		// 	return fmt.Errorf("failed to create kube client: %w", err)
-		// }
+		kubeClient, err := kube.NewKubeHTTPClient()
+		if err != nil {
+			return fmt.Errorf("failed to create kube client: %w", err)
+		}
 
-		// err = tenancy.CreateTenants(ctx, flags.filename, kubeClient)
-		// if err != nil {
-		// 	return err
-		// }
+		err = tenancy.CreateTenants(ctx, flags.filename, kubeClient)
+		if err != nil {
+			return err
+		}
 
 		if flags.export {
-			err := tenancy.ExportTenants(flags.filename, os.Stdout)
+			err = tenancy.ExportTenants(flags.filename, os.Stdout)
 			if err != nil {
 				return err
 			}
