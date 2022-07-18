03ec1a5ef8d1f25ffbb6bf1b2f1b5db52baf7220
Jordan Pellizzari <jordan@weave.works>
2022-07-18 10:14:47 -0700

    Export YamlView

--- ui/index.ts
+++ ui/index.ts
@@ -2,11 +2,12 @@ import AutomationsTable from "./components/AutomationsTable";
 import BucketDetail from "./components/BucketDetail";
 import Button from "./components/Button";
 import DataTable, { SortType } from "./components/DataTable";
+import EventsTable from "./components/EventsTable";
 import FilterableTable, {
-  filterConfig,
-  FilterConfigCallback,
   filterByStatusCallback,
   filterByTypeCallback,
+  filterConfig,
+  FilterConfigCallback,
 } from "./components/FilterableTable";
 import Flex from "./components/Flex";
 import FluxRuntime from "./components/FluxRuntime";
@@ -17,21 +18,25 @@ import HelmChartDetail from "./components/HelmChartDetail";
 import HelmReleaseDetail from "./components/HelmReleaseDetail";
 import HelmRepositoryDetail from "./components/HelmRepositoryDetail";
 import Icon, { IconType } from "./components/Icon";
+import InfoList from "./components/InfoList";
 import Interval from "./components/Interval";
 import KubeStatusIndicator from "./components/KubeStatusIndicator";
 import KustomizationDetail from "./components/KustomizationDetail";
 import LoadingPage from "./components/LoadingPage";
+import Metadata from "./components/Metadata";
 import Page from "./components/Page";
 import RepoInputWithAuth from "./components/RepoInputWithAuth";
 import SourcesTable from "./components/SourcesTable";
+import SubRouterTabs, { RouterTab } from "./components/SubRouterTabs";
 import Timestamp from "./components/Timestamp";
 import UserSettings from "./components/UserSettings";
+import YamlView from "./components/YamlView";
 import AppContextProvider from "./contexts/AppContext";
+import AuthContextProvider, { Auth, AuthCheck } from "./contexts/AuthContext";
+import CallbackStateContextProvider from "./contexts/CallbackStateContext";
 import CoreClientContextProvider, {
   UnAuthorizedInterceptor,
 } from "./contexts/CoreClientContext";
-import AuthContextProvider, { Auth, AuthCheck } from "./contexts/AuthContext";
-import CallbackStateContextProvider from "./contexts/CallbackStateContext";
 import {
   Automation,
   useGetHelmRelease,
@@ -41,10 +46,12 @@ import {
 import { useFeatureFlags } from "./hooks/featureflags";
 import { useListFluxRuntimeObjects } from "./hooks/flux";
 import { useIsAuthenticated } from "./hooks/gitprovider";
+import { useGetObject } from "./hooks/objects";
 import { useListSources } from "./hooks/sources";
 import { Applications as applicationsClient } from "./lib/api/applications/applications.pb";
 import { Core as coreClient } from "./lib/api/core/core.pb";
 import { FluxObjectKind } from "./lib/api/core/types.pb";
+import { fluxObjectKindToKind } from "./lib/objects";
 import {
   clearCallbackState,
   getCallbackState,
@@ -55,12 +62,6 @@ import { V2Routes } from "./lib/types";
 import { statusSortHelper } from "./lib/utils";
 import OAuthCallback from "./pages/OAuthCallback";
 import SignIn from "./pages/SignIn";
-import SubRouterTabs, { RouterTab } from "./components/SubRouterTabs";
-import InfoList from "./components/InfoList";
-import EventsTable from "./components/EventsTable";
-import { useGetObject } from "./hooks/objects";
-import Metadata from "./components/Metadata";
-import { fluxObjectKindToKind } from "./lib/objects";
 
 export {
   AppContextProvider,
@@ -126,4 +127,5 @@ export {
   useListFluxRuntimeObjects,
   UserSettings,
   V2Routes,
+  YamlView,
 };
