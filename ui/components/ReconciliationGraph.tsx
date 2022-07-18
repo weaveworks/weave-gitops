be6c7b3ef55b890b0071e003d07791ca7a479707
Joshua Israel <joshua.israel@weave.works>
2022-07-18 15:08:59 -0400

    default aka not working collapsible

--- ui/components/ReconciliationGraph.tsx
+++ ui/components/ReconciliationGraph.tsx
@@ -126,6 +126,7 @@ function ReconciliationGraph({
   };
 
   const MakeGraph = ({ width, height, rootNode }: GraphProps) => {
+    const svgRef = React.useRef();
     //add source to tree
     const rootNodeWithSource = {
       ...source,
@@ -133,54 +134,141 @@ function ReconciliationGraph({
       children: [rootNode],
     };
     const root = d3.hierarchy(rootNodeWithSource);
-    const links = root.links();
-    const svgRef = React.useRef();
-    const dx = 650;
-    const dy = 200;
-
-    //returns function
+    const dx = 500;
+    const dy = width / 6;
     const tree = d3.tree().nodeSize([dx, dy]);
+    const diagonal = d3
+      .linkHorizontal()
+      .x((d) => d.y)
+      .y((d) => d.x);
+    const margin = { top: 10, right: 120, bottom: 10, left: 40 };
 
-    let x0 = Infinity;
-    let x1 = -x0;
-    tree(root).each((d) => {
-      if (d.x > x1) x1 = d.x;
-      if (d.x < x0) x0 = d.x;
+    root.x0 = dy / 2;
+    root.y0 = 0;
+    root.descendants().forEach((d, i) => {
+      d.id = i;
+      d._children = d.children;
+      if (d.depth && d.data.name.length !== 7) d.children = null;
     });
 
     React.useEffect(() => {
-      console.log(x1 - x0 + dx * 2);
-      const svg = d3.select(svgRef.current);
-      // .attr("viewBox", [0, 0, 1000, x1 - x0 + dx * 2]);
+      const svg = d3
+        .select(svgRef.current)
+        .attr("viewBox", [-margin.top, -margin.bottom, width, dx])
+        .style("font", "10px sans-serif")
+        .style("user-select", "none");
 
-      const g = svg.append("g");
-
-      const link = g
+      const gLink = svg
         .append("g")
-        .attr("stroke", "#999")
-        .attr("stroke-opacity", 0.6)
-        .selectAll("line")
-        .data(links)
-        .join("line")
-        .attr("x1", (edge) => edge.source.x)
-        .attr("x2", (edge) => edge.target.x)
-        .attr("y1", (edge) => edge.source.y)
-        .attr("y2", (edge) => edge.target.y);
+        .attr("fill", "none")
+        .attr("stroke", "#555")
+        .attr("stroke-opacity", 0.4)
+        .attr("stroke-width", 1.5);
 
-      const node = g
+      const gNode = svg
         .append("g")
-        .selectAll("g")
-        .data(root.descendants())
-        .join("g")
-        .attr("transform", (d) => `translate(${d.x},${d.y})`);
+        .attr("cursor", "pointer")
+        .attr("pointer-events", "all");
 
-      const data = node.append("foreignObject").html((d) => {
-        const html = renderToString(<NodeHtml object={d.data} />);
-        return html;
-      });
+      function update(source) {
+        const duration = d3.event && d3.event.altKey ? 2500 : 250;
+        const nodes = root.descendants().reverse();
+        const links = root.links();
+
+        // Compute the new tree layout.
+        tree(root);
+
+        let left = root;
+        let right = root;
+        root.eachBefore((node) => {
+          if (node.x < left.x) left = node;
+          if (node.x > right.x) right = node;
+        });
+
+        const height = right.x - left.x + margin.top + margin.bottom;
+
+        const transition = svg
+          .transition()
+          .duration(duration)
+          .attr("viewBox", [-margin.left, left.x - margin.top, width, height])
+          .tween(
+            "resize",
+            window.ResizeObserver ? null : () => () => svg.dispatch("toggle")
+          );
+
+        // Update the nodes…
+        const node = gNode.selectAll("g").data(nodes, (d) => d.id);
+
+        // Enter any new nodes at the parent's previous position.
+        const nodeEnter = node
+          .enter()
+          .append("g")
+          .attr("transform", (d) => `translate(${source.y0},${source.x0})`)
+          .attr("fill-opacity", 0)
+          .attr("stroke-opacity", 0)
+          .on("click", (event, d) => {
+            d.children = d.children ? null : d._children;
+            update(d);
+          });
+
+        const data = nodeEnter.append("foreignObject").html((d) => {
+          const html = renderToString(<NodeHtml object={d.data} />);
+          return html;
+        });
+
+        // Transition nodes to their new position.
+        const nodeUpdate = node
+          .merge(nodeEnter)
+          .transition(transition)
+          .attr("transform", (d) => `translate(${d.y},${d.x})`)
+          .attr("fill-opacity", 1)
+          .attr("stroke-opacity", 1);
+
+        // Transition exiting nodes to the parent's new position.
+        const nodeExit = node
+          .exit()
+          .transition(transition)
+          .remove()
+          .attr("transform", (d) => `translate(${source.y},${source.x})`)
+          .attr("fill-opacity", 0)
+          .attr("stroke-opacity", 0);
+
+        // Update the links…
+        const link = gLink.selectAll("path").data(links, (d) => d.target.id);
+
+        // Enter any new links at the parent's previous position.
+        const linkEnter = link
+          .enter()
+          .append("path")
+          .attr("d", (d) => {
+            const o = { x: source.x0, y: source.y0 };
+            return diagonal({ source: o, target: o });
+          });
+
+        // Transition links to their new position.
+        link.merge(linkEnter).transition(transition).attr("d", diagonal);
+
+        // Transition exiting nodes to the parent's new position.
+        link
+          .exit()
+          .transition(transition)
+          .remove()
+          .attr("d", (d) => {
+            const o = { x: source.x, y: source.y };
+            return diagonal({ source: o, target: o });
+          });
+
+        // Stash the old positions for transition.
+        root.eachBefore((d) => {
+          d.x0 = d.x;
+          d.y0 = d.y;
+        });
+      }
+
+      update(root);
     }, []);
 
-    return <svg ref={svgRef} />;
+    return <svg ref={svgRef} preserveAspectRatio="meet" />;
   };
 
   return (
