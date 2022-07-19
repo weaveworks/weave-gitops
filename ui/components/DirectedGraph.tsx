abddaeaca4a4ddd23d17c558baa8c66d617a07a6
Joshua Israel <joshua.israel@weave.works>
2022-07-19 09:03:38 -0400

    it is nothing but black squares but trust me it is a step in the right direction and this is not a cry for help

--- ui/components/DirectedGraph.tsx
+++ ui/components/DirectedGraph.tsx
@@ -1,309 +1,61 @@
-import Slider from "@material-ui/core/Slider";
 import * as d3 from "d3";
-import dagreD3 from "dagre-d3";
 import _ from "lodash";
 import * as React from "react";
 import styled from "styled-components";
-import { muiTheme } from "../lib/theme";
-import {
-  calculateNodeOffsetX,
-  calculateZoomRatio,
-  mapScaleToZoomPercent,
-  mapZoomPercentToScale,
-} from "../lib/utils";
-import Flex from "./Flex";
-import Spacer from "./Spacer";
-import Text from "./Text";
 
-export const defaultScale = 40;
-
-const loadingText = `Fetching all reconciled objects, \
-    building directional relationship, \
-    determining central node...`;
-
-const SliderFlex = styled(Flex)`
-  padding-top: ${(props) => props.theme.spacing.base};
-  min-height: 400px;
-  min-width: 60px;
-  height: 100%;
-  width: 5%;
-`;
-
-const PercentFlex = styled(Flex)`
-  color: ${muiTheme.palette.primary.main};
-  padding: 10px;
-  background: rgba(0, 179, 236, 0.1);
-  border-radius: 2px;
-`;
-
-const GraphFlex = styled(Flex)`
-  &.loading {
-    position: relative;
-  }
-`;
-
-const LoadingText = styled(Text)`
-  position: absolute;
-`;
-
-const Svg = styled.svg`
-  &.loading {
-    opacity: 0;
-    pointer-events: none;
-  }
-`;
-
-type DirectedGraphState = {
-  zoomRatio: number;
-  nodeOffsetX: number;
-};
-
-type LabelType = "html" | "text";
-type LabelShape = "rect" | "ellipse";
-
-type Props<N> = {
+type Props = {
   className?: string;
-  nodes: { id: any; data: N; label: (v: N) => string }[];
-  edges: { source: any; target: any }[];
-  scale: number;
-  labelType?: LabelType;
-  labelShape?: LabelShape;
+  descendants?: any[];
+  links?: any[];
+  hierarchy?: any;
 };
 
-function DirectedGraph<T>({
-  className,
-  nodes,
-  edges,
-  scale,
-  labelType,
-  labelShape,
-}: Props<T>) {
-  const svgRef = React.useRef();
-  const graphRef = React.useRef<D3Graph>();
+function DirectedGraph({ className, descendants, links, hierarchy }: Props) {
+  const [nodes, setNodes] = React.useState(null);
+  const [paths, setPaths] = React.useState(null);
+  const gLinks = React.useRef(null);
 
-  const initialZoomPercent = mapScaleToZoomPercent(scale);
-
-  const [zoomPercent, setZoomPercent] =
-    React.useState<number>(initialZoomPercent);
-  const [state, setState] = React.useState<DirectedGraphState>({
-    zoomRatio: calculateZoomRatio(initialZoomPercent),
-    nodeOffsetX: 0,
-  });
-
-  React.useEffect(() => {
-    if (!svgRef.current) {
-      return;
-    }
-
-    // https://github.com/jsdom/jsdom/issues/2531
-    if (process.env.NODE_ENV === "test") {
-      return;
-    }
-
-    const { zoomRatio, nodeOffsetX } = state;
-
-    const graph = new D3Graph(svgRef.current, {
-      labelShape,
-      labelType,
-      initialZoomOptions: {
-        zoomPercent: initialZoomPercent,
-        zoomRatio: zoomRatio,
-        nodeOffsetX: nodeOffsetX,
-      },
-    });
-    graph.update(nodes, edges);
-    graph.render();
-    graphRef.current = graph;
-  }, []);
+  // const scaleX = () =>
+  //   d3
+  //     .scaleLinear()
+  //     .domain(
+  //       d3.extent((d) => {
+  //         console.log(d);
+  //         d.x;
+  //       })
+  //     )
+  //     .range([20, 1000]);
+  // const scaleY = (d) =>
+  //   d3
+  //     .scaleLinear(d)
+  //     .domain(d3.extent((d) => d.y))
+  //     .range([20, 1000]);
 
   React.useEffect(() => {
-    const { nodeOffsetX } = state;
-
-    let newNodeOffsetX = 0;
-    const newZoomRatio = calculateZoomRatio(zoomPercent);
-
-    if (nodeOffsetX === 0) {
-      const d3Graph = graphRef.current.graph;
-      const graphNodes = d3Graph ? d3Graph.nodes() : null;
-      const rootNode = graphNodes
-        ? d3Graph.node(graphNodes[graphNodes.length - 1])
-        : null;
-
-      newNodeOffsetX = calculateNodeOffsetX(
-        rootNode,
-        zoomPercent,
-        newZoomRatio
+    setNodes(descendants);
+    d3.select(gLinks.current)
+      .selectAll("path")
+      .data(links)
+      .join("path")
+      .attr(
+        "d",
+        d3
+          .linkHorizontal()
+          .source((d) => [d.x, d.y])
+          .target((d) => [d.x, d.y])
       );
-
-      setState({
-        nodeOffsetX: newNodeOffsetX,
-        zoomRatio: newZoomRatio,
-      });
-    }
-
-    graphRef.current.zoom({
-      zoomPercent,
-      zoomRatio: newZoomRatio,
-      nodeOffsetX: nodeOffsetX || newNodeOffsetX,
-    });
-  }, [zoomPercent]);
-
-  React.useEffect(() => {
-    graphRef.current.update(nodes, edges);
-    graphRef.current.render();
-  }, [nodes, edges]);
-
-  const { nodeOffsetX } = state;
-
-  const isLoadingGraph = nodeOffsetX === 0;
-
-  const loadingClassName = isLoadingGraph ? "loading" : "";
+  }, [descendants, links]);
 
   return (
-    <GraphFlex wide tall className={`${className} ${loadingClassName}`}>
-      {isLoadingGraph && <LoadingText>{loadingText}</LoadingText>}
-
-      <Svg
-        viewBox="0 0 100 100"
-        preserveAspectRatio="xMidYMid meet"
-        ref={svgRef}
-        className={loadingClassName}
-      />
-
-      <Flex tall>
-        <SliderFlex column center align>
-          <Slider
-            onChange={(_, value: number) =>
-              setZoomPercent(mapScaleToZoomPercent(value))
-            }
-            defaultValue={scale}
-            orientation="vertical"
-            aria-label="zoom"
-          />
-          <Spacer padding="base" />
-          <PercentFlex>{mapZoomPercentToScale(zoomPercent)}%</PercentFlex>
-        </SliderFlex>
-      </Flex>
-    </GraphFlex>
+    <svg width={1000} height={1000}>
+      <g>
+        {_.map(descendants, (d) => {
+          return <rect width={100} height={100} x={d.x} y={d.y}></rect>;
+        })}
+      </g>
+      <g ref={gLinks} />
+    </svg>
   );
 }
 
-export default styled(DirectedGraph)`
-  overflow: hidden;
-  text {
-    font-weight: 300;
-    font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;
-    font-size: 12px;
-  }
-  .edgePath path {
-    stroke: #333;
-    stroke-width: 1.5px;
-  }
-  foreignObject {
-    display: flex;
-    flex-direction: column;
-    width: 650px;
-    height: 200px;
-    overflow: visible;
-  }
-  .MuiSlider-vertical {
-    min-height: 400px;
-  }
-  .MuiSlider-vertical .MuiSlider-track {
-    width: 6px;
-  }
-  .MuiSlider-vertical .MuiSlider-rail {
-    width: 6px;
-  }
-  .MuiSlider-vertical .MuiSlider-thumb {
-    margin-left: -9px;
-  }
-  .MuiSlider-thumb {
-    width: 24px;
-    height: 24px;
-  }
-`;
-
-type ZoomOptions = {
-  zoomPercent: number;
-  zoomRatio: number;
-  nodeOffsetX: number;
-};
-
-type D3GraphOptions = {
-  initialZoomOptions: ZoomOptions;
-  labelType?: LabelType;
-  labelShape?: LabelShape;
-};
-
-// D3 doesn't play nicely with React, as they both manipulate the DOM.
-// Since polling was added, we re-render the graph after every poll.
-// Split the D3 graphic logic into a class to avoid resetting transform state on every render.
-class D3Graph {
-  containerEl;
-  svg;
-  graph;
-  opts;
-
-  constructor(element: any, opts: D3GraphOptions) {
-    const dagreD3LibRef = dagreD3;
-
-    this.graph = new dagreD3LibRef.graphlib.Graph();
-    this.opts = opts;
-    this.containerEl = element;
-    this.svg = d3.select(element);
-    this.svg.append("g");
-    this.zoom(opts.initialZoomOptions);
-  }
-
-  zoom(opts: ZoomOptions) {
-    const { zoomPercent, zoomRatio, nodeOffsetX } = opts;
-
-    const zoom = d3.zoom().on("zoom", (e) => {
-      e.transform.k = zoomRatio;
-      this.svg.select("g").attr("transform", e.transform);
-    });
-
-    this.svg
-      .call(zoom)
-      .call(
-        zoom.transform,
-        d3.zoomIdentity.translate(nodeOffsetX, 0).scale(zoomPercent)
-      )
-      .on("wheel.zoom", null);
-  }
-
-  update(nodes, edges) {
-    this.graph
-      .setGraph({
-        nodesep: 100,
-        ranksep: 150,
-        rankdir: "TB",
-      })
-      .setDefaultEdgeLabel(() => {
-        return {};
-      });
-
-    _.each(nodes, (n) => {
-      this.graph.setNode(n.id, {
-        label: n.label(n.data),
-        labelType: this.opts.labelType,
-        shape: this.opts.labelShape,
-        width: 630,
-        height: 180,
-        rx: 10,
-        ry: 10,
-        labelClass: "node-label",
-      });
-    });
-
-    _.each(edges, (e) => {
-      this.graph.setEdge(e.source, e.target, { arrowhead: "undirected" });
-    });
-  }
-
-  render() {
-    const render = new dagreD3.render();
-    render(d3.select("svg g"), this.graph);
-  }
-}
+export default styled(DirectedGraph).attrs({ className: DirectedGraph.name })``;
--- ui/components/ReconciliationGraph.tsx
+++ ui/components/ReconciliationGraph.tsx
@@ -1,6 +1,5 @@
 import * as d3 from "d3";
 import * as React from "react";
-import { renderToString } from "react-dom/server";
 import styled from "styled-components";
 import { useGetReconciledObjects } from "../hooks/flux";
 import {
@@ -11,6 +10,7 @@ import {
 import { UnstructuredObjectWithChildren } from "../lib/graph";
 import images from "../lib/images";
 import { removeKind } from "../lib/utils";
+import DirectedGraph from "./DirectedGraph";
 import Flex from "./Flex";
 import { computeReady } from "./KubeStatusIndicator";
 import { ReconciledVisualizationProps } from "./ReconciledObjectsTable";
@@ -116,258 +116,33 @@ function ReconciliationGraph({
       )
     : { data: [], error: null, isLoading: false };
 
-  const rootNode = parentObject;
-  rootNode.children = objects;
-
-  type GraphProps = {
-    width: number;
-    height: number;
-    rootNode: UnstructuredObjectWithChildren;
+  const secondNode = parentObject;
+  secondNode.children = objects;
+  const rootNode = {
+    ...source,
+    kind: removeKind(source.kind),
+    children: [secondNode],
   };
+  const root = d3.hierarchy(rootNode, (d) => d.children);
+  const makeTree = d3.tree().nodeSize([100, 100]);
+  const tree = makeTree(root);
 
-  const MakeGraph = ({ width, height, rootNode }: GraphProps) => {
-    const svgRef = React.useRef();
-    //add source to tree
-    const rootNodeWithSource = {
-      ...source,
-      kind: removeKind(source.kind),
-      children: [rootNode],
-    };
-    const root = d3.hierarchy(rootNodeWithSource);
-    const dx = 500;
-    const dy = width / 6;
-    const tree = d3.tree().nodeSize([dx, dy]);
-    const diagonal = d3
-      .linkHorizontal()
-      .x((d) => d.y)
-      .y((d) => d.x);
-    const margin = { top: 10, right: 120, bottom: 10, left: 40 };
+  const descendants = tree.descendants();
+  const links = tree.links();
 
-    root.x0 = dy / 2;
-    root.y0 = 0;
-    root.descendants().forEach((d, i) => {
-      d.id = i;
-      d._children = d.children;
-      if (d.depth && d.data.name.length !== 7) d.children = null;
-    });
-
-    React.useEffect(() => {
-      const svg = d3
-        .select(svgRef.current)
-        .attr("viewBox", [-margin.top, -margin.bottom, width, dx])
-        .style("font", "10px sans-serif")
-        .style("user-select", "none");
-
-      const gLink = svg
-        .append("g")
-        .attr("fill", "none")
-        .attr("stroke", "#555")
-        .attr("stroke-opacity", 0.4)
-        .attr("stroke-width", 1.5);
-
-      const gNode = svg
-        .append("g")
-        .attr("cursor", "pointer")
-        .attr("pointer-events", "all");
-
-      function update(source) {
-        const duration = d3.event && d3.event.altKey ? 2500 : 250;
-        const nodes = root.descendants().reverse();
-        const links = root.links();
-
-        // Compute the new tree layout.
-        tree(root);
-
-        let left = root;
-        let right = root;
-        root.eachBefore((node) => {
-          if (node.x < left.x) left = node;
-          if (node.x > right.x) right = node;
-        });
-
-        const height = right.x - left.x + margin.top + margin.bottom;
-
-        const transition = svg
-          .transition()
-          .duration(duration)
-          .attr("viewBox", [-margin.left, left.x - margin.top, width, height])
-          .tween(
-            "resize",
-            window.ResizeObserver ? null : () => () => svg.dispatch("toggle")
-          );
-
-        // Update the nodes…
-        const node = gNode.selectAll("g").data(nodes, (d) => d.id);
-
-        // Enter any new nodes at the parent's previous position.
-        const nodeEnter = node
-          .enter()
-          .append("g")
-          .attr("transform", (d) => `translate(${source.y0},${source.x0})`)
-          .attr("fill-opacity", 0)
-          .attr("stroke-opacity", 0)
-          .on("click", (event, d) => {
-            d.children = d.children ? null : d._children;
-            update(d);
-          });
-
-        const data = nodeEnter.append("foreignObject").html((d) => {
-          const html = renderToString(<NodeHtml object={d.data} />);
-          return html;
-        });
-
-        // Transition nodes to their new position.
-        const nodeUpdate = node
-          .merge(nodeEnter)
-          .transition(transition)
-          .attr("transform", (d) => `translate(${d.y},${d.x})`)
-          .attr("fill-opacity", 1)
-          .attr("stroke-opacity", 1);
-
-        // Transition exiting nodes to the parent's new position.
-        const nodeExit = node
-          .exit()
-          .transition(transition)
-          .remove()
-          .attr("transform", (d) => `translate(${source.y},${source.x})`)
-          .attr("fill-opacity", 0)
-          .attr("stroke-opacity", 0);
-
-        // Update the links…
-        const link = gLink.selectAll("path").data(links, (d) => d.target.id);
-
-        // Enter any new links at the parent's previous position.
-        const linkEnter = link
-          .enter()
-          .append("path")
-          .attr("d", (d) => {
-            const o = { x: source.x0, y: source.y0 };
-            return diagonal({ source: o, target: o });
-          });
-
-        // Transition links to their new position.
-        link.merge(linkEnter).transition(transition).attr("d", diagonal);
-
-        // Transition exiting nodes to the parent's new position.
-        link
-          .exit()
-          .transition(transition)
-          .remove()
-          .attr("d", (d) => {
-            const o = { x: source.x, y: source.y };
-            return diagonal({ source: o, target: o });
-          });
-
-        // Stash the old positions for transition.
-        root.eachBefore((d) => {
-          d.x0 = d.x;
-          d.y0 = d.y;
-        });
-      }
-
-      update(root);
-    }, []);
-
-    return <svg ref={svgRef} preserveAspectRatio="meet" />;
-  };
+  console.log(tree);
 
   return (
     <RequestStateHandler loading={isLoading} error={error}>
       <div className={className} style={{ height: "100%", width: "100%" }}>
-        <MakeGraph width={1000} height={1000} rootNode={rootNode} />
-        {/* <DirectedGraph
-          scale={defaultScale}
-          nodes={nodes}
-          edges={edges}
-          labelShape="rect"
-          labelType="html"
-        /> */}
+        <DirectedGraph
+          descendants={descendants}
+          links={links}
+          hierarchy={root}
+        />
       </div>
     </RequestStateHandler>
   );
 }
 
-export default styled(ReconciliationGraph)`
-  .node {
-    font-size: 8px;
-    width: 650px;
-    height: 200px;
-    display: flex;
-    justify-content: space-between;
-  }
-
-  rect {
-    fill: white;
-    stroke: ${(props) => props.theme.colors.neutral20};
-    stroke-width: 3;
-  }
-
-  .status {
-    display: flex;
-    align-items: center;
-  }
-
-  .kind-text {
-    overflow: hidden;
-    text-overflow: ellipsis;
-    font-size: 14px;
-  }
-
-  .status-line {
-    width: 2.5%;
-    border-radius: 10px 0px 0px 10px;
-  }
-
-  .nodeText {
-    width: 95%;
-    align-items: flex-start;
-    justify-content: space-evenly;
-  }
-
-  .Current {
-    color: ${(props) => props.theme.colors.success};
-
-    &.status-line {
-      background-color: ${(props) => props.theme.colors.success};
-    }
-  }
-
-  .InProgress {
-    color: ${(props) => props.theme.colors.suspended};
-
-    &.status-line {
-      background-color: ${(props) => props.theme.colors.suspended};
-    }
-  }
-
-  .Failed {
-    color: ${(props) => props.theme.colors.alert};
-
-    &.status-line {
-      background-color: ${(props) => props.theme.colors.alert};
-    }
-  }
-
-  .name {
-    color: ${(props) => props.theme.colors.black};
-    font-weight: 800;
-    font-size: 14px;
-    white-space: pre-wrap;
-  }
-
-  .kind {
-    color: ${(props) => props.theme.colors.neutral30};
-  }
-
-  .edgePath path {
-    stroke: ${(props) => props.theme.colors.neutral30};
-    stroke-width: 1px;
-  }
-
-  foreignObject {
-    display: flex;
-    flex-direction: column;
-    width: 650px;
-    height: 200px;
-  }
-`;
+export default styled(ReconciliationGraph)``;
