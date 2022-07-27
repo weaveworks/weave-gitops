d26f4856d486552af60f621ed17737aa812c5db8
Robin Sonefors <robin.sonefors@weave.works>
2022-07-27 13:36:24 +0100

    Remove pointless test setup

--- pkg/flux/flux_suite_test.go
+++ pkg/flux/flux_suite_test.go
@@ -5,14 +5,9 @@ import (
 
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
-	"github.com/weaveworks/weave-gitops/pkg/version"
 )
 
 func TestFlux(t *testing.T) {
 	RegisterFailHandler(Fail)
 	RunSpecs(t, "Flux Suite")
 }
-
-var _ = BeforeSuite(func() {
-	version.FluxVersion = "0.12.0"
-})
