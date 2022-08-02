90a7e6dd49860512cd666666d7086b4e146b3862
Ahmed Magdy <ahmed.magdy@magalix.com>
2022-08-02 13:16:07 +0200

    export MultiErrorRequestType to be used in EE

--- ui/index.ts
+++ ui/index.ts
@@ -60,7 +60,7 @@ import {
   getProviderToken,
 } from "./lib/storage";
 import { muiTheme, theme } from "./lib/theme";
-import { V2Routes } from "./lib/types";
+import { V2Routes, MultiRequestError } from "./lib/types";
 import { statusSortHelper, isAllowedLink } from "./lib/utils";
 import OAuthCallback from "./pages/OAuthCallback";
 import SignIn from "./pages/SignIn";
@@ -135,4 +135,5 @@ export {
   UserSettings,
   V2Routes,
   YamlView,
+  MultiRequestError,
 };
