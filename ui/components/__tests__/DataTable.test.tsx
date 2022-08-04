fa7cad5977b44921c4f754d207ee40b5a20fe2e9
Ahmed Magdy <ahussein@outlook.com>
2022-08-04 15:36:16 +0200

    remove dataTable test case related to sorting as this part has been refactored and using Lodash OrderBy instead

--- ui/components/__tests__/DataTable.test.tsx
+++ ui/components/__tests__/DataTable.test.tsx
@@ -1,9 +1,8 @@
-import { fireEvent, render, screen } from "@testing-library/react";
 import "jest-styled-components";
 import React from "react";
 import renderer from "react-test-renderer";
 import { withTheme } from "../../lib/test-utils";
-import DataTable, { SortType, sortWithType } from "../DataTable";
+import DataTable, { SortType } from "../DataTable";
 
 describe("DataTable", () => {
   const rows = [
@@ -58,112 +57,6 @@ describe("DataTable", () => {
       sortValue: ({ lastSyncedAt }) => lastSyncedAt,
     },
   ];
-
-  describe("sorting", () => {
-    describe("sortWithType", () => {
-      it("should handle sorting with case SortType.string", () => {
-        const nameSort = sortWithType(rows, false, {
-          label: "Name",
-          value: ({ name }) => <a href="/some_url">{name}</a>,
-          sortType: SortType.string,
-          sortValue: ({ name }) => name,
-        });
-        expect(nameSort[0].name).toBe("nginx");
-      });
-      it("should handle sorting with case SortType.bool", () => {
-        const boolSort = sortWithType(rows, false, {
-          label: "Status",
-          value: "status",
-          sortType: SortType.number,
-          sortValue: ({ status, suspended }) => {
-            if (suspended) return 2;
-            if (status) return 3;
-            else return 1;
-          },
-        });
-        expect(boolSort[0].status).toBe(false);
-        expect(boolSort[2].status).toBe(true);
-      });
-      it("should handle sorting with case SortType.date", () => {
-        const dateSort = sortWithType(rows, false, {
-          label: "Last Updated",
-          value: "lastUpdate",
-          sortType: SortType.date,
-          sortValue: ({ lastUpdate }) => lastUpdate,
-        });
-        expect(dateSort[0].lastUpdate).toBe("2004-01-02T15:04:05-0700");
-      });
-      it("should handle sorting with case SortType.number", () => {
-        const numberSort = sortWithType(rows, false, {
-          label: "Last Synced At",
-          value: "lastSyncedAt",
-          sortType: SortType.number,
-          sortValue: ({ lastSyncedAt }) => lastSyncedAt,
-        });
-        expect(numberSort[0].lastSyncedAt).toBe(1000);
-      });
-    });
-    it("initially sorts based on defaultSort", () => {
-      render(withTheme(<DataTable fields={fields} rows={rows} />));
-      const firstRow = screen.getAllByRole("row")[1];
-      expect(firstRow.innerHTML).toMatch(/nginx/);
-    });
-    it("reverses sort on thead click", () => {
-      render(withTheme(<DataTable fields={fields} rows={rows} />));
-
-      const nameButton = screen.getByText("Name");
-      fireEvent.click(nameButton);
-      const firstRow = screen.getAllByRole("row")[1];
-      expect(firstRow.innerHTML).toMatch(/the-cool-app/);
-    });
-    it("resets reverseSort and switches sort column on different thead click", () => {
-      render(withTheme(<DataTable fields={fields} rows={rows} />));
-      const nameButton = screen.getByText("Name");
-      fireEvent.click(nameButton);
-      const statusButton = screen.getByText("Status");
-      fireEvent.click(statusButton);
-      const firstRow = screen.getAllByRole("row")[1];
-      expect(firstRow.innerHTML).toMatch(/podinfo/);
-    });
-    it("should render text when rows is empty", () => {
-      render(withTheme(<DataTable fields={fields} rows={[]} />));
-      const firstRow = screen.getAllByRole("row")[1];
-      expect(firstRow.innerHTML).toMatch(/No/);
-    });
-    it("sorts by value when no sortValue property exists", () => {
-      const rows = [
-        {
-          name: "b",
-        },
-        {
-          name: "c",
-        },
-        {
-          name: "a",
-        },
-      ];
-
-      const fields = [
-        {
-          label: "Name",
-          value: "name",
-        },
-      ];
-
-      render(
-        withTheme(<DataTable defaultSort={0} fields={fields} rows={rows} />)
-      );
-
-      let firstRow = screen.getAllByRole("row")[1];
-      expect(firstRow.innerHTML).toMatch(/a/);
-
-      const nameButton = screen.getByText("Name");
-      fireEvent.click(nameButton);
-
-      firstRow = screen.getAllByRole("row")[1];
-      expect(firstRow.innerHTML).toMatch(/c/);
-    });
-  });
 
   describe("snapshots", () => {
     it("renders", () => {
