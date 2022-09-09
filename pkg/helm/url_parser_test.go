d28afc707ae29d1b8deb2e2e7427e2cdb445e307
sarataha <sara@weave.works>
2022-09-09 15:02:52 +0200

    Update TestParseService

--- pkg/helm/url_parser_test.go
+++ pkg/helm/url_parser_test.go
@@ -1,6 +1,8 @@
 package helm
 
 import (
+	"errors"
+	"fmt"
 	"net/url"
 	"strings"
 	"testing"
@@ -20,14 +22,18 @@ type Service struct {
 
 // ParseArtifactURL takes HelmRepository Artifact URL for a remote cluster and
 // returns the components of the URL.
-func ParseArtifactURL(serviceURL string) (*Service, error) {
-	u, err := url.Parse(serviceURL)
+func ParseArtifactURL(artifactURL string) (*Service, error) {
+	u, err := url.Parse(artifactURL)
 	if err != nil {
 		return nil, err
 	}
 
 	// Split hostname to get namespace and name.
 	host := strings.Split(u.Hostname(), ".")
+
+	if len(host) != 5 || host[2] != "svc" || u.Path == "/" {
+		return nil, fmt.Errorf("invalid artifact URL %s", artifactURL)
+	}
 
 	port := u.Port()
 	if port == "" {
@@ -44,21 +50,50 @@ func ParseArtifactURL(serviceURL string) (*Service, error) {
 }
 
 func TestParseService(t *testing.T) {
-	artifactURL := "http://source-controller.flux-system.svc.cluster.local./demo-index.yaml"
-
-	parsed, err := ParseArtifactURL(artifactURL)
-	if err != nil {
-		t.Fatal(err)
+	testCases := []struct {
+		name        string
+		artifactURL string
+		want        *Service
+		err         error
+	}{
+		{
+			"parses correctly",
+			"http://source-controller.flux-system.svc.cluster.local/demo-index.yaml",
+			&Service{
+				Scheme:    "http",
+				Namespace: "flux-system",
+				Name:      "source-controller",
+				Path:      "/demo-index.yaml",
+				Port:      "80",
+			},
+			nil,
+		},
+		{
+			"wrong url",
+			"http://github.com/example.repo",
+			nil,
+			errors.New("invalid artifact URL http://github.com/example.repo"),
+		},
+		{
+			"empty path",
+			"http://source-controller.flux-system.svc.cluster.local/",
+			nil,
+			errors.New("invalid artifact URL http://source-controller.flux-system.svc.cluster.local/"),
+		},
 	}
 
-	want := &Service{
-		Scheme:    "http",
-		Namespace: "flux-system",
-		Name:      "source-controller",
-		Path:      "/demo-index.yaml",
-		Port:      "80",
-	}
-	if diff := cmp.Diff(want, parsed); diff != "" {
-		t.Fatalf("failed to parse URL:\n%s", diff)
+	for _, tt := range testCases {
+		t.Run(tt.name, func(t *testing.T) {
+			parsed, err := ParseArtifactURL(tt.artifactURL)
+			if tt.err != nil {
+				if diff := cmp.Diff(tt.err.Error(), err.Error()); diff != "" {
+					t.Fatalf("got wrong error:\n%s", diff)
+				}
+			}
+
+			if diff := cmp.Diff(tt.want, parsed); diff != "" {
+				t.Fatalf("failed to parse URL:\n%s", diff)
+			}
+		})
 	}
 }
