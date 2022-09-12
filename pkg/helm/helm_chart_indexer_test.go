04feb6d4f26779f8694d513775de15d14c651066
Sara Elzayat <sara@weave.works>
2022-09-12 14:01:22 +0200

    Add ListChartsByCluster and ListChartsByRepositoryAndCluster

--- pkg/helm/helm_chart_indexer_test.go
+++ pkg/helm/helm_chart_indexer_test.go
@@ -17,6 +17,11 @@ type ObjectReference struct {
 	Namespace  string
 }
 
+type Chart struct {
+	Name    string
+	Version string
+}
+
 // HelmChartIndexer indexs details of Helm charts that have been seen in Helm
 // repositories.
 type HelmChartIndexer struct {
@@ -24,14 +29,15 @@ type HelmChartIndexer struct {
 }
 
 // AddChart inserts a new chart into helm_charts table.
-func (i *HelmChartIndexer) AddChart(ctx context.Context, db *sql.DB, name, version string, clusterRef types.NamespacedName, repoRef ObjectReference) error {
+func (i *HelmChartIndexer) AddChart(ctx context.Context, name, version string, clusterRef types.NamespacedName, repoRef ObjectReference) error {
 	sqlStatement := `
 INSERT INTO helm_charts (name, version,
 	repo_kind, repo_api_version, repo_name, repo_namespace,
 	cluster_name, cluster_namespace)
 VALUES ($1, $2, $3, $4, $5, $6, $7, $8)`
 
-	_, err := db.Exec(
+	_, err := i.CacheDB.ExecContext(
+		ctx,
 		sqlStatement, name, version,
 		repoRef.Kind, repoRef.APIVersion, repoRef.Name, repoRef.Namespace,
 		clusterRef.Name, clusterRef.Namespace)
@@ -58,13 +64,62 @@ func (i *HelmChartIndexer) Count(ctx context.Context) (int64, error) {
 	return count, nil
 }
 
+// ListChartsByCluster returns a list of charts filtered by cluster.
+func (i *HelmChartIndexer) ListChartsByCluster(ctx context.Context, clusterRef types.NamespacedName) ([]Chart, error) {
+	sqlStatement := `
+SELECT name, version FROM helm_charts 
+WHERE cluster_name = $1 AND cluster_namespace = $2`
+
+	rows, err := i.CacheDB.QueryContext(ctx, sqlStatement, clusterRef.Name, clusterRef.Namespace)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	var charts []Chart
+	for rows.Next() {
+		var chart Chart
+		if err := rows.Scan(&chart.Name, &chart.Version); err != nil {
+			return nil, err
+		}
+		charts = append(charts, chart)
+	}
+
+	return charts, nil
+}
+
+// ListChartsByRepositoryAndCluster returns a list of charts filtered by helm repository and cluster.
+func (i *HelmChartIndexer) ListChartsByRepositoryAndCluster(ctx context.Context, repoRef ObjectReference, clusterRef types.NamespacedName) ([]Chart, error) {
+	sqlStatement := `
+SELECT name, version FROM helm_charts 
+WHERE repo_kind = $1 AND repo_api_version = $2 AND repo_name = $3 AND repo_namespace = $4
+AND cluster_name = $5 AND cluster_namespace = $6`
+
+	rows, err := i.CacheDB.QueryContext(ctx, sqlStatement, repoRef.Kind, repoRef.APIVersion, repoRef.Name, repoRef.Namespace, clusterRef.Name, clusterRef.Namespace)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	var charts []Chart
+	for rows.Next() {
+		var chart Chart
+		if err := rows.Scan(&chart.Name, &chart.Version); err != nil {
+			return nil, err
+		}
+		charts = append(charts, chart)
+	}
+
+	return charts, nil
+}
+
 func TestHelmChartIndex(t *testing.T) {
 	db := createDB(t)
 	indexer := HelmChartIndexer{
 		CacheDB: db,
 	}
 
-	if err := indexer.AddChart(context.TODO(), db, "redis", "1.0.1",
+	if err := indexer.AddChart(context.TODO(), "redis", "1.0.1",
 		nsn("cluster1", "clusters"),
 		objref("HelmRepository", "", "bitnami-charts", "team-ns")); err != nil {
 		t.Fatal(err)
@@ -75,6 +130,70 @@ func TestHelmChartIndex(t *testing.T) {
 	}
 	if count != 1 {
 		t.Fatalf("got %d, want 1", count)
+	}
+}
+
+func TestListChartsByCluster(t *testing.T) {
+	db := createDB(t)
+	indexer := HelmChartIndexer{
+		CacheDB: db,
+	}
+
+	if err := indexer.AddChart(context.TODO(), "redis", "1.0.1",
+		nsn("cluster1", "clusters"),
+		objref("HelmRepository", "", "bitnami-charts", "team-ns")); err != nil {
+		t.Fatal(err)
+	}
+	if err := indexer.AddChart(context.TODO(), "nginx", "1.0.1",
+		nsn("cluster1", "clusters"),
+		objref("HelmRepository", "", "bitnami-charts", "team-ns")); err != nil {
+		t.Fatal(err)
+	}
+	if err := indexer.AddChart(context.TODO(), "nginx", "1.0.1",
+		nsn("cluster2", "clusters"),
+		objref("HelmRepository", "", "bitnami-charts", "team-ns")); err != nil {
+		t.Fatal(err)
+	}
+
+	charts, err := indexer.ListChartsByCluster(context.TODO(), nsn("cluster1", "clusters"))
+	if err != nil {
+		t.Fatal(err)
+	}
+	if len(charts) != 2 {
+		t.Fatalf("got %d, want 2", len(charts))
+	}
+}
+
+func TestListChartsByRepositoryAndCluster(t *testing.T) {
+	db := createDB(t)
+	indexer := HelmChartIndexer{
+		CacheDB: db,
+	}
+
+	if err := indexer.AddChart(context.TODO(), "redis", "1.0.1",
+		nsn("cluster1", "clusters"),
+		objref("HelmRepository", "", "bitnami-charts", "team-ns")); err != nil {
+		t.Fatal(err)
+	}
+	if err := indexer.AddChart(context.TODO(), "nginx", "1.0.1",
+		nsn("cluster1", "clusters"),
+		objref("HelmRepository", "", "bitnami-charts", "team-ns")); err != nil {
+		t.Fatal(err)
+	}
+	if err := indexer.AddChart(context.TODO(), "nginx", "1.0.1",
+		nsn("cluster2", "clusters"),
+		objref("HelmRepository", "", "bitnami-charts", "team-ns")); err != nil {
+		t.Fatal(err)
+	}
+
+	charts, err := indexer.ListChartsByRepositoryAndCluster(context.TODO(),
+		objref("HelmRepository", "", "bitnami-charts", "team-ns"),
+		nsn("cluster1", "clusters"))
+	if err != nil {
+		t.Fatal(err)
+	}
+	if len(charts) != 2 {
+		t.Fatalf("got %d, want 2", len(charts))
 	}
 }
 
