5856962b177b7bfcfa685a1ed6d41eb3dadd50d3
Joshua Israel <65822698+joshri@users.noreply.github.com>
2022-09-14 17:39:40 -0400

    Add help message when user has no notifications (#2744)
    * message flex in notifications
    
    * move message flex to notifications table
    
    * remove empty rows const
    
    * add HTML entity space character

--- ui/pages/v2/Notifications.tsx
+++ ui/pages/v2/Notifications.tsx
@@ -3,14 +3,12 @@ import styled from "styled-components";
 import NotificationsTable from "../../components/NotificationsTable";
 import Page from "../../components/Page";
 import { useListProviders } from "../../hooks/notifications";
-
 type Props = {
   className?: string;
 };
 
 function Notifications({ className }: Props) {
   const { data, isLoading, error } = useListProviders();
-
   return (
     <Page
       className={className}
