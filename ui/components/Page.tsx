9a5a8360bf19507cf90ffb4c3c272584ebbbf1ba
Ahmed Magdy <ahmed.magdy@weave.works>
2022-09-29 10:51:02 +0200

    use white color istead of rgba

--- ui/components/Page.tsx
+++ ui/components/Page.tsx
@@ -17,7 +17,7 @@ export type PageProps = {
 };
 
 export const Content = styled(Flex)`
-  background-color: rgba(255, 255, 255, 1);
+  background-color: ${(props) => props.theme.colors.white};
   border-radius: 10px;
   box-sizing: border-box;
   margin: 0 auto;
