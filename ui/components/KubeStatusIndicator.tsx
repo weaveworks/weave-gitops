bb4396dded42a4dc2ebd4e75117ef66b7ece1a39
Joshua Israel <joshua.israel@weave.works>
2022-10-06 10:40:31 -0400

    yep i crashed the graph but at least im fixing it

--- ui/components/KubeStatusIndicator.tsx
+++ ui/components/KubeStatusIndicator.tsx
@@ -19,7 +19,7 @@ export enum ReadyType {
 }
 
 export function computeReady(conditions: Condition[]): ReadyType {
-  if (!conditions.length) return undefined;
+  if (!conditions || !conditions[0]) return undefined;
   const readyCondition =
     _.find(conditions, (c) => c.type === "Ready") ||
     _.find(conditions, (c) => c.type === "Available");
@@ -39,7 +39,7 @@ export function computeReady(conditions: Condition[]): ReadyType {
 }
 
 export function computeMessage(conditions: Condition[]) {
-  if (!conditions.length) return undefined;
+  if (!conditions || !conditions[0]) return undefined;
   const readyCondition =
     _.find(conditions, (c) => c.type === "Ready") ||
     _.find(conditions, (c) => c.type === "Available");
