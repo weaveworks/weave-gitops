9f17e8c91ceb45d737e5ed4eb78c170728a3bb66
Robin Sonefors <robin.sonefors@weave.works>
2022-10-11 15:30:02 +0100

    Add silly little struct for paths
    I'm not sure how much of a good idea this actually is - it's
    definitely under-developed. However, I had a small breakdown while
    trying to get paths right and had to try something to sort some paths
    out.
    
    Basically, we expect the git repo to be "somewhere". Inside that git
    repository, there's a manifests directory, which is the same as the
    current working directory. Inside that directory, there's a clusters
    directory. Somewhere, probably but not necessarily inside the manifests
    directory but definitely inside the git directory, there's a workload
    directory. All of these paths can be refered to as relative paths,
    absolute paths, and some of them (in flux) should be prefixed with `./`.
    
    This bans all somethingPath variables, and puts a paths variable
    instead. That variable is a struct that has documented members so we
    can remember which path is which. There's no variable called
    `relativePath`.
    
    Inside this struct, we should check those invariants described above,
    but for the moment I mainly just copy-pasted the code we already had.

--- pkg/run/install/install.go
+++ pkg/run/install/install.go
@@ -1,42 +1,8 @@
 package install
 
 import (
-	"errors"
-	"os"
-	"path/filepath"
-
 	corev1 "k8s.io/api/core/v1"
 )
-
-// FindGitRepoDir finds git repo root directory
-func FindGitRepoDir() (string, error) {
-	gitDir := "."
-
-	for {
-		if _, err := os.Stat(filepath.Join(gitDir, ".git")); err == nil {
-			break
-		}
-
-		gitDir = filepath.Join(gitDir, "..")
-
-		if gitDir == "/" {
-			return "", errors.New("not in a git repo")
-		}
-	}
-
-	return filepath.Abs(gitDir)
-}
-
-// GetRelativePathToRootDir gets relative path to a directory from the git root. It returns an error if there's no git repo.
-func GetRelativePathToRootDir(rootDir string, path string) (string, error) {
-	absGitDir, err := filepath.Abs(rootDir)
-
-	if err != nil { // not in a git repo
-		return "", err
-	}
-
-	return filepath.Rel(absGitDir, path)
-}
 
 // isPodStatusConditionPresentAndEqual returns true when conditionType is present and equal to status.
 func isPodStatusConditionPresentAndEqual(conditions []corev1.PodCondition, conditionType corev1.PodConditionType, status corev1.ConditionStatus) bool {
