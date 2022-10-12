9e68c2275b23d5c9eb872e89bd12c6dbd6bd116c
Luiz Filho <luiz.filho@weave.works>
2022-10-12 13:17:39 -0300

    Fixing tests

--- core/clustersmngr/delegating_cache_test.go
+++ core/clustersmngr/delegating_cache_test.go
@@ -3,11 +3,13 @@ package clustersmngr_test
 import (
 	"context"
 	"testing"
+	"time"
 
 	. "github.com/onsi/gomega"
 	"github.com/weaveworks/weave-gitops/core/clustersmngr"
 	corev1 "k8s.io/api/core/v1"
 	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/util/rand"
 	"k8s.io/kubectl/pkg/scheme"
 	"sigs.k8s.io/controller-runtime/pkg/cache"
 	"sigs.k8s.io/controller-runtime/pkg/client"
@@ -20,18 +22,29 @@ func TestDelegatingCacheGet(t *testing.T) {
 	cache, err := cache.New(k8sEnv.Rest, cache.Options{})
 	g.Expect(err).To(BeNil())
 
+	ctx, cancel := context.WithTimeout(context.Background(), 1*time.Second)
+	defer cancel()
+
+	go cache.Start(ctx)
+
+	if ok := cache.WaitForCacheSync(ctx); !ok {
+		g.Fail("failed syncing client cache")
+	}
+
 	delegatingCache := clustersmngr.NewDelegatingCache(fakeReader, cache, scheme.Scheme)
 
 	ns := &corev1.Namespace{
 		ObjectMeta: v1.ObjectMeta{
-			Name: "test",
+			Name: "test" + rand.String(5),
 		},
 	}
 
-	err = delegatingCache.Get(context.Background(), client.ObjectKeyFromObject(ns), ns)
+	g.Expect(k8sEnv.Client.Create(ctx, ns)).To(Succeed())
+
+	err = delegatingCache.Get(ctx, client.ObjectKeyFromObject(ns), ns)
 	g.Expect(err).To(BeNil())
 
-	err = delegatingCache.Get(context.Background(), client.ObjectKeyFromObject(ns), ns)
+	err = delegatingCache.Get(ctx, client.ObjectKeyFromObject(ns), ns)
 	g.Expect(err).To(BeNil())
 
 	g.Expect(fakeReader.Called).To(Equal(1))
@@ -44,14 +57,31 @@ func TestDelegatingCacheList(t *testing.T) {
 	cache, err := cache.New(k8sEnv.Rest, cache.Options{})
 	g.Expect(err).To(BeNil())
 
+	ctx, cancel := context.WithTimeout(context.Background(), 1*time.Second)
+	defer cancel()
+
+	go cache.Start(ctx)
+
+	if ok := cache.WaitForCacheSync(ctx); !ok {
+		g.Fail("failed syncing client cache")
+	}
+
+	ns := &corev1.Namespace{
+		ObjectMeta: v1.ObjectMeta{
+			Name: "test" + rand.String(5),
+		},
+	}
+
+	g.Expect(k8sEnv.Client.Create(ctx, ns)).To(Succeed())
+
 	delegatingCache := clustersmngr.NewDelegatingCache(fakeReader, cache, scheme.Scheme)
 
 	nsList := &corev1.NamespaceList{}
 
-	err = delegatingCache.List(context.Background(), nsList)
+	err = delegatingCache.List(ctx, nsList)
 	g.Expect(err).To(BeNil())
 
-	err = delegatingCache.List(context.Background(), nsList)
+	err = delegatingCache.List(ctx, nsList)
 	g.Expect(err).To(BeNil())
 
 	g.Expect(fakeReader.Called).To(Equal(1))
