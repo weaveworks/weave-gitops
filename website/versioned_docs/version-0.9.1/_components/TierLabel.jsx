64197b95711eaafc9257d93f36f2dac36b75ac67
David Harris <david.harris@weave.works>
2022-12-09 16:46:36 +0000

    update ALL tierlabels

--- website/versioned_docs/version-0.9.1/_components/TierLabel.jsx
+++ website/versioned_docs/version-0.9.1/_components/TierLabel.jsx
@@ -8,21 +8,9 @@ const containerStyle = {
   fontVariant: "all-small-caps",
 };
 
-// This determines the current version of the docs you're looking at
-// E.g. /docs/next or /docs/0.2.5
-const getCurrentVersionPath = () => {
-  const { "docusaurus-plugin-content-docs": data } = useGlobalData();
-  const currentVersion = data?.default?.versions?.find(
-    (v) => v.name === "current"
-  );
-  // Fallback to /docs just in case. Not sure if this is async etc.
-  return currentVersion?.path || "/docs";
-};
-
 export default function TierLabel({ tiers }) {
   return (
     <Link
-      to={`${getCurrentVersionPath()}/enterprise/intro`}
       title={`This feature is a available on ${tiers}`}
       style={containerStyle}
     >
