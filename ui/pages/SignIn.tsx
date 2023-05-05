e5110507d339c75f4f6293a57ba99aad272b56b8
Joshua Israel <65822698+joshri@users.noreply.github.com>
2023-05-05 11:13:23 -0400

    Change "G" logo to Weaveworks logo (#3675)
    * new logo
    
    * increased spacing

--- ui/pages/SignIn.tsx
+++ ui/pages/SignIn.tsx
@@ -19,7 +19,7 @@ export const FormWrapper = styled(Flex)`
   background-color: ${(props) => props.theme.colors.white};
   border-radius: ${(props) => props.theme.borderRadius.soft};
   width: 500px;
-  padding-top: ${(props) => props.theme.spacing.large};
+  padding-top: 40px;
   align-content: space-between;
   .MuiButton-label {
     width: 250px;
@@ -34,7 +34,10 @@ export const FormWrapper = styled(Flex)`
 `;
 
 const Logo = styled(Flex)`
-  margin-bottom: ${(props) => props.theme.spacing.medium};
+  margin-bottom: ${(props) => props.theme.spacing.large};
+  img:first-child {
+    margin-right: ${(props) => props.theme.spacing.xs};
+  }
 `;
 
 const Footer = styled(Flex)`
@@ -117,8 +120,9 @@ function SignIn() {
         }}
       >
         <div>
-          <Logo wide center>
-            <img src={images.weaveLogo} />
+          <Logo wide align center>
+            <img src={images.logoLight} height="60px" width="auto" />
+            <img src={images.logotype} />
           </Logo>
           {isFlagEnabled("OIDC_AUTH") ? (
             <Flex wide center>
