6c13f8bb45f669564a4bd29680a4130656483b38
Yiannis <yiannis@weave.works>
2023-05-25 11:07:48 +0100

    chore: Update go-git-providers to 0.16.0

--- pkg/vendorfakes/fakegitprovider/org_repository.go
+++ pkg/vendorfakes/fakegitprovider/org_repository.go
@@ -60,6 +60,18 @@ type OrgRepository struct {
 	deployKeysReturnsOnCall map[int]struct {
 		result1 gitprovider.DeployKeyClient
 	}
+	DeployTokensStub        func() (gitprovider.DeployTokenClient, error)
+	deployTokensMutex       sync.RWMutex
+	deployTokensArgsForCall []struct {
+	}
+	deployTokensReturns struct {
+		result1 gitprovider.DeployTokenClient
+		result2 error
+	}
+	deployTokensReturnsOnCall map[int]struct {
+		result1 gitprovider.DeployTokenClient
+		result2 error
+	}
 	FilesStub        func() gitprovider.FileClient
 	filesMutex       sync.RWMutex
 	filesArgsForCall []struct {
@@ -430,6 +442,62 @@ func (fake *OrgRepository) DeployKeysReturnsOnCall(i int, result1 gitprovider.De
 	fake.deployKeysReturnsOnCall[i] = struct {
 		result1 gitprovider.DeployKeyClient
 	}{result1}
+}
+
+func (fake *OrgRepository) DeployTokens() (gitprovider.DeployTokenClient, error) {
+	fake.deployTokensMutex.Lock()
+	ret, specificReturn := fake.deployTokensReturnsOnCall[len(fake.deployTokensArgsForCall)]
+	fake.deployTokensArgsForCall = append(fake.deployTokensArgsForCall, struct {
+	}{})
+	stub := fake.DeployTokensStub
+	fakeReturns := fake.deployTokensReturns
+	fake.recordInvocation("DeployTokens", []interface{}{})
+	fake.deployTokensMutex.Unlock()
+	if stub != nil {
+		return stub()
+	}
+	if specificReturn {
+		return ret.result1, ret.result2
+	}
+	return fakeReturns.result1, fakeReturns.result2
+}
+
+func (fake *OrgRepository) DeployTokensCallCount() int {
+	fake.deployTokensMutex.RLock()
+	defer fake.deployTokensMutex.RUnlock()
+	return len(fake.deployTokensArgsForCall)
+}
+
+func (fake *OrgRepository) DeployTokensCalls(stub func() (gitprovider.DeployTokenClient, error)) {
+	fake.deployTokensMutex.Lock()
+	defer fake.deployTokensMutex.Unlock()
+	fake.DeployTokensStub = stub
+}
+
+func (fake *OrgRepository) DeployTokensReturns(result1 gitprovider.DeployTokenClient, result2 error) {
+	fake.deployTokensMutex.Lock()
+	defer fake.deployTokensMutex.Unlock()
+	fake.DeployTokensStub = nil
+	fake.deployTokensReturns = struct {
+		result1 gitprovider.DeployTokenClient
+		result2 error
+	}{result1, result2}
+}
+
+func (fake *OrgRepository) DeployTokensReturnsOnCall(i int, result1 gitprovider.DeployTokenClient, result2 error) {
+	fake.deployTokensMutex.Lock()
+	defer fake.deployTokensMutex.Unlock()
+	fake.DeployTokensStub = nil
+	if fake.deployTokensReturnsOnCall == nil {
+		fake.deployTokensReturnsOnCall = make(map[int]struct {
+			result1 gitprovider.DeployTokenClient
+			result2 error
+		})
+	}
+	fake.deployTokensReturnsOnCall[i] = struct {
+		result1 gitprovider.DeployTokenClient
+		result2 error
+	}{result1, result2}
 }
 
 func (fake *OrgRepository) Files() gitprovider.FileClient {
@@ -949,6 +1017,8 @@ func (fake *OrgRepository) Invocations() map[string][][]interface{} {
 	defer fake.deleteMutex.RUnlock()
 	fake.deployKeysMutex.RLock()
 	defer fake.deployKeysMutex.RUnlock()
+	fake.deployTokensMutex.RLock()
+	defer fake.deployTokensMutex.RUnlock()
 	fake.filesMutex.RLock()
 	defer fake.filesMutex.RUnlock()
 	fake.getMutex.RLock()
