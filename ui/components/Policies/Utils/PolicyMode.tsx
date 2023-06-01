d05c176ab12da82cecde6472956e57b40a0b12ce
AsmaaNabilBakr <Asmaa.nabilbakr@gmail.com>
2023-06-01 10:42:52 +0300

    refactor Policy mode component

--- ui/components/Policies/Utils/PolicyMode.tsx
+++ ui/components/Policies/Utils/PolicyMode.tsx
@@ -1,22 +1,8 @@
 import React from "react";
-import styled from "styled-components";
 import Flex from "../../Flex";
 import Icon, { IconType } from "../../Icon";
+import Text from "../../Text";
 
-const ModeWrapper = styled(Flex)`
-  align-items: center;
-  justify-content: flex-start;
-  display: inline-flex;
-  margin-right: ${(props) => props.theme.spacing.xs};
-  svg {
-    color: ${(props) => props.theme.colors.neutral30};
-    font-size: ${(props) => props.theme.fontSizes.large};
-    margin-right: ${(props) => props.theme.spacing.xxs};
-  }
-  span {
-    text-transform: capitalize;
-  }
-`;
 interface Props {
   modeName: string;
   showName?: boolean;
@@ -26,42 +12,36 @@ const capitalizeFirstLetter = (strToCapitalize: string) =>
   strToCapitalize.charAt(0).toUpperCase() + strToCapitalize.slice(1);
 
 const PolicyMode = ({ modeName, showName = false }: Props) => {
+  let mode = null;
   switch (modeName.toLocaleLowerCase()) {
     case "audit":
-      return ModeTooltip(
-        "audit",
-        showName,
-        <Icon size="base" type={IconType.Policy} color="neutral30" />
-      );
+      mode = {
+        name: "audit",
+        icon: (
+          <Icon type={IconType.Policy} color="neutral30" size="base" />
+        ),
+      };
+      break;
     case "admission":
-      return ModeTooltip(
-        "enforce",
-        showName,
-        <Icon size="base" type={IconType.VerifiedUser} color="neutral30" />
-      );
+      mode = {
+        name: "enforce",
+        icon: <Icon type={IconType.VerifiedUser} color="neutral30" size="base" />,
+      };
+      break;
     default:
-      return (
-        <ModeWrapper>
-          <span>-</span>
-        </ModeWrapper>
-      );
+      return <Text>-</Text>;
   }
-};
-
-const ModeTooltip = (mode: string, showName: boolean, icon: any) => {
   return (
-    <>
-      {!showName ? (
-        <ModeWrapper>
-          <span title={capitalizeFirstLetter(mode)}>{icon}</span>
-        </ModeWrapper>
-      ) : (
-        <ModeWrapper>
-          {icon}
-          <span>{mode}</span>
-        </ModeWrapper>
+    <Flex alignItems="center" start gap="4">
+      <span title={!showName && capitalizeFirstLetter(mode.name)}>
+        {mode.icon}
+      </span>
+      {showName && (
+        <Text capitalize size="medium">
+          {mode.name}
+        </Text>
       )}
-    </>
+    </Flex>
   );
 };
 
