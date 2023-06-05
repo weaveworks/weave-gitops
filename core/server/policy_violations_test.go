953dba9c640d53a0db6be034e93cc3036dea85dd
Waleed Hammam <waleed.hamam@weave.works>
2023-06-05 18:34:04 +0300

    refactor tests using gomega

--- core/server/policy_violations_test.go
+++ core/server/policy_violations_test.go
@@ -2,233 +2,141 @@ package server_test
 
 import (
 	"context"
-	"errors"
 	"testing"
 
-	"github.com/google/go-cmp/cmp"
+	. "github.com/onsi/gomega"
 	pb "github.com/weaveworks/weave-gitops/pkg/api/core"
-	"google.golang.org/protobuf/testing/protocmp"
+	"github.com/weaveworks/weave-gitops/pkg/kube"
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/apimachinery/pkg/runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client/fake"
 )
 
-func TestGetPolicyViolation(t *testing.T) {
-	tests := []struct {
-		name         string
-		ViolationId  string
-		clusterState []runtime.Object
-		clusterName  string
-		err          error
-		expected     *pb.GetPolicyValidationResponse
-	}{
-		{
-			name:        "get policy violation",
-			ViolationId: "66101548-12c1-4f79-a09a-a12979903fba",
-			clusterState: []runtime.Object{
-				makeEvent(t),
-			},
-			clusterName: "Default",
-			expected: &pb.GetPolicyValidationResponse{
-				Validation: &pb.PolicyValidation{
-					Id:              "66101548-12c1-4f79-a09a-a12979903fba",
-					Name:            "Missing app Label",
-					PolicyId:        "weave.policies.missing-app-label",
-					ClusterId:       "cluster-1",
-					Category:        "Access Control",
-					Severity:        "high",
-					CreatedAt:       "0001-01-01T00:00:00Z",
-					Message:         "Policy event",
-					Entity:          "my-deployment",
-					Namespace:       "default",
-					Description:     "Missing app label",
-					HowToSolve:      "how_to_solve",
-					ViolatingEntity: `{"apiVersion":"apps/v1","kind":"Deployment","metadata":{"name":"nginx-deployment","namespace":"default","uid":"af912668-957b-46d4-bc7a-51e6994cba56"},"spec":{"template":{"spec":{"containers":[{"image":"nginx:latest","imagePullPolicy":"Always","name":"nginx","ports":[{"containerPort":80,"protocol":"TCP"}]}]}}}}`,
-					ClusterName:     "Default",
-					Occurrences: []*pb.PolicyValidationOccurrence{
-						{
-							Message: "occurrence details",
-						},
-					},
-				},
-			},
-			err: nil,
-		},
-		{
-			name:        "policy violation doesn't exist",
-			ViolationId: "invalid-id",
-			clusterState: []runtime.Object{
-				makeEvent(t),
-			},
-			clusterName: "Default",
-			err:         errors.New("no policy violation found with id invalid-id and cluster: Default"),
-		},
-	}
+func TestGetViolation(t *testing.T) {
+	g := NewGomegaWithT(t)
 
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
+	ctx := context.Background()
 
-			fakeCl := fake.NewClientBuilder().WithRuntimeObjects(tt.clusterState...).Build()
-			cfg := makeServerConfig(fakeCl, t, "")
-			s := makeServer(cfg, t)
+	scheme, err := kube.CreateScheme()
+	g.Expect(err).To(BeNil())
 
-			policyViolation, err := s.GetPolicyValidation(context.Background(), &pb.GetPolicyValidationRequest{
-				ValidationId: tt.ViolationId,
-				ClusterName:  tt.clusterName,
-			})
-			if err != nil {
-				if tt.err == nil {
-					t.Fatalf("failed to get policy violation:\n%s", err)
-				}
-				if diff := cmp.Diff(tt.err.Error(), err.Error()); diff != "" {
-					t.Fatalf("unexpected error while getting policy:\n%s", diff)
-				}
-			} else {
-				if diff := cmp.Diff(tt.expected, policyViolation, protocmp.Transform()); diff != "" {
-					t.Fatalf("policy violation didn't match expected:\n%s", diff)
-				}
-			}
-		})
-	}
+	client := fake.NewClientBuilder().WithScheme(scheme).WithRuntimeObjects(makeValidationEvent(t)).Build()
+	cfg := makeServerConfig(client, t, "")
+	c := makeServer(cfg, t)
+
+	// existing validation
+	res, err := c.GetPolicyValidation(ctx, &pb.GetPolicyValidationRequest{
+		ValidationId: "66101548-12c1-4f79-a09a-a12979903fba",
+	})
+	g.Expect(err).NotTo(HaveOccurred())
+	g.Expect(res.Validation).NotTo(BeNil())
+	g.Expect(res.Validation.Id).To(Equal("66101548-12c1-4f79-a09a-a12979903fba"))
+	g.Expect(res.Validation.Name).To(Equal("Missing app Label"))
+	g.Expect(res.Validation.PolicyId).To(Equal("weave.policies.missing-app-label"))
+	g.Expect(res.Validation.ClusterId).To(Equal("cluster-1"))
+	g.Expect(res.Validation.Category).To(Equal("Access Control"))
+	g.Expect(res.Validation.Severity).To(Equal("high"))
+	g.Expect(res.Validation.CreatedAt).To(Equal("0001-01-01T00:00:00Z"))
+	g.Expect(res.Validation.Message).To(Equal("Policy event"))
+	g.Expect(res.Validation.Entity).To(Equal("my-deployment"))
+	g.Expect(res.Validation.Namespace).To(Equal("default"))
+	g.Expect(res.Validation.Description).To(Equal("Missing app label"))
+	g.Expect(res.Validation.HowToSolve).To(Equal("how_to_solve"))
+	g.Expect(res.Validation.ViolatingEntity).To(Equal(`{"apiVersion":"apps/v1","kind":"Deployment","metadata":{"name":"nginx-deployment","namespace":"default","uid":"af912668-957b-46d4-bc7a-51e6994cba56"},"spec":{"template":{"spec":{"containers":[{"image":"nginx:latest","imagePullPolicy":"Always","name":"nginx","ports":[{"containerPort":80,"protocol":"TCP"}]}]}}}}`))
+	g.Expect(res.Validation.ClusterName).To(Equal("Default"))
+	g.Expect(res.Validation.Occurrences).To(Equal([]*pb.PolicyValidationOccurrence{{Message: "occurrence details"}}))
+
+	// non existing validation
+	res, err = c.GetPolicyValidation(ctx, &pb.GetPolicyValidationRequest{
+		ValidationId: "invalid-id",
+	})
+	g.Expect(err).To(HaveOccurred())
+	g.Expect(res.Validation).To(BeNil())
+}
+
+func TestListApplicationValidations(t *testing.T) {
+	g := NewGomegaWithT(t)
+
+	ctx := context.Background()
+
+	scheme, err := kube.CreateScheme()
+	g.Expect(err).To(BeNil())
+
+	client := fake.NewClientBuilder().WithScheme(scheme).WithRuntimeObjects(makeValidationEvent(t), makeValidationEvent(t, func(e *corev1.Event) {
+		e.ObjectMeta.Name = "Missing Owner Label - fake-event-2"
+		e.InvolvedObject.Namespace = "weave-system"
+		e.ObjectMeta.Namespace = "weave-system"
+		e.InvolvedObject.Name = "app1"
+		e.InvolvedObject.Kind = "HelmRelease"
+		e.Annotations["policy_name"] = "Missing Owner Label"
+		e.Annotations["policy_id"] = "weave.policies.missing-app-label"
+		e.Labels["pac.weave.works/id"] = "56701548-12c1-4f79-a09a-a12979904"
+	})).Build()
+
+	cfg := makeServerConfig(client, t, "")
+	c := makeServer(cfg, t)
+	res, err := c.ListPolicyValidations(ctx, &pb.ListPolicyValidationsRequest{
+		Application: "app1",
+		Kind:        "HelmRelease",
+		Namespace:   "weave-system",
+	})
+	g.Expect(err).NotTo(HaveOccurred())
+	g.Expect(err).To(BeNil())
+	g.Expect(len(res.Violations)).To(Equal(1))
+	g.Expect(res.Violations[0].Id).To(Equal("56701548-12c1-4f79-a09a-a12979904"))
+	g.Expect(res.Violations[0].Name).To(Equal("Missing Owner Label"))
+	g.Expect(res.Violations[0].PolicyId).To(Equal("weave.policies.missing-app-label"))
+	g.Expect(res.Violations[0].ClusterId).To(Equal("cluster-1"))
+	g.Expect(res.Violations[0].Category).To(Equal("Access Control"))
+	g.Expect(res.Violations[0].Severity).To(Equal("high"))
+	g.Expect(res.Violations[0].CreatedAt).To(Equal("0001-01-01T00:00:00Z"))
+	g.Expect(res.Violations[0].Message).To(Equal("Policy event"))
+	g.Expect(res.Violations[0].Entity).To(Equal("app1"))
+	g.Expect(res.Violations[0].Namespace).To(Equal("weave-system"))
+	g.Expect(res.Violations[0].ClusterName).To(Equal("Default"))
 }
 
 func TestListPolicyValidations(t *testing.T) {
-	tests := []struct {
-		name         string
-		clusterState []runtime.Object
-		events       []*corev1.Event
-		err          error
-		expected     *pb.ListPolicyValidationsResponse
-		clusterName  string
-		appName      string
-		appKind      string
-		namespace    string
-	}{
-		{
-			name: "list policy violations",
-			clusterState: []runtime.Object{
-				makeEvent(t),
-				makeEvent(t, func(e *corev1.Event) {
-					e.ObjectMeta.Name = "Missing Owner Label - fake-event-2"
-					e.InvolvedObject.Namespace = "weave-system"
-					e.ObjectMeta.Namespace = "weave-system"
-					e.Annotations["policy_name"] = "Missing Owner Label"
-					e.Annotations["policy_id"] = "weave.policies.missing-app-label"
-					e.Labels["pac.weave.works/id"] = "56701548-12c1-4f79-a09a-a12979903"
-				}),
-			},
-			expected: &pb.ListPolicyValidationsResponse{
-				Violations: []*pb.PolicyValidation{
-					{
-						Id:          "66101548-12c1-4f79-a09a-a12979903fba",
-						Name:        "Missing app Label",
-						PolicyId:    "weave.policies.missing-app-label",
-						ClusterId:   "cluster-1",
-						Category:    "Access Control",
-						Severity:    "high",
-						CreatedAt:   "0001-01-01T00:00:00Z",
-						Message:     "Policy event",
-						Entity:      "my-deployment",
-						Namespace:   "default",
-						ClusterName: "Default",
-					},
-					{
-						Id:          "56701548-12c1-4f79-a09a-a12979903",
-						Name:        "Missing Owner Label",
-						PolicyId:    "weave.policies.missing-app-label",
-						ClusterId:   "cluster-1",
-						Category:    "Access Control",
-						Severity:    "high",
-						CreatedAt:   "0001-01-01T00:00:00Z",
-						Message:     "Policy event",
-						Entity:      "my-deployment",
-						Namespace:   "weave-system",
-						ClusterName: "Default",
-					},
-				},
-				Total: int32(2),
-			},
-		},
-		{
-			name: "list application policy violations",
-			clusterState: []runtime.Object{
-				makeEvent(t, func(e *corev1.Event) {
-					e.ObjectMeta.Name = "Missing Owner Label - fake-event-2"
-					e.InvolvedObject.Namespace = "weave-system"
-					e.ObjectMeta.Namespace = "weave-system"
-					e.InvolvedObject.Name = "app1"
-					e.InvolvedObject.Kind = "HelmRelease"
-					e.Annotations["policy_name"] = "Missing Owner Label"
-					e.Annotations["policy_id"] = "weave.policies.missing-app-label"
-					e.Labels["pac.weave.works/id"] = "56701548-12c1-4f79-a09a-a12979904"
-				}),
-			},
-			expected: &pb.ListPolicyValidationsResponse{
-				Violations: []*pb.PolicyValidation{
-					{
-						Id:          "56701548-12c1-4f79-a09a-a12979904",
-						Name:        "Missing Owner Label",
-						PolicyId:    "weave.policies.missing-app-label",
-						ClusterId:   "cluster-1",
-						Category:    "Access Control",
-						Severity:    "high",
-						CreatedAt:   "0001-01-01T00:00:00Z",
-						Message:     "Policy event",
-						Entity:      "app1",
-						Namespace:   "weave-system",
-						ClusterName: "Default",
-					},
-				},
-				Total: int32(1),
-			},
-			appName:   "app1",
-			appKind:   "HelmRelease",
-			namespace: "weave-system",
-		},
-		{
-			name: "list policy violations with cluster filtering",
-			clusterState: []runtime.Object{
-				makeEvent(t),
-			},
-			expected:    &pb.ListPolicyValidationsResponse{},
-			clusterName: "wrong",
-		},
-	}
+	g := NewGomegaWithT(t)
 
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			fakeCl := fake.NewClientBuilder().WithRuntimeObjects(tt.clusterState...).Build()
+	ctx := context.Background()
 
-			cfg := makeServerConfig(fakeCl, t, "")
-			s := makeServer(cfg, t)
+	scheme, err := kube.CreateScheme()
+	g.Expect(err).To(BeNil())
 
-			policyViolation, err := s.ListPolicyValidations(context.Background(), &pb.ListPolicyValidationsRequest{
-				ClusterName: tt.clusterName,
-				Application: tt.appName,
-				Kind:        tt.appKind,
-				Namespace:   tt.namespace,
-			})
-			if err != nil {
-				if tt.err == nil {
-					t.Fatalf("failed to list policy violation:\n%s", err)
-				}
-				if diff := cmp.Diff(tt.err.Error(), err.Error()); diff != "" {
-					t.Fatalf("unexpected error while getting policy:\n%s", diff)
-				}
-			} else {
-				if policyViolation.Total != tt.expected.Total {
-					t.Fatalf("total policy violation didn't match expected:\n%s", cmp.Diff(tt.expected.Total, policyViolation.Total))
-				}
-				if diff := cmp.Diff(tt.expected.Violations, policyViolation.Violations, protocmp.Transform()); diff != "" {
-					t.Fatalf("policy violation didn't match expected:\n%s", diff)
-				}
-			}
-		})
-	}
+	client := fake.NewClientBuilder().WithScheme(scheme).WithRuntimeObjects(makeValidationEvent(t), makeValidationEvent(t, func(e *corev1.Event) {
+		e.ObjectMeta.Name = "Missing Owner Label - fake-event-2"
+		e.InvolvedObject.Namespace = "weave-system"
+		e.ObjectMeta.Namespace = "weave-system"
+		e.InvolvedObject.Name = "app1"
+		e.InvolvedObject.Kind = "HelmRelease"
+		e.Annotations["policy_name"] = "Test Policy"
+		e.Annotations["policy_id"] = "weave.policies.test-policy"
+		e.Labels["pac.weave.works/id"] = "56701548-12c1-4f79-a09a-a12979904"
+	})).Build()
+
+	cfg := makeServerConfig(client, t, "")
+	c := makeServer(cfg, t)
+	res, err := c.ListPolicyValidations(ctx, &pb.ListPolicyValidationsRequest{
+		PolicyId: "weave.policies.test-policy",
+	})
+	g.Expect(err).NotTo(HaveOccurred())
+	g.Expect(err).To(BeNil())
+	g.Expect(len(res.Violations)).To(Equal(1))
+	g.Expect(res.Violations[0].Id).To(Equal("56701548-12c1-4f79-a09a-a12979904"))
+	g.Expect(res.Violations[0].Name).To(Equal("Test Policy"))
+	g.Expect(res.Violations[0].PolicyId).To(Equal("weave.policies.test-policy"))
+	g.Expect(res.Violations[0].ClusterId).To(Equal("cluster-1"))
+	g.Expect(res.Violations[0].Category).To(Equal("Access Control"))
+	g.Expect(res.Violations[0].Severity).To(Equal("high"))
+	g.Expect(res.Violations[0].CreatedAt).To(Equal("0001-01-01T00:00:00Z"))
+	g.Expect(res.Violations[0].Message).To(Equal("Policy event"))
+	g.Expect(res.Violations[0].Entity).To(Equal("app1"))
+	g.Expect(res.Violations[0].Namespace).To(Equal("weave-system"))
+	g.Expect(res.Violations[0].ClusterName).To(Equal("Default"))
 }
 
-func makeEvent(t *testing.T, opts ...func(e *corev1.Event)) *corev1.Event {
+func makeValidationEvent(t *testing.T, opts ...func(e *corev1.Event)) *corev1.Event {
 	t.Helper()
 	event := &corev1.Event{
 		InvolvedObject: corev1.ObjectReference{
