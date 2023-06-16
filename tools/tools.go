20bafdd20a9bd0cf73ab8884e290ffe1a4cfef49
Simon Howe <simon@weave.works>
2023-06-16 10:05:40 +0200

    Try and fix ci static checks

--- tools/tools.go
+++ tools/tools.go
@@ -4,6 +4,7 @@
 package tools
 
 import (
+	_ "github.com/grpc-ecosystem/grpc-gateway/v2/protoc-gen-grpc-gateway"
 	_ "github.com/grpc-ecosystem/protoc-gen-grpc-gateway-ts"
 	_ "github.com/maxbrunsfeld/counterfeiter/v6"
 )
