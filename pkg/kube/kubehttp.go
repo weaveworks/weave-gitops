65f5be703d418af77fdebeb926eeeca7323dde28
Yiannis <yiannis@weave.works>
2023-07-04 09:01:11 +0100

    fix: Remove v1 API scheme registrations

--- pkg/kube/kubehttp.go
+++ pkg/kube/kubehttp.go
@@ -12,11 +12,8 @@ import (
 	helmv2 "github.com/fluxcd/helm-controller/api/v2beta1"
 	automation1 "github.com/fluxcd/image-automation-controller/api/v1beta1"
 	reflectorv1 "github.com/fluxcd/image-reflector-controller/api/v1beta1"
-	kustomizev1 "github.com/fluxcd/kustomize-controller/api/v1"
 	kustomizev1beta2 "github.com/fluxcd/kustomize-controller/api/v1beta2"
-	notificationv1 "github.com/fluxcd/notification-controller/api/v1"
 	notificationv1beta2 "github.com/fluxcd/notification-controller/api/v1beta2"
-	sourcev1 "github.com/fluxcd/source-controller/api/v1"
 	sourcev1beta2 "github.com/fluxcd/source-controller/api/v1beta2"
 	"github.com/pkg/errors"
 	pacv2beta2 "github.com/weaveworks/policy-agent/api/v2beta2"
@@ -32,9 +29,7 @@ func CreateScheme() (*apiruntime.Scheme, error) {
 	scheme := apiruntime.NewScheme()
 	builder := apiruntime.SchemeBuilder{
 		sourcev1beta2.AddToScheme,
-		sourcev1.AddToScheme,
 		kustomizev1beta2.AddToScheme,
-		kustomizev1.AddToScheme,
 		helmv2.AddToScheme,
 		corev1.AddToScheme,
 		extensionsv1.AddToScheme,
@@ -42,7 +37,6 @@ func CreateScheme() (*apiruntime.Scheme, error) {
 		rbacv1.AddToScheme,
 		authv1.AddToScheme,
 		notificationv1beta2.AddToScheme,
-		notificationv1.AddToScheme,
 		reflectorv1.AddToScheme,
 		automation1.AddToScheme,
 		pacv2beta2.AddToScheme,
