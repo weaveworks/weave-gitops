60db89ed123b1ccbf732ee708e6ccbe2ccc40549
Joshua Israel <joshua.israel@weave.works>
2023-07-07 14:57:17 -0400

    pass down component pieces as actions to allow for hook usage

--- ui/components/CheckboxActions.tsx
+++ ui/components/CheckboxActions.tsx
@@ -4,13 +4,13 @@ import * as React from "react";
 import styled from "styled-components";
 import { useSyncFluxObject } from "../hooks/automations";
 import { useToggleSuspend } from "../hooks/flux";
+import { ObjectRef } from "../lib/api/core/types.pb";
 import Button from "./Button";
 import Flex from "./Flex";
 import Icon, { IconType } from "./Icon";
-import Spacer from "./Spacer";
 import SyncButton from "./SyncButton";
 
-export const makeObjects = (checked: string[], rows: any[]) => {
+export const makeObjects = (checked: string[], rows: any[]): ObjectRef[] => {
   const objects = [];
   checked.forEach((uid) => {
     const row = _.find(rows, (row) => {
@@ -27,21 +27,25 @@ export const makeObjects = (checked: string[], rows: any[]) => {
   return objects;
 };
 
-type Props = {
-  className?: string;
-  checked?: string[];
-  rows?: any[];
+const DefaultSync: React.FC<{ reqObjects: ObjectRef[] }> = ({ reqObjects }) => {
+  const defaultSync = useSyncFluxObject(reqObjects);
+  return (
+    <SyncButton
+      disabled={reqObjects[0] ? false : true}
+      loading={defaultSync.isLoading}
+      onClick={(opts) => defaultSync.mutateAsync(opts)}
+    />
+  );
 };
 
-function CheckboxActions({ className, checked = [], rows = [] }: Props) {
-  const [reqObjects, setReqObjects] = React.useState([]);
-  const hasChecked = checked.length > 0;
-
-  React.useEffect(() => {
-    if (hasChecked && rows.length) setReqObjects(makeObjects(checked, rows));
-  }, [checked, rows]);
-
-  function createSuspendHandler(suspend: boolean) {
+const DefaultSuspend: React.FC<{
+  reqObjects: ObjectRef[];
+  suspend: boolean;
+}> = ({ reqObjects, suspend }) => {
+  function createDefaultSuspendHandler(
+    reqObjects: ObjectRef[],
+    suspend: boolean
+  ) {
     const result = useToggleSuspend(
       {
         objects: reqObjects,
@@ -52,40 +56,75 @@ function CheckboxActions({ className, checked = [], rows = [] }: Props) {
         ? "automations"
         : "sources"
     );
-
     return () => result.mutateAsync();
   }
 
-  const sync = useSyncFluxObject(reqObjects);
+  return (
+    <Tooltip
+      title={suspend ? "Suspend Selected" : "Resume Selected"}
+      placement="top"
+    >
+      <div>
+        <Button
+          disabled={reqObjects[0] ? false : true}
+          onClick={createDefaultSuspendHandler(reqObjects, suspend)}
+        >
+          <Icon
+            type={suspend ? IconType.PauseIcon : IconType.PlayIcon}
+            size="medium"
+          />
+        </Button>
+      </div>
+    </Tooltip>
+  );
+};
+
+type Action = {
+  element: React.ReactElement;
+  additionalProps?: { [key: string]: any };
+};
+
+type Props = {
+  className?: string;
+  checked?: string[];
+  rows?: any[];
+  actions?: Action[];
+};
+
+function CheckboxActions({
+  className,
+  checked = [],
+  rows = [],
+  actions,
+}: Props) {
+  const [reqObjects, setReqObjects] = React.useState([]);
+  const hasChecked = checked.length > 0;
+
+  React.useEffect(() => {
+    if (hasChecked && rows.length) setReqObjects(makeObjects(checked, rows));
+  }, [checked, rows]);
+
+  const defaultActions = [
+    { element: DefaultSync },
+    { element: DefaultSuspend, additionalProps: { suspend: true } },
+    { element: DefaultSuspend, additionalProps: { suspend: false } },
+  ];
+  const hasActions = actions || defaultActions;
 
   return (
-    <Flex start align className={className}>
-      <SyncButton
-        onClick={(opts) => sync.mutateAsync(opts)}
-        loading={sync.isLoading}
-        disabled={!hasChecked}
-      />
-      <Spacer padding="xxs" />
-      <Tooltip title="Suspend Selected" placement="top">
-        <div>
-          <Button disabled={!hasChecked} onClick={createSuspendHandler(true)}>
-            <Icon type={IconType.PauseIcon} size="medium" />
-          </Button>
-        </div>
-      </Tooltip>
-      <Spacer padding="xxs" />
-      <Tooltip title="Resume Selected" placement="top">
-        <div>
-          <Button disabled={!hasChecked} onClick={createSuspendHandler(false)}>
-            <Icon type={IconType.PlayIcon} size="medium" />
-          </Button>
-        </div>
-      </Tooltip>
-      <Spacer padding="xxs" />
+    <Flex start align className={className} gap="8">
+      {hasActions.map((action) => {
+        return React.createElement(action.element, {
+          ...action.additionalProps,
+          reqObjects: reqObjects,
+        });
+      })}
     </Flex>
   );
 }
 
 export default styled(CheckboxActions).attrs({
   className: CheckboxActions.name,
-})``;
+})`
+  margin-right: 8px;
+`;
