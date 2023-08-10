230020348cfe981c0003f43d2d8bede4e0c1f89a
Waleed Hammam <waleed.hammam@gmail.com>
2023-08-10 15:21:15 +0300

    run make proto

--- ui/lib/api/core/types.pb.ts
+++ ui/lib/api/core/types.pb.ts
@@ -20,7 +20,7 @@ export enum Kind {
   ImagePolicy = "ImagePolicy",
   Pod = "Pod",
   Policy = "Policy",
-  AllPoliciesViolations = "AllPoliciesViolations"
+  AllPoliciesViolations = "AllPoliciesViolations",
 }
 
 export enum HelmRepositoryType {
