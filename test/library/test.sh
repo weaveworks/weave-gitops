7a6b3fd4d3c0721459f74805fc7c4416ca9d9303
Jordan Pellizzari <jordan@weave.works>
2023-08-17 13:32:29 -0700

    Convert to yarn for package management

--- test/library/test.sh
+++ test/library/test.sh
@@ -4,7 +4,7 @@ set -x
 
 git clone https://github.com/weaveworks-gitops-test/wego-library-test.git test/library/wego-library-test
 cd test/library/wego-library-test
-npm ci
-npm run build
+yarn install --frozen-lockfile
+yarn build
 go mod tidy
 go build main.go
