0b507dbc3501b440a9ebf9b4416d600f930584b0
AlinaGoaga <goaga.alina@gmail.com>
2023-09-19 10:57:20 +0200

    Refactor getHelmReleaseInventory to use getHelmReleaseObjects

--- core/server/helm_release.go
+++ core/server/helm_release.go
@@ -1,86 +1,25 @@
 package server
 
 import (
-	"bytes"
-	"compress/gzip"
 	"context"
-	"encoding/base64"
-	"encoding/json"
 	"fmt"
-	"io"
 	"strings"
 
 	helmv2 "github.com/fluxcd/helm-controller/api/v2beta1"
-	"github.com/fluxcd/pkg/ssa"
 	"github.com/weaveworks/weave-gitops/core/clustersmngr"
-	"github.com/weaveworks/weave-gitops/core/server/types"
 	pb "github.com/weaveworks/weave-gitops/pkg/api/core"
-	v1 "k8s.io/api/core/v1"
-	"sigs.k8s.io/controller-runtime/pkg/client"
 )
 
 func getHelmReleaseInventory(ctx context.Context, helmRelease helmv2.HelmRelease, c clustersmngr.Client, cluster string) ([]*pb.GroupVersionKind, error) {
-	storageNamespace := helmRelease.GetStorageNamespace()
-
-	storageName := helmRelease.GetReleaseName()
-
-	storageVersion := helmRelease.Status.LastReleaseRevision
-	if storageVersion < 1 {
-		// skip release if it failed to install
-		return nil, nil
+	k8sClient, err := c.Scoped(cluster)
+	if err != nil {
+		return nil, fmt.Errorf("error getting scoped client for cluster=%s: %w", cluster, err)
 	}
 
-	storageSecret := &v1.Secret{}
-	secretName := fmt.Sprintf("sh.helm.release.v1.%s.v%v", storageName, storageVersion)
-	key := client.ObjectKey{
-		Name:      secretName,
-		Namespace: storageNamespace,
-	}
+	objects, err := getHelmReleaseObjects(ctx, k8sClient, &helmRelease)
 
-	if helmRelease.Spec.KubeConfig != nil {
-		// helmrelease secret is on another cluster so we cannot inspect it to figure out the inventory and version and other things
-		return nil, nil
-	} else {
-		if err := c.Get(ctx, cluster, key, storageSecret); err != nil {
-			return nil, err
-		}
-	}
-
-	releaseData, releaseFound := storageSecret.Data["release"]
-	if !releaseFound {
-		return nil, fmt.Errorf("failed to decode the Helm storage object for HelmRelease '%s'", helmRelease.Name)
-	}
-
-	byteData, err := base64.StdEncoding.DecodeString(string(releaseData))
 	if err != nil {
 		return nil, err
-	}
-
-	var magicGzip = []byte{0x1f, 0x8b, 0x08}
-	if bytes.Equal(byteData[0:3], magicGzip) {
-		r, err := gzip.NewReader(bytes.NewReader(byteData))
-		if err != nil {
-			return nil, err
-		}
-
-		defer r.Close()
-
-		uncompressedByteData, err := io.ReadAll(r)
-		if err != nil {
-			return nil, err
-		}
-
-		byteData = uncompressedByteData
-	}
-
-	storage := types.HelmReleaseStorage{}
-	if err := json.Unmarshal(byteData, &storage); err != nil {
-		return nil, fmt.Errorf("failed to decode the Helm storage object for HelmRelease '%s': %w", helmRelease.Name, err)
-	}
-
-	objects, err := ssa.ReadObjects(strings.NewReader(storage.Manifest))
-	if err != nil {
-		return nil, fmt.Errorf("failed to read the Helm storage object for HelmRelease '%s': %w", helmRelease.Name, err)
 	}
 
 	var gvk []*pb.GroupVersionKind
