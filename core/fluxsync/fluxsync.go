fada58840103918fe8986476bfe01ef6b6addbf2
yiannis <yiannis@weave.works>
2023-11-03 18:14:11 +0000

    fix: Update deprecated code

--- core/fluxsync/fluxsync.go
+++ core/fluxsync/fluxsync.go
@@ -44,14 +44,8 @@ func RequestReconciliation(ctx context.Context, k client.Client, name client.Obj
 
 // WaitForSync polls the k8s API until the resources is sync'd, and times out eventually.
 func WaitForSync(ctx context.Context, c client.Client, key client.ObjectKey, obj Reconcilable) error {
-	//nolint:staticcheck // deprecated, tracking issue: https://github.com/weaveworks/weave-gitops/issues/3812
-	if err := wait.PollImmediate(
-		k8sPollInterval,
-		k8sTimeout,
-		checkResourceSync(ctx, c, key, obj, obj.GetLastHandledReconcileRequest()),
-	); err != nil {
-		//nolint:staticcheck // deprecated, tracking issue: https://github.com/weaveworks/weave-gitops/issues/3812
-		if errors.Is(err, wait.ErrWaitTimeout) {
+	if err := wait.PollUntilContextTimeout(context.Background(), k8sPollInterval, k8sTimeout, true, checkResourceSync(ctx, c, key, obj, obj.GetLastHandledReconcileRequest())); err != nil {
+		if wait.Interrupted(err) {
 			return errors.New("sync request timed out. The sync operation may still be in progress")
 		}
 
@@ -61,8 +55,8 @@ func WaitForSync(ctx context.Context, c client.Client, key client.ObjectKey, obj
 	return nil
 }
 
-func checkResourceSync(ctx context.Context, c client.Client, name client.ObjectKey, obj Reconcilable, lastReconcile string) func() (bool, error) {
-	return func() (bool, error) {
+func checkResourceSync(ctx context.Context, c client.Client, name client.ObjectKey, obj Reconcilable, lastReconcile string) func(context.Context) (bool, error) {
+	return func(_ context.Context) (bool, error) {
 		err := c.Get(ctx, name, obj.AsClientObject())
 		if err != nil {
 			return false, err
