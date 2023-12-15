3b9896f8cc35fff5c01b1c463a82cd9e1d5cf00b
Eneko Fernandez <eneko@weave.works>
2023-12-15 12:08:36 +0100

    can set configuration via feature flag
    Signed-off-by: Eneko Fernandez <eneko@weave.works>

--- core/server/fluxruntime_test.go
+++ core/server/fluxruntime_test.go
@@ -1,9 +1,11 @@
 package server
 
 import (
+	"os"
 	"testing"
 
 	"github.com/google/go-cmp/cmp"
+	"github.com/weaveworks/weave-gitops/pkg/featureflags"
 )
 
 func Test_getRuntimeLabels(t *testing.T) {
@@ -13,14 +15,34 @@ func Test_getRuntimeLabels(t *testing.T) {
 		wantRuntimeLabels        []string
 	}{
 		{
-			name: "should return flux if not feature flag exists",
-			wantRuntimeLabels: []string{
-				FluxNamespacePartOf,
-			},
+			name:              "should return flux if not feature flag exists",
+			wantRuntimeLabels: FluxRuntimeLabels,
+		},
+		{
+			name:                     "should return flux if feature flag exists but empty",
+			wantRuntimeLabels:        FluxRuntimeLabels,
+			gitopsRuntimeFeatureFlag: "",
+		},
+		{
+			name:                     "should return flux if feature flag exists and false",
+			wantRuntimeLabels:        FluxRuntimeLabels,
+			gitopsRuntimeFeatureFlag: "false",
+		},
+		{
+			name:                     "should return weave gitops if feature flag exists and true",
+			wantRuntimeLabels:        WeaveGitopsRuntimeLabels,
+			gitopsRuntimeFeatureFlag: "true",
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
+			if tt.gitopsRuntimeFeatureFlag != "" {
+				_ = os.Setenv(GitopsRuntimeFeatureFlag, tt.gitopsRuntimeFeatureFlag)
+			}
+			defer func() {
+				_ = os.Unsetenv(GitopsRuntimeFeatureFlag)
+			}()
+			featureflags.SetFromEnv(os.Environ())
 			gotLabels := getRuntimeLabels()
 			if diff := cmp.Diff(tt.wantRuntimeLabels, gotLabels); diff != "" {
 				t.Fatalf("unexpected labels:\n%s", diff)
