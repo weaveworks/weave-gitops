bc26497b14051f7bbae1dd7c153f29b7ca11662a
Eneko Fernandez <eneko@weave.works>
2023-12-28 07:08:26 +0100

    Refactor "Getting Started" guide for clarity and structure
    Changes include dividing the guide into steps rather than days and improving descriptions for more clarity. The restructuring allows users to follow the guide more intuitively, providing a clearer understanding of different stages, such as bootstrapping and authentication. The enhancement aims to provide a more structured and digestible learning experience.
    
    Signed-off-by: Eneko Fernandez <eneko@weave.works>

--- website/sidebars.js
+++ website/sidebars.js
@@ -41,7 +41,7 @@ module.exports = {
           items: [
             {
               type: "category",
-              label: "Day 0 - Getting Started",
+              label: "Step 1 - Getting Started",
               link: {
                 type: "doc",
                 id: "enterprise/getting-started/install-enterprise-getting-started",
