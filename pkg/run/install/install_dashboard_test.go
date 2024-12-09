d088b5889f2d7b72ba63c316338de1089a057d6a
Charles Sibbald <casibbald@gmail.com>
2024-12-09 21:27:16 +0200

    Debug : testing output of test

--- pkg/run/install/install_dashboard_test.go
+++ pkg/run/install/install_dashboard_test.go
@@ -218,8 +218,7 @@ var _ = Describe("GetInstalledDashboard", func() {
 		})
 		Expect(err).ToNot(HaveOccurred())
 		Expect(dashboardType).To(Equal(DashboardTypeOSS))
-		println(dashboardName)
-		Expect(dashboardName).To(Equal(""))
+		Expect(dashboardName).To(Equal([]string{dashboardName, "-", "?"}))
 	})
 
 	// It("returns the enterprise dashboard type if the dashboard is installed with a helmrelease", func() {
