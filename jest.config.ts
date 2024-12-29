70008c630cb5f6af3fdeb5c6d0d4f4e71ab48e7f
Amund Tenstad <github@amund.io>
2024-12-29 20:28:52 +0100

    chore: jest 29 (#4303)
    * chore(deps): upgrade jest to 29
    
    * fix: jest

---
+++ jest.config.ts
@@ -0,0 +1,25 @@
+import type { Config } from "jest";
+
+const config: Config = {
+  preset: "ts-jest",
+  moduleNameMapper: {
+    "\\.(jpg|ico|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
+      "<rootDir>/ui/lib/fileMock.js",
+    "\\.(css|less)$": "<rootDir>/ui/lib/fileMock.js",
+  },
+  transform: {
+    "\\.tsx?$": "ts-jest",
+    "\\.jsx?$": [
+      "babel-jest",
+      {
+        configFile: "./babel.config.js",
+      },
+    ],
+  },
+  transformIgnorePatterns: ["/node_modules/(?!(yaml)/)"],
+  setupFilesAfterEnv: ["<rootDir>/setup-jest.ts"],
+  modulePathIgnorePatterns: ["<rootDir>/dist/"],
+  testEnvironment: "jsdom",
+};
+
+export default config;
