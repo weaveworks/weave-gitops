1b6d71b83a7ed103037b840f7e050b0b354787ca
Erik Godding Boye <egboye@gmail.com>
2024-12-30 17:14:57 +0100

    chore!: remove core server GetSessionLogs operation (#4325)

--- pkg/run/constants/constants.go
+++ pkg/run/constants/constants.go
@@ -1,14 +1,7 @@
 package constants
 
 const (
-	RunDevBucketName        = "run-dev-bucket"
-	RunDevKsName            = "run-dev-ks"
-	RunDevHelmName          = "run-dev-helm"
-	GitOpsRunNamespace      = "gitops-run"
-	RunDevBucketCredentials = RunDevBucketName + "-credentials"
-	RunDevKsDecryption      = RunDevKsName + "-decryption"
-)
-
-const (
-	PausedAnnotation = "loft.sh/paused"
+	RunDevBucketName = "run-dev-bucket"
+	RunDevKsName     = "run-dev-ks"
+	RunDevHelmName   = "run-dev-helm"
 )
