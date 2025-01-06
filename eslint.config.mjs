0f290a8279f7d0f2cdc3cd260c1a29d7bd3d9516
Erik Godding Boye <egboye@gmail.com>
2025-01-06 22:25:26 +0000

    Migrate eslint config

---
+++ eslint.config.mjs
@@ -0,0 +1,41 @@
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import importPlugin from 'eslint-plugin-import';
+
+export default tseslint.config({
+    extends: [
+        eslint.configs.recommended,
+        importPlugin.flatConfigs.errors,
+        importPlugin.flatConfigs.warnings,
+        tseslint.configs.recommended,
+        importPlugin.flatConfigs.typescript,
+    ],
+    rules: {
+        "import/default": 0,
+        "import/no-named-as-default-member": 0,
+        "import/named": 2,
+        "import/order": [
+            2,
+            {
+                alphabetize: {
+                    order: "asc",
+                    caseInsensitive: true,
+                },
+            }
+        ],
+        "@typescript-eslint/explicit-module-boundary-types": 0,
+        "@typescript-eslint/no-explicit-any": 0,
+        "@typescript-eslint/ban-ts-comment": 0,
+        "import/no-named-as-default": 0,
+        "@typescript-eslint/switch-exhaustiveness-check": "error",
+        "import/no-unresolved": 0,
+    },
+    ignores: ["**/*.pb.ts"],
+    languageOptions: {
+        ecmaVersion: 5,
+        sourceType: "script",
+        parserOptions: {
+            project: "./tsconfig.json",
+        },
+    },
+});
