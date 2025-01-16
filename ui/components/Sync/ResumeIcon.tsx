296c06ff954437f63dc4d646efdf3f7589088f32
Martin Proffitt <mproffitt@choclab.net>
2025-01-16 21:01:18 +0100

    Better centring for the resume icon
    The resume icon was offset inside the circle by 2 pixels causing it to
    look slightly out of place compared to other sync control icons.

--- ui/components/Sync/ResumeIcon.tsx
+++ ui/components/Sync/ResumeIcon.tsx
@@ -5,7 +5,7 @@ function ResumeIcon() {
     <svg
       width="24"
       height="24"
-      viewBox="0 0 24 24"
+      viewBox="-2 0 24 24"
       xmlns="http://www.w3.org/2000/svg"
     >
       <path d="M4.08331 18.75V4.75H6.41665V18.75H4.08331ZM8.74998 18.75L20.4166 11.75L8.74998 4.75V18.75Z" />
